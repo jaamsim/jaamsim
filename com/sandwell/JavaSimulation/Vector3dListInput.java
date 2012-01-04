42a4c01d58f1108f2c35be5460f3d5feaf95cd8c
Stephen Wong <stephen.wong@ausencosandwell.com>
2012-01-04 14:42:18 -0800

    JS: Added units to Vector3dListInput parsing
    This change is required for allowing keywords like Points to accept units
    
    Signed-off-by: Stephen Wong <stephen.wong@ausencosandwell.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausencosandwell.com>

--- com/sandwell/JavaSimulation/Vector3dListInput.java
+++ com/sandwell/JavaSimulation/Vector3dListInput.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
 
 import javax.vecmath.Vector3d;
 
+import com.sandwell.JavaSimulation3D.InputAgent;
+
 public class Vector3dListInput extends ListInput<ArrayList<Vector3d>> {
 
 	public Vector3dListInput(String key, String cat, ArrayList<Vector3d> def) {
@@ -35,8 +37,43 @@ public class Vector3dListInput extends ListInput<ArrayList<Vector3d>> {
 		ArrayList<Vector3d> tempValue = new ArrayList<Vector3d>();
 
 		for( StringVector innerInput : splitData ) {
-			Vector3d temp = Input.parseVector3d(innerInput);
-			tempValue.add(temp);
+			DoubleVector temp;
+
+			// If there is more than one value, and the last one is not a number, then assume it is a unit
+			if( innerInput.size() > 1 && !Tester.isDouble( innerInput.get( innerInput.size()-1 ) ) ) {
+
+				// Determine the units
+				Unit unit = Input.parseEntity( innerInput.get( innerInput.size()- 1), Unit.class );
+
+				// Determine the default units
+				Unit defaultUnit = Input.tryParseEntity( unitString.replaceAll("[()]", "").trim(), Unit.class );
+				if( defaultUnit == null ) {
+					throw new InputErrorException( "Could not determine default units " + unitString );
+				}
+
+				// Determine the conversion factor to the default units
+				double conversionFactor = unit.getConversionFactorToUnit( defaultUnit );
+
+				// Parse and convert the values
+				Input.assertCountRange(innerInput.subString(0,innerInput.size()-2), 1, 3);
+				temp = Input.parseDoubleVector(innerInput.subString(0,innerInput.size()-2), Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, conversionFactor);
+
+			}
+			else {
+				// Parse the values
+				Input.assertCountRange(innerInput, 1, 3);
+				temp = Input.parseDoubleVector(innerInput, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+				if( unitString.length() > 0 )
+					InputAgent.logWarning( "Missing units.  Assuming %s.", unitString );
+			}
+
+			// pad the vector to have 3 elements
+			while (temp.size() < 3) {
+				temp.add(0.0d);
+			}
+
+			tempValue.add(new Vector3d(temp.get(0), temp.get(1), temp.get(2)));
 		}
 
 		value = tempValue;
