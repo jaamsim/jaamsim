79158be317969b71b711e41bc7c27d6dacb26c00
Stephen Wong <stephen.wong@ausencosandwell.com>
2012-01-04 10:31:21 -0800

    JS: Added units to Vector3dInput parsing
    This change is required for allowing keywords like Position, Size, and Orientation to accept units
    
    Signed-off-by: Stephen Wong <stephen.wong@ausencosandwell.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausencosandwell.com>

--- com/sandwell/JavaSimulation/Vector3dInput.java
+++ com/sandwell/JavaSimulation/Vector3dInput.java
@@ -16,6 +16,8 @@ package com.sandwell.JavaSimulation;
 
 import javax.vecmath.Vector3d;
 
+import com.sandwell.JavaSimulation3D.InputAgent;
+
 public class Vector3dInput extends Input<Vector3d> {
 	private double minValue = Double.NEGATIVE_INFINITY;
 	private double maxValue = Double.POSITIVE_INFINITY;
@@ -26,8 +28,36 @@ public class Vector3dInput extends Input<Vector3d> {
 
 	public void parse(StringVector input)
 	throws InputErrorException {
-		Input.assertCountRange(input, 1, 3);
-		DoubleVector temp = Input.parseDoubleVector(input, minValue, maxValue);
+		DoubleVector temp;
+
+		// If there is more than one value, and the last one is not a number, then assume it is a unit
+		if( input.size() > 1 && !Tester.isDouble( input.get( input.size()-1 ) ) ) {
+
+			// Determine the units
+			Unit unit = Input.parseEntity( input.get( input.size()- 1), Unit.class );
+
+			// Determine the default units
+			Unit defaultUnit = Input.tryParseEntity( unitString.replaceAll("[()]", "").trim(), Unit.class );
+			if( defaultUnit == null ) {
+				throw new InputErrorException( "Could not determine default units " + unitString );
+			}
+
+			// Determine the conversion factor to the default units
+			double conversionFactor = unit.getConversionFactorToUnit( defaultUnit );
+
+			// Parse and convert the values
+			Input.assertCountRange(input.subString(0,input.size()-2), 1, 3);
+			temp = Input.parseDoubleVector(input.subString(0,input.size()-2), minValue, maxValue, conversionFactor);
+		}
+		else {
+			// Parse the values
+			Input.assertCountRange(input, 1, 3);
+			temp = Input.parseDoubleVector(input, minValue, maxValue);
+
+			if( unitString.length() > 0 )
+				InputAgent.logWarning( "Missing units.  Assuming %s.", unitString );
+		}
+
 		// pad the vector to have 3 elements
 		while (temp.size() < 3) {
 			temp.add(0.0d);
