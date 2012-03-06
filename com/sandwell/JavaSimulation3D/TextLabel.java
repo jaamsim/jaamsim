9230ed64a15b851064ce7d6378184c7216d6f615
Stephen Wong <stephen.wong@ausencosandwell.com>
2012-03-06 13:12:01 -0800

    JS3D: Added TextLabel keywords DropShadow and DropShadowColour
    The DropShadow keyword allows the user to easily add a drop shadow to a text label without having to define a second text label with an offset.
    
    DropShadow - if the value is true, then a drop shadow appears for the text label.  Default is false.
    
    DropShadowColour - the colour of the drop shadow.  Default is black.
    
    Example:
    
    SimulationTitle DropShadow { TRUE }  DropShadowColour { red }
    
    Signed-off-by: Stephen Wong <stephen.wong@ausencosandwell.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausencosandwell.com>

--- com/sandwell/JavaSimulation3D/TextLabel.java
+++ com/sandwell/JavaSimulation3D/TextLabel.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 
 import javax.vecmath.Vector3d;
 
+import com.sandwell.JavaSimulation.BooleanInput;
 import com.sandwell.JavaSimulation.ColourInput;
 import com.sandwell.JavaSimulation.DoubleInput;
 import com.sandwell.JavaSimulation.Input;
@@ -39,8 +40,11 @@ public class TextLabel extends DisplayEntity  {
 	private static final int defFont;
 	protected int fontStyle;
 	private final ColourInput fontColor;
+	private final BooleanInput dropShadow;
+	private final ColourInput dropShadowColor;
 
 	protected LabelShape reference;
+	protected LabelShape shadow;
 
 	static {
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -69,12 +73,20 @@ public class TextLabel extends DisplayEntity  {
 		this.addInput(fontColor, true, "FontColor");
 
 		addEditableKeyword( "FontStyle",        "", "Plain",    false, "Graphics" );
+
+		dropShadow = new BooleanInput( "DropShadow", "Graphics", false );
+		this.addInput( dropShadow, true );
+
+		dropShadowColor = new ColourInput("DropShadowColour", "Graphics", Shape.getPresetColor(Shape.COLOR_BLACK));
+		this.addInput(dropShadowColor, true, "DropShadowColor");
 	}
 
 	public TextLabel() {
 		fontStyle = Font.PLAIN;
 
 		reference = new LabelShape("", fontColor.getValue());
+		shadow = new LabelShape("", dropShadowColor.getValue());
+		this.getModel().addChild( shadow );
 		this.getModel().addChild( reference );
 	}
 
@@ -121,7 +133,9 @@ public class TextLabel extends DisplayEntity  {
 		if( in == text ||
 			in == textHeight ||
 			in == fontName ||
-			in == fontColor ) {
+			in == fontColor ||
+			in == dropShadow ||
+			in == dropShadowColor ) {
 			modelNeedsRender = true;
 		}
 	}
@@ -135,6 +149,20 @@ public class TextLabel extends DisplayEntity  {
 			reference.setFillColor(fontColor.getValue());
 			reference.setFont(fontName.getChoice(), fontStyle, 1);
 			reference.setText(getRenderText(time));
+
+			if( dropShadow.getValue() ) {
+				shadow.setHeight(textHeight.getValue());
+				shadow.setFillColor(dropShadowColor.getValue());
+				shadow.setFont(fontName.getChoice(), fontStyle, 1);
+				shadow.setText(getRenderText(time));
+
+				// Offset the shadow by 10% of the text height
+				double offset = textHeight.getValue() * 0.10;
+				shadow.setPos( -1.0 * offset, -1.0 * offset, 0.0 );
+			}
+			else {
+				shadow.setText("");
+			}
 
 			Vector3d tmp = new Vector3d();
 			reference.getSize(tmp);
