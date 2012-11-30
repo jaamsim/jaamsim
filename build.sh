4e3a79d1c632af0b3af01b5f7c21e33e4d6a94e4
Harvey Harrison <harvey.harrison@ausenco.com>
2012-11-29 16:53:53 -0800

    JS: update the dumb build script for new file locations
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

--- build.sh
+++ build.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 #
-# Build script for a JaamSim jarfile including the Java3D classes
+# Build script for a JaamSim jarfile
 # Created Dec 9, 2011 by Harvey Harrison
 #
-
 JARNAME="JaamSim$1.jar"
 LOGFILE="build.log"
-SIMSOURCE="com/sandwell/JavaSimulation/*.java com/sandwell/JavaSimulation3D/*.java com/sandwell/JavaSimulation3D/util/*.java"
-EXTSOURCE="com/eteks/sweethome3d/*/*.java"
 
-BUILDDIR="build/"
-RESFILES="resources/"
-BLDFILES="-C $BUILDDIR ."
+CLASSES=" -C build/ ."
+RESFILES="-C src/main/resources/ ."
+
+BUILDDIR="-d build/"
+CLASSPATH="-classpath jar/;jar/*"
 
 # Clean out any build products from old builds
 # note we don't use git-clean here to avoid removing temp files we have in the
 # source tree that people might want to keep.
-rm -rf $BUILDDIR
-mkdir $BUILDDIR
+if [ ! -d build ]
+then
+	mkdir build
+else
+	rm -rf build/*
+fi
 
 # Move old build files out of the way
 if [ -e "$LOGFILE" ]
@@ -26,8 +29,7 @@ then
 fi
 
 # Compile
-javac -d $BUILDDIR -Xmaxwarns 100000 -Xlint -classpath lib/ $SIMSOURCE $EXTSOURCE > "$LOGFILE" 2>&1
-cp -r lib/* $BUILDDIR
+javac -Xmaxwarns 100000 -Xlint $CLASSPATH $BUILDDIR src/main/java/com/sandwell/*/*.java src/main/java/com/jaamsim/*/*.java src/main/java/com/jaamsim/render/util/*.java > "$LOGFILE" 2>&1
 
 # If there were any errors during compilation, complain
 # test for a non-zero error code
@@ -41,7 +43,7 @@ fi
 
 # Package
 rm -f $JARNAME
-jar cfe $JARNAME com.sandwell.JavaSimulation3D.GUIFrame $RESFILES $BLDFILES
+jar cfe $JARNAME com.sandwell.JavaSimulation3D.GUIFrame $CLASSES $RESFILES
 
 # If there were any errors while producing the jar file, complain
 # test for a non-zero error code
