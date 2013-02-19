e250d88e4763516f7006603e4def965ce868aea8
Geoffrey Savinkoff <Geoffrey.Savinkoff@ausenco.com>
2013-02-19 13:54:04 -0800

    JS3D: BooleanIndicator class
    Graphic representation of a boolean property.
    
    Signed-off-by: Geoffrey Savinkoff <Geoffrey.Savinkoff@ausenco.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

---
+++ src/main/java/com/sandwell/JavaSimulation3D/BooleanIndicator.java
@@ -0,0 +1,43 @@
+package com.sandwell.JavaSimulation3D;
+
+import com.sandwell.JavaSimulation.ColourInput;
+import com.sandwell.JavaSimulation.Keyword;
+
+public class BooleanIndicator extends DisplayEntity {
+
+	private PropertyReader propReader;
+
+	@Keyword(desc = "The colour of the indicator when the property is true",
+	         example = "BinLevel TrueColor { green }")
+	private final ColourInput trueColor;
+
+	@Keyword(desc = "The colour of the indicator when the property is false",
+	         example = "BinLevel TrueColor { green }")
+	private final ColourInput falseColor;
+
+	{
+		propReader = new PropertyReader();
+		addInputGroup(propReader);
+
+		trueColor = new ColourInput("TrueColour", "Key Inputs", ColourInput.GREEN);
+		this.addInput(trueColor, true, "TrueColor");
+
+		falseColor = new ColourInput("FalseColour", "Key Inputs", ColourInput.RED);
+		this.addInput(falseColor, true, "FalseColor");
+	}
+
+	public BooleanIndicator() {
+	}
+
+	public void updateGraphics( double time ) {
+
+		String val = propReader.getPropertyValueString(time);
+		if(val.equals( "true" ) ) {
+			setTagColour(DisplayModelCompat.TAG_CONTENTS, trueColor.getValue());
+		}
+		else {
+			setTagColour(DisplayModelCompat.TAG_CONTENTS, falseColor.getValue());
+		}
+	}
+
+}
