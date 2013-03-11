6d8228356e525619becac226fb416cb76fa397c3
Harry King <harry.king@ausenco.com>
2013-03-10 17:16:55 -0700

    JS: User-defined probability distributions
    DiscreteDistribution and ContinuousDistribution
    
    Signed-off-by: Harry King <harry.king@ausenco.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

---
+++ src/main/java/com/jaamsim/ProbabilityDistributions/ContinuousDistribution.java
@@ -0,0 +1,121 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2013 Ausenco Engineering Canada Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package com.jaamsim.ProbabilityDistributions;
+
+import com.sandwell.JavaSimulation.DoubleListInput;
+import com.sandwell.JavaSimulation.DoubleVector;
+import com.sandwell.JavaSimulation.InputErrorException;
+import com.sandwell.JavaSimulation.Keyword;
+
+/**
+ * ContinuousDistribution is a user-defined probability distribution that selects an output value based on an ordered list
+ * of values and cumulative probabilities.  The inputs specify a continuous cumulative probability distribution by
+ * linearly interpolating between the given values and cumulative probabilities.
+ * @author Harry King
+ *
+ */
+public class ContinuousDistribution extends NewProbabilityDistribution {
+
+	@Keyword(desc = "The list of values for the user-defined cumulative probability distribution.",
+	         example = "ContinuousDist-1 ValueList { 2.0  4.3  8.9 }")
+	private final DoubleListInput valueListInput;
+
+	@Keyword(desc = "The list of cumulative probabilities corresponding to the values in the ValueList.  " +
+			"The cumulative probabilities must be given in increasing order.  The first value must be exactly 0.0.  " +
+			"The last value must be exactly 1.0.",
+	         example = "ContinuousDist-1 CumulativeProbabilityList { 0.0  0.6  1.0 }")
+	private final DoubleListInput cumulativeProbabilityListInput;
+
+	{
+		valueListInput = new DoubleListInput( "ValueList", "Key Inputs", null);
+		this.addInput( valueListInput, true);
+
+		cumulativeProbabilityListInput = new DoubleListInput( "CumulativeProbabilityList", "Key Inputs", null);
+		this.addInput( cumulativeProbabilityListInput, true);
+	}
+
+	@Override
+	public void validate() {
+		super.validate();
+
+		// The number of entries in the ValueList and CumulativeProbabilityList inputs must match
+		if( cumulativeProbabilityListInput.getValue().size() != valueListInput.getValue().size() ) {
+			throw new InputErrorException( "The number of entries for CumulativeProbabilityList and ValueList must be equal" );
+		}
+
+		// The first entry in the CumulativeProbabilityList must be 0.0
+		if( cumulativeProbabilityListInput.getValue().get(0) != 0.0 ) {
+			throw new InputErrorException( "The first entry for CumulativeProbabilityList must be exactly 0.0" );
+		}
+
+		// The last entry in the CumulativeProbabilityList must be 1.0
+		int n = cumulativeProbabilityListInput.getValue().size();
+		if( cumulativeProbabilityListInput.getValue().get(n-1) != 1.0 ) {
+			throw new InputErrorException( "The last entry for CumulativeProbabilityList must be exactly 1.0" );
+		}
+
+		// The entries in the CumulativeProbabilityList must be given in increasing order
+		double last = -1.0;
+		for( int i=0; i<n; i++) {
+			if( cumulativeProbabilityListInput.getValue().get(i) <= last ) {
+				throw new InputErrorException( "The entries for CumulativeProbabilityList must be given in increasing order" );
+			}
+			last = cumulativeProbabilityListInput.getValue().get(i);
+		}
+	}
+
+	@Override
+	protected double getNextNonZeroSample() {
+
+		double rand = randomGenerator1.nextDouble();
+		DoubleVector cumList = cumulativeProbabilityListInput.getValue();
+		for( int i=1; i<cumList.size(); i++) {
+			if( rand < cumList.get(i) ) {
+				double cum = cumList.get(i);
+				double lastCum = cumList.get(i-1);
+				double val = valueListInput.getValue().get(i);
+				double lastVal = valueListInput.getValue().get(i-1);
+				return lastVal + (rand-lastCum)*(val-lastVal)/(cum-lastCum);
+			}
+		}
+		return valueListInput.getValue().get( cumList.size()-1 );
+	}
+
+	@Override
+	protected double getMeanValue() {
+		double sum = 0.0;
+		DoubleVector cumList = cumulativeProbabilityListInput.getValue();
+		DoubleVector valueList = valueListInput.getValue();
+		for( int i=1; i<cumList.size(); i++) {
+			sum += ( cumList.get(i) - cumList.get(i-1) ) * ( valueList.get(i) + valueList.get(i-1) );
+		}
+		return 0.5 * sum;
+	}
+
+	@Override
+	protected double getStandardDeviation() {
+		double sum = 0.0;
+		DoubleVector cumList = cumulativeProbabilityListInput.getValue();
+		DoubleVector valueList = valueListInput.getValue();
+		for( int i=1; i<cumList.size(); i++) {
+			double val = valueList.get(i);
+			double lastVal = valueList.get(i-1);
+			sum += ( cumList.get(i) - cumList.get(i-1) ) * ( val*val + val*lastVal + lastVal*lastVal );
+		}
+
+		double mean = getMeanValue();
+		return  Math.sqrt( sum/3.0 - (mean * mean) );
+	}
+}
---
+++ src/main/java/com/jaamsim/ProbabilityDistributions/DiscreteDistribution.java
@@ -0,0 +1,97 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2013 Ausenco Engineering Canada Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package com.jaamsim.ProbabilityDistributions;
+
+import com.sandwell.JavaSimulation.DoubleListInput;
+import com.sandwell.JavaSimulation.DoubleVector;
+import com.sandwell.JavaSimulation.InputErrorException;
+import com.sandwell.JavaSimulation.Keyword;
+
+/**
+ * DiscreteDistribution is a user-defined probability distribution that selects from a given list of specific values
+ * based on a specified probability for each value.  No interpolation is performed between values.
+ * @author Harry King
+ *
+ */
+public class DiscreteDistribution extends NewProbabilityDistribution {
+
+	@Keyword(desc = "The list of discrete values that can be returned by the distribution.  " +
+			"The values can be any positive or negative and can be listed in any order.  " +
+			"No interpolation is performed between these values.",
+	         example = "DiscreteDist-1 ValueList { 6.2 10.1 }")
+	private final DoubleListInput valueListInput;
+
+	@Keyword(desc = "The list of probabilities corresponding to the discrete values in the ValueList.  Must sum to 1.0.",
+	         example = "DiscreteDist-1 ProbabilityList { 0.3  0.7 }")
+	private final DoubleListInput probabilityListInput;
+
+	{
+		valueListInput = new DoubleListInput( "ValueList", "Key Inputs", null);
+		this.addInput( valueListInput, true);
+
+		probabilityListInput = new DoubleListInput( "ProbabilityList", "Key Inputs", null);
+		this.addInput( probabilityListInput, true);
+	}
+
+	@Override
+	public void validate() {
+		super.validate();
+
+		// The number of entries in the ValueList and ProbabilityList inputs must match
+		if( probabilityListInput.getValue().size() != valueListInput.getValue().size() ) {
+			throw new InputErrorException( "The number of entries for ProbabilityList and ValueList must be equal" );
+		}
+
+		// The entries in the ProbabilityList must sum to 1.0
+		if( Math.abs( probabilityListInput.getValue().sum() - 1.0 ) > 1.0e-10 ) {
+			throw new InputErrorException( "The entries in the ProbabilityList must sum to 1.0" );
+		}
+	}
+
+	@Override
+	protected double getNextNonZeroSample() {
+
+		double rand = randomGenerator1.nextDouble();
+		double cumProb = 0.0;
+		DoubleVector probList = probabilityListInput.getValue();
+		for( int i=0; i<probList.size(); i++) {
+			cumProb += probList.get(i);
+			if( rand <= cumProb ) {
+				return valueListInput.getValue().get(i);
+			}
+		}
+		return valueListInput.getValue().get( probList.size()-1 );
+	}
+
+	@Override
+	protected double getMeanValue() {
+		double ret = 0.0;
+		for( int i=0; i<probabilityListInput.getValue().size(); i++) {
+			ret += probabilityListInput.getValue().get(i) * valueListInput.getValue().get(i);
+		}
+		return ret;
+	}
+
+	@Override
+	protected double getStandardDeviation() {
+		double sum = 0.0;
+		for( int i=0; i<probabilityListInput.getValue().size(); i++) {
+			double val = valueListInput.getValue().get(i);
+			sum += probabilityListInput.getValue().get(i) * val * val;
+		}
+		double mean = getMeanValue();
+		return  Math.sqrt( sum - (mean * mean) );
+	}
+}
