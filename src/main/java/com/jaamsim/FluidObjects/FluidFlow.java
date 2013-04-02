67be70ffb3f5286472c7688cd712ca98182853f8
Harry King <harry.king@ausenco.com>
2013-04-01 21:46:50 -0700

    JS: Make FluidFlow a sub-class of FluidFlowCalculation
    Signed-off-by: Harry King <harry.king@ausenco.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

--- src/main/java/com/jaamsim/FluidObjects/FluidFlow.java
+++ src/main/java/com/jaamsim/FluidObjects/FluidFlow.java
@@ -15,52 +15,23 @@
 package com.jaamsim.FluidObjects;
 
 import java.util.ArrayList;
-import com.jaamsim.CalculationObjects.CalculationEntity;
 import com.jaamsim.input.Output;
-import com.sandwell.JavaSimulation.EntityInput;
-import com.jaamsim.units.VolumeFlowUnit;
 import com.sandwell.JavaSimulation.InputErrorException;
 import com.sandwell.JavaSimulation.ErrorException;
-import com.sandwell.JavaSimulation.Keyword;
 
 /**
  * FluidFlow tracks the flow rate between a source and a destination.
  * @author Harry King
  *
  */
-public class FluidFlow extends CalculationEntity {
+public class FluidFlow extends FluidFlowCalculation {
 
-	@Keyword(desc = "The Fluid being moved by the flow.",
-	      example = "FluidFlow1 Fluid { Fluid1 }")
-	private final EntityInput<Fluid> fluidInput;
-
-	@Keyword(desc = "The source for the flow.",
-	      example = "FluidFlow1 Source { Tank1 }")
-	private final EntityInput<FluidComponent> sourceInput;
-
-	@Keyword(desc = "The destination for the flow.",
-	      example = "FluidFlow1 Destination { Tank1 }")
-	private final EntityInput<FluidComponent> destinationInput;
-
-	private double flowRate;  // The volumetric flow rate (m3/s) for the route.
 	private double flowAcceleration;  // The rate of change of the volumetric flow rate with respect to time (m3/s2).
-	private double lastUpdateTime;  // The time at which the last update was performed.
 
 	private ArrayList<FluidComponent> routeList;  // A list of the hydraulic components in the flow, from source to destination.
 	private double totalFlowInertia;  // The sum of Density x Length / FlowArea for the hydraulic components in the route.
 	private double destinationBaseInletPressure;  // The base pressure at the destination's inlet.
 	private double destinationTargetInletPressure;  // The desired inlet pressure at the destination's inlet.
-
-	{
-		fluidInput = new EntityInput<Fluid>( Fluid.class, "Fluid", "Key Inputs", null);
-		this.addInput( fluidInput, true);
-
-		sourceInput = new EntityInput<FluidComponent>( FluidComponent.class, "Source", "Key Inputs", null);
-		this.addInput( sourceInput, true);
-
-		destinationInput = new EntityInput<FluidComponent>( FluidComponent.class, "Destination", "Key Inputs", null);
-		this.addInput( destinationInput, true);
-	}
 
 	public FluidFlow() {
 		routeList = new ArrayList<FluidComponent>();
@@ -70,18 +41,13 @@ public class FluidFlow extends CalculationEntity {
 	public void validate() {
 		super.validate();
 
-		// Confirm that the fluid has been specified
-		if( fluidInput.getValue() == null ) {
-			throw new InputErrorException( "The keyword Fluid must be set." );
-		}
-
 		// Confirm that the source has been specified
-		if( sourceInput.getValue() == null ) {
+		if( this.getSource() == null ) {
 			throw new InputErrorException( "The keyword Source must be set." );
 		}
 
 		// Confirm that the destination has been specified
-		if( destinationInput.getValue() == null ) {
+		if( this.getDestination() == null ) {
 			throw new InputErrorException( "The keyword Destination must be set." );
 		}
 	}
@@ -90,13 +56,11 @@ public class FluidFlow extends CalculationEntity {
 	public void earlyInit() {
 		super.earlyInit();
 
-		flowRate = 0.0;
 		flowAcceleration = 0.0;
-		lastUpdateTime = 0.0;
 
 		// Construct the list of hydraulic components in the flow path
 		routeList.clear();
-		routeList.add( destinationInput.getValue() );
+		routeList.add( this.getDestination() );
 		FluidComponent prev = routeList.get(0).getPrevious();
 		while( prev != null ) {
 			routeList.add(0, prev);
@@ -104,7 +68,7 @@ public class FluidFlow extends CalculationEntity {
 		}
 
 		// Confirm that the first component is the source
-		if( routeList.get(0) != sourceInput.getValue() ) {
+		if( routeList.get(0) != this.getSource() ) {
 			throw new InputErrorException( "The source of the route is not connected to the destination by the 'Previous' keyword inputs for the individual components." );
 		}
 
@@ -123,26 +87,10 @@ public class FluidFlow extends CalculationEntity {
 	}
 
 	@Override
-	public void update() {
-
-		// Calculate the elapsed time
-		double t = 3600.0 * this.getCurrentTime();  // convert from hours to seconds
-		double dt = t - lastUpdateTime;
-		lastUpdateTime = t;
-
-		// Update the volume stored at the source and destination
-		double dV = flowRate * dt;
-		if( dV > 0.0 ) {
-			dV = Math.min( dV, sourceInput.getValue().getFluidVolume() );
-		}
-		else {
-			dV = - Math.min( - dV, destinationInput.getValue().getFluidVolume() );
-		}
-		sourceInput.getValue().addVolume( -dV );
-		destinationInput.getValue().addVolume( dV );
+	protected void calcFlowRate( FluidComponent source, FluidComponent destination, double dt ) {
 
 		// Update the flow rate
-		flowRate += flowAcceleration * dt;
+		this.setFlowRate( this.getFlowRate() + flowAcceleration * dt );
 
 		// Update the flow velocity and base pressures in each component of the flow route
 		// (base pressure ignores the affect of acceleration)
@@ -153,8 +101,8 @@ public class FluidFlow extends CalculationEntity {
 		}
 
 		// Update the flow acceleration
-		destinationBaseInletPressure = destinationInput.getValue().getBaseInletPressure();
-		destinationTargetInletPressure = destinationInput.getValue().getTargetInletPressure();
+		destinationBaseInletPressure = destination.getBaseInletPressure();
+		destinationTargetInletPressure = destination.getTargetInletPressure();
 		flowAcceleration = ( destinationBaseInletPressure
 				- destinationTargetInletPressure ) / totalFlowInertia;
 
@@ -165,26 +113,11 @@ public class FluidFlow extends CalculationEntity {
 		}
 
 		// Confirm that the pressure is now balanced
-		double diff = destinationInput.getValue().getInletPressure() /
-				destinationInput.getValue().getTargetInletPressure() - 1.0;
+		double diff = destination.getInletPressure() /
+				destination.getTargetInletPressure() - 1.0;
 		if( Math.abs( diff ) > 1.0e-4 ) {
 			throw new ErrorException( "Pressure did not balance correctly.  Difference = " + diff );
 		}
-	}
-
-	public double getFlowRate() {
-		return flowRate;
-	}
-
-	public Fluid getFluid() {
-		return fluidInput.getValue();
-	}
-
-	@Output(name = "FlowRate",
-	 description = "The volumetric flow rate for the system.",
-	        unit = VolumeFlowUnit.class)
-	public double getFlowRate( double simTime ) {
-		return flowRate;
 	}
 
 	@Output(name = "FlowAcceleration",
