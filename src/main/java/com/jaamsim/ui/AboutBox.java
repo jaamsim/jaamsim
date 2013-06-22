f11607542b6504e27228ba865079d2064effb502
Harvey Harrison <harvey.harrison@ausenco.com>
2013-06-21 17:19:36 -0700

    JS: update version to 2013-25
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

--- src/main/java/com/jaamsim/ui/AboutBox.java
+++ src/main/java/com/jaamsim/ui/AboutBox.java
@@ -57,7 +57,7 @@ public class AboutBox extends FrameBox implements ActionListener {
 		getContentPane().add( lab );
 
 		// display model info
-		lab = new JLabel("Version: 2013-24");
+		lab = new JLabel("Version: 2013-25");
 		constraints.gridy = index++;
 		constraints.insets = new Insets( 0, 75, 0, 75 );
 		layout.setConstraints( lab, constraints );
