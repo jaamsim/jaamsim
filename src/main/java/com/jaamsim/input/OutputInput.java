dd7166eaf8cde2796787e1f15edd95b821fe8042
Harry King <harry.king@ausenco.com>
2013-07-30 12:21:20 -0700

    JS: Allow a chain of Outputs to be specified in OutputInput
    Signed-off-by: Harry King <harry.king@ausenco.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

--- src/main/java/com/jaamsim/input/OutputInput.java
+++ src/main/java/com/jaamsim/input/OutputInput.java
@@ -22,9 +22,10 @@ import com.sandwell.JavaSimulation.StringVector;
 public class OutputInput<T> extends Input<String> {
 
 	private Class<T> klass;
-	private Entity ent;
-	private String outputName;
-	private OutputHandle out;
+	private Entity ent;  // The Entity against which to apply the first Output name
+	private String outputName;  // The first Output name in the chain
+	private OutputHandle out;  // The OutputHandle for the first Output in the chain
+	private StringVector outputNameList;  // The names of the second, third, etc. Outputs in the chain.
 
 	public OutputInput(Class<T> klass, String key, String cat, String def) {
 		super(key, cat, def);
@@ -33,23 +34,61 @@ public class OutputInput<T> extends Input<String> {
 
 	@Override
 	public void parse(StringVector input) throws InputErrorException {
-		Input.assertCount(input, 2);
+
+		if( input.isEmpty() ) {
+			ent = null;
+			outputName = "";
+			out = null;
+			outputNameList.clear();
+			this.updateEditingFlags();
+			return;
+		}
+
+		Input.assertCountRange(input, 2, Integer.MAX_VALUE);
 
 		ent = Input.parseEntity(input.get(0), Entity.class);
 		outputName = input.get(1);
 		out = ent.getOutputHandle(outputName);
 
+		outputNameList = new StringVector();
+		if( input.size() > 2 )
+			outputNameList = input.subString(2, input.size()-1);
+
 		Class<?> retClass = out.getReturnType();
-		if (!klass.isAssignableFrom(retClass)) {
-			throw new InputErrorException("OutputInput class mismatch. Expected: %s, got: %s", klass.toString(), retClass.toString());
+		if( input.size() == 2 ) {
+			if ( klass != Object.class && !klass.isAssignableFrom(retClass))
+				throw new InputErrorException("OutputInput class mismatch. Expected: %s, got: %s", klass.toString(), retClass.toString());
+		}
+		else {
+			if (!(Entity.class).isAssignableFrom(retClass))
+				throw new InputErrorException("OutputInput class mismatch. The first output in the output chain must return an Entity");
 		}
 
 		value = String.format("%s.%s", ent.getInputName(), outputName);
+		if( input.size() > 2 ) {
+			for( String name: outputNameList ) {
+				value += "." + name;
+			}
+		}
 		this.updateEditingFlags();
 	}
 
+	public OutputHandle getOutputHandle(double simTime) {
+		OutputHandle o = out;
+		for( String name : outputNameList ) {
+			Entity e = o.getValue(simTime, Entity.class);
+			if( e == null || !e.hasOutput(name) )
+				return null;
+			o = e.getOutputHandle(name);
+		}
+		return o;
+	}
+
 	public T getOutputValue(double simTime) {
-		return out.getValue(simTime, klass);
+		OutputHandle o = this.getOutputHandle(simTime);
+		if( o == null )
+			return null;
+		return o.getValue(simTime, klass);
 	}
 
 }
