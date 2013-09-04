ae5f0f812295333aced775297eca411cf1883d99
Geoffrey Savinkoff <Geoffrey.Savinkoff@ausenco.com>
2013-09-04 11:51:45 -0700

    JS: Add TimeSeriesInput class
    Modelled after SampleInput.  Can accept a constant or a time series.
    
    Signed-off-by: Geoffrey Savinkoff <Geoffrey.Savinkoff@ausenco.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

---
+++ src/main/java/com/sandwell/JavaSimulation/TimeSeriesInput.java
@@ -0,0 +1,54 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2013 Ausenco Engineering Canada Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package com.sandwell.JavaSimulation;
+
+import com.jaamsim.units.Unit;
+import com.jaamsim.units.UserSpecifiedUnit;
+
+public class TimeSeriesInput extends Input<TimeSeriesProvider> {
+	private Class<? extends Unit> unitType;
+
+	public TimeSeriesInput(String key, String cat, TimeSeriesProvider def) {
+		super(key, cat, def);
+		unitType = null;
+	}
+
+	public void setUnitType(Class<? extends Unit> u) {
+		unitType = u;
+	}
+
+	@Override
+	public void parse(StringVector input)
+	throws InputErrorException {
+		Input.assertCountRange(input, 1, 2);
+
+		// Try to parse as a constant value
+		try {
+			DoubleVector tmp = Input.parseDoubles(input, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, unitType);
+			value = new TimeSeriesConstantDouble(unitType, tmp.get(0));
+			this.updateEditingFlags();
+			return;
+		}
+		catch (InputErrorException e) {}
+
+		// If not a constant, try parsing a TimeSeriesProvider
+		Entity ent = Input.parseEntity(input.get(0), Entity.class);
+		TimeSeriesProvider s = Input.castImplements(ent, TimeSeriesProvider.class);
+		if( s.getUnitType() != UserSpecifiedUnit.class )
+			Input.assertUnitsMatch(unitType, s.getUnitType());
+		value = s;
+		this.updateEditingFlags();
+	}
+}
