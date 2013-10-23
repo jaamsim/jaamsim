4639bbcdca96aaec217af0d79322ed1d2418c555
Matt Chudleigh <matt.chudleigh@ausenco.com>
2013-10-23 16:16:36 -0700

    JS: Rudimentary binary file converter utility
    This class functions as a simple command line utility to convert collada
    files to our binary format.
    
    Signed-off-by: Matt Chudleigh <matt.chudleigh@ausenco.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

---
+++ src/main/java/com/jaamsim/MeshFiles/BinaryExporter.java
@@ -0,0 +1,31 @@
+package com.jaamsim.MeshFiles;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+import com.jaamsim.collada.ColParser;
+
+public class BinaryExporter {
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Usage: BinaryExporter [collada-input-filename] [jsb-binary-output-filename]");
+			return;
+		}
+
+		String inputName = args[0];
+		String outputName = args[1];
+
+		try {
+			ColParser.setKeepData(true);
+			MeshData data = ColParser.parse(new URL("file:///" + inputName));
+			DataBlock block = data.getDataAsBlock();
+			File outFile = new File(outputName);
+			FileOutputStream outStream = new FileOutputStream(outFile);
+			BlockWriter.writeBlock(outStream, block);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
