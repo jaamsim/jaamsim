161645c910884ff65cf17949a42c9b31087291fc
Harry King <harry.king@ausenco.com>
2013-12-01 23:16:55 -0800

    JS: Add object Branch
    Signed-off-by: Harry King <harry.king@ausenco.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

---
+++ src/main/java/com/jaamsim/BasicObjects/Branch.java
@@ -0,0 +1,73 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2013 Ausenco Engineering Canada Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package com.jaamsim.BasicObjects;
+
+import com.jaamsim.Samples.SampleInput;
+import com.jaamsim.units.DimensionlessUnit;
+import com.sandwell.JavaSimulation.EntityListInput;
+import com.sandwell.JavaSimulation.InputErrorException;
+import com.sandwell.JavaSimulation.Keyword;
+import com.sandwell.JavaSimulation3D.DisplayEntity;
+
+public class Branch extends LinkedComponent {
+
+	@Keyword(description = "The list of possible next objects to which the processed DisplayEntity can be passed.",
+	         example = "Branch-1 NextComponentList { Server-1 Server-2 }")
+	protected final EntityListInput<LinkedComponent> nextComponentList;
+
+	@Keyword(description = "A number that determines the choice of next component:\n" +
+			"     1 = first branch, 2 = second branch, etc.\n" +
+			"A constant value, a distribution to be sampled, or a time series can be entered.",
+	         example = "Branch-1 Choice { 2 }")
+	private final SampleInput choice;
+
+	{
+		nextComponentInput.setHidden(true);
+
+		nextComponentList = new EntityListInput<LinkedComponent>( LinkedComponent.class, "NextComponentList", "Key Inputs", null);
+		this.addInput( nextComponentList, true);
+
+		choice = new SampleInput( "Choice", "Key Inputs", null);
+		choice.setUnitType( DimensionlessUnit.class );
+		this.addInput( choice, true);
+	}
+
+	@Override
+	public void validate() {
+		super.validate();
+
+		// Confirm that at least one next component has been specified
+		if( nextComponentList.getValue() == null ) {
+			throw new InputErrorException( "The keyword NextComponentList must be set." );
+		}
+
+		// Confirm that a choice input has been specified
+		if( choice.getValue() == null ) {
+			throw new InputErrorException( "The keyword Choice must be set." );
+		}
+	}
+
+	@Override
+	public void addDisplayEntity( DisplayEntity ent ) {
+		super.addDisplayEntity(ent);
+
+		// Choose the next component for this entity
+		int i = (int) choice.getValue().getNextSample(this.getSimTime());
+
+		// Pass the entity to the next component
+		nextComponentList.getValue().get(i-1).addDisplayEntity(ent);
+	}
+
+}
