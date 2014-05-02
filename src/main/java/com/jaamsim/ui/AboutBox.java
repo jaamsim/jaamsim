74f90f95f31b802681042da599b918485852a373
Harvey Harrison <harvey.harrison@ausenco.com>
2014-05-02 11:05:55 -0700

    JS: update version to 2014-18
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

--- src/main/java/com/jaamsim/ui/AboutBox.java
+++ src/main/java/com/jaamsim/ui/AboutBox.java
@@ -32,7 +32,7 @@ import com.sandwell.JavaSimulation.Simulation;
 public class AboutBox extends FrameBox implements ActionListener {
 	private static AboutBox instance;
 
-	public static final String version = "2014-17";
+	public static final String version = "2014-18";
 	public static final String copyright = "Copyright (C) 2014 Ausenco Engineering Canada Inc.";
 
 	public AboutBox() {
