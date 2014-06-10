eca7ca56d3136151d64b809536127da88984b466
Harvey Harrison <harvey.harrison@ausenco.com>
2014-06-10 06:52:55 -0700

    JS: remove the StringVector use from InputAgent, no callers remain of this helper
    Signed-off-by: Harvey Harrison <harvey.harrison@ausenco.com>

--- src/main/java/com/jaamsim/input/InputAgent.java
+++ src/main/java/com/jaamsim/input/InputAgent.java
@@ -44,7 +44,6 @@ import com.sandwell.JavaSimulation.Group;
 import com.sandwell.JavaSimulation.InputErrorException;
 import com.sandwell.JavaSimulation.ObjectType;
 import com.sandwell.JavaSimulation.Simulation;
-import com.sandwell.JavaSimulation.StringVector;
 import com.sandwell.JavaSimulation3D.GUIFrame;
 
 public class InputAgent {
@@ -1267,40 +1266,6 @@ public class InputAgent {
 		return inputs;
 	}
 
-
-	/**
-	 * Expects a StringVector of one of two forms:
-	 * 1.  { entry entry entry } { entry entry entry }
-	 * 2.  entry entry entry
-	 * If format 1, returns a vector of stringvectors without braces.
-	 * if format 2, returns a vector of a stringvector, size1.
-	 * @param data
-	 * @return
-	 */
-	public static ArrayList<StringVector> splitStringVectorByBraces(StringVector data) {
-		ArrayList<StringVector> newData = new ArrayList<StringVector>();
-		for (int i=0; i < data.size(); i++) {
-
-			//skip over opening brace if present
-			if (data.get(i).equals("{") )
-				continue;
-
-			StringVector cmd = new StringVector();
-
-			//iterate until closing brace, or end of entry
-			for (int j = i; j < data.size(); j++, i++){
-				if (data.get(j).equals("}"))
-					break;
-
-				cmd.add(data.get(j));
-			}
-
-			//add to vector
-			newData.add(cmd);
-		}
-
-		return newData;
-	}
 
 	/**
 	 * Converts a file path String to a URI.
--- src/main/java/com/jaamsim/input/KeyedVec3dInput.java
+++ src/main/java/com/jaamsim/input/KeyedVec3dInput.java
@@ -22,7 +22,6 @@ import com.jaamsim.units.TimeUnit;
 import com.jaamsim.units.Unit;
 import com.sandwell.JavaSimulation.DoubleVector;
 import com.sandwell.JavaSimulation.InputErrorException;
-import com.sandwell.JavaSimulation.StringVector;
 
 public class KeyedVec3dInput extends Input<Vec3d> {
 	private Class<? extends Unit> unitType = DimensionlessUnit.class;
@@ -68,13 +67,8 @@ public class KeyedVec3dInput extends Input<Vec3d> {
 			throw new InputErrorException("Value entry not formated correctly: %s", valInput.toString());
 		}
 
-		StringVector temp = new StringVector();
-		temp.addAll(timeInput.subList(1, 3));
-		DoubleVector time = Input.parseDoubles(temp, 0.0d, Double.POSITIVE_INFINITY, TimeUnit.class);
-
-		temp.clear();
-		temp.addAll(valInput.subList(1, 5));
-		DoubleVector vals = Input.parseDoubles(temp, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, unitType);
+		DoubleVector time = Input.parseDoubles(timeInput.subList(1, 3), 0.0d, Double.POSITIVE_INFINITY, TimeUnit.class);
+		DoubleVector vals = Input.parseDoubles(valInput.subList(1, 5), Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, unitType);
 		Vec3d val = new Vec3d(vals.get(0), vals.get(1), vals.get(2));
 		curve.addKey(time.get(0), val);
 	}
