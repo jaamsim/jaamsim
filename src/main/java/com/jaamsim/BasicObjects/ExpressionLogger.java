02adb56b2e11539bd446b5fb18efe74ca5752f72
Harry King <harry.king@ausenco.com>
2015-07-18 20:34:17 -0700

    JS: create new report object "ExpressionLogger"
    Signed-off-by: Harry King <harry.king@ausenco.com>
    Signed-off-by: Stephen Wong <stephen.wong@ausenco.com>

---
+++ src/main/java/com/jaamsim/BasicObjects/ExpressionLogger.java
@@ -0,0 +1,176 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2015 Ausenco Engineering Canada Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package com.jaamsim.BasicObjects;
+
+import java.util.ArrayList;
+
+import com.jaamsim.Graphics.DisplayEntity;
+import com.jaamsim.Samples.SampleExpListInput;
+import com.jaamsim.Samples.SampleProvider;
+import com.jaamsim.basicsim.EntityTarget;
+import com.jaamsim.basicsim.FileEntity;
+import com.jaamsim.events.ProcessTarget;
+import com.jaamsim.input.Input;
+import com.jaamsim.input.InputAgent;
+import com.jaamsim.input.Keyword;
+import com.jaamsim.input.UnitTypeListInput;
+import com.jaamsim.input.ValueInput;
+import com.jaamsim.units.TimeUnit;
+import com.jaamsim.units.Unit;
+import com.jaamsim.units.UserSpecifiedUnit;
+
+public class ExpressionLogger extends DisplayEntity {
+	private FileEntity file;
+	private ArrayList<String> columnFormats;
+
+	@Keyword(description = "The unit types for the quantities being logged.",
+	         exampleList = {"DistanceUnit  SpeedUnit"})
+	private final UnitTypeListInput unitTypeListInput;
+
+	@Keyword(description = "One or more sources of data to be logged.\n"
+			+ "Each source is specified by an Expression. Also acceptable are: "
+			+ "a constant value, a Probability Distribution, TimeSeries, or a "
+			+ "Calculation Object.",
+	         exampleList = {"{ [Entity1].Output1 } { [Entity2].Output2 }"})
+	protected final SampleExpListInput dataSource;
+
+	@Keyword(description = "The interval between entries in the log file.",
+	         exampleList = { "24.0 h" })
+	private final ValueInput interval;
+
+	{
+		unitTypeListInput = new UnitTypeListInput("UnitTypeList", "Key Inputs", null);
+		unitTypeListInput.setRequired(true);
+		this.addInput(unitTypeListInput);
+
+		dataSource = new SampleExpListInput("DataSource", "Key Inputs", null);
+		dataSource.setUnitType(UserSpecifiedUnit.class);
+		dataSource.setEntity(this);
+		dataSource.setRequired(true);
+		this.addInput(dataSource);
+
+		interval = new ValueInput("Interval", "Key Inputs", null);
+		interval.setUnitType(TimeUnit.class);
+		interval.setValidRange(1.0e-10, Double.POSITIVE_INFINITY);
+		interval.setRequired(true);
+		this.addInput(interval);
+	}
+
+	public ExpressionLogger() {
+		columnFormats = new ArrayList<>();
+	}
+
+	@Override
+	public void updateForInput(Input<?> in) {
+		super.updateForInput(in);
+
+		if (in == unitTypeListInput) {
+			dataSource.setUnitTypeList(unitTypeListInput.getUnitTypeList());
+			return;
+		}
+	}
+
+	@Override
+	public void earlyInit() {
+		super.earlyInit();
+
+		columnFormats.clear();
+
+		// Close the file if it is already open
+		if (file != null) {
+			file.close();
+			file = null;
+		}
+
+		// Create the report file
+		StringBuilder tmp = new StringBuilder(InputAgent.getReportFileName(InputAgent.getRunName()));
+		tmp.append("-").append(this.getName());
+		tmp.append(".log");
+		file = new FileEntity(tmp.toString());
+
+		// Write the header line
+		file.format("%n%20s", "SimTime");
+		ArrayList<String> toks = new ArrayList<>();
+		dataSource.getValueTokens(toks);
+		for (String str : toks) {
+			if (str.equals("{") || str.equals("}"))
+				continue;
+			int length = Math.max(20, str.length());
+			StringBuilder fmt = new StringBuilder();
+			fmt.append("\t%").append(length).append("s");
+			columnFormats.add(fmt.toString());
+			file.format(fmt.toString(), str);
+		}
+
+		// Write the units line
+		String unit = Unit.getDisplayedUnit(TimeUnit.class);
+		file.format("%n%20s", unit);
+		for (int i=0; i<dataSource.getListSize(); i++) {
+			unit = Unit.getDisplayedUnit(dataSource.getUnitType(i));
+			file.format(columnFormats.get(i), unit);
+		}
+
+		// Empty the output buffer
+		file.flush();
+
+	}
+
+	@Override
+	public void startUp() {
+		super.startUp();
+
+		this.startAction();
+	}
+
+	private void startAction() {
+
+		// Schedule the next time an entry in the log file will be written
+		this.scheduleProcess(interval.getValue(), 5, endActionTarget);
+	}
+
+	private void endAction() {
+
+		double simTime = getSimTime();
+		double factor = Unit.getDisplayedUnitFactor(TimeUnit.class);
+		file.format("%n%20s", simTime/factor);
+
+		// Write the entry in the log file
+		for (int i=0; i<dataSource.getListSize(); i++) {
+			SampleProvider samp = dataSource.getValue().get(i);
+			factor = Unit.getDisplayedUnitFactor(dataSource.getUnitType(i));
+			file.format(columnFormats.get(i), samp.getNextSample(simTime)/factor);
+		}
+
+		// Empty the output buffer
+		file.flush();
+
+		// Get ready for the next entry
+		this.startAction();
+	}
+
+	protected final ProcessTarget endActionTarget = new EndActionTarget(this);
+
+	private static class EndActionTarget extends EntityTarget<ExpressionLogger> {
+		EndActionTarget(ExpressionLogger ent) {
+			super(ent, "endAction");
+		}
+
+		@Override
+		public void process() {
+			ent.endAction();
+		}
+	}
+
+}
