bff9cf335ff41eb83fd9dd0983648a8b9ca76218
Matt Chudleigh <matt.chudleigh@gmail.com>
2015-08-11 11:39:07 -0700

    JS: Refactor AnimCurve to seperate bezier solver
    Signed-off-by: Matt Chudleigh <matt.chudleigh@gmail.com>
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/main/java/com/jaamsim/render/AnimCurve.java
+++ src/main/java/com/jaamsim/render/AnimCurve.java
@@ -18,7 +18,6 @@ package com.jaamsim.render;
 
 import java.util.ArrayList;
 
-import com.jaamsim.math.Vec2d;
 import com.jaamsim.math.Vec4d;
 
 public class AnimCurve {
@@ -39,6 +38,37 @@ public class AnimCurve {
 	// Cache the last value as this may be requested several times in a row
 	private Vec4d lastVal;
 	private double lastTime;
+
+	// Animation curves represent up to 4 values at a given time
+	private static class CurveVal {
+		public double time;
+		public Vec4d vals;
+	}
+
+	private static CurveVal solveBezier(double s, Vec4d p0, Vec4d p1, Vec4d c0, Vec4d c1, int numComponents) {
+
+		double oneMinS = 1 - s;
+		double coeffP0 = oneMinS*oneMinS*oneMinS;
+		double coeffC0 = 3*s*oneMinS*oneMinS;
+		double coeffC1 = 3*s*s*oneMinS;
+		double coeffP1 = s*s*s;
+
+		CurveVal ret = new CurveVal();
+
+		ret.time = (coeffP0*p0.x + coeffC0*c0.x + coeffC1*c1.x + coeffP1*p1.x);
+
+		ret.vals = new Vec4d();
+		for (int compInd = 0; compInd < numComponents; ++compInd) {
+			double v = coeffP0*p0.getByInd(compInd+1) +
+			           coeffC0*c0.getByInd(compInd+1) +
+			           coeffC1*c1.getByInd(compInd+1) +
+			           coeffP1*p1.getByInd(compInd+1);
+
+			ret.vals.setByInd(compInd, v);
+		}
+		return ret;
+	}
+
 
 	public static AnimCurve buildFromColCurve(ColCurve colData) {
 		ArrayList<Double> ts = new ArrayList<>();
@@ -62,34 +92,26 @@ public class AnimCurve {
 					throw new RenderException("Missing OUT_TANGENT component in collada bezier animation");
 				}
 
+				Vec4d p0 = new Vec4d();
+				Vec4d p1 = new Vec4d();
+
+				// Due to collada weirdness we need to reassemble the basic bezier position vectors
+				p0.x = colData.in[i  ].x;
+				p1.x = colData.in[i+1].x;
+				for (int compInd = 0; compInd < colData.numComponents; ++compInd) {
+					p0.setByInd(compInd+1, colData.out[i  ].getByInd(compInd));
+					p1.setByInd(compInd+1, colData.out[i+1].getByInd(compInd));
+				}
+
+				Vec4d c0 = colData.outTan[i];
+				Vec4d c1 = colData.inTan[i+1];
+
 				// Interpolate bezier as 4 linear segments (this may need to be improved at a later date)
 				for (double s = 0.25; s <=1; s+=0.25) {
 
-					Vec4d val = new Vec4d();
-					double t = 0;
-
-					double oneMinS = 1 - s;
-					double coeffP0 = oneMinS*oneMinS*oneMinS;
-					double coeffC0 = 3*s*oneMinS*oneMinS;
-					double coeffC1 = 3*s*s*oneMinS;
-					double coeffP1 = s*s*s;
-
-					// Run the bezier solver for each component in the output vector
-					for (int compInd = 0; compInd < colData.numComponents; ++compInd) {
-
-						// Calculate the bezier result, see the collada 1.4.1 spec (page 4-4) for the source of this algorithm
-						Vec2d p0 = new Vec2d(colData.in[i  ].x,    colData.out[i  ].getByInd(compInd));
-						Vec2d p1 = new Vec2d(colData.in[i+1].x,    colData.out[i+1].getByInd(compInd));
-						Vec2d c0 = new Vec2d(colData.outTan[i].x,  colData.outTan[i].getByInd(compInd + 1));
-						Vec2d c1 = new Vec2d(colData.inTan[i+1].x, colData.inTan[i+1].getByInd(compInd + 1));
-
-						t = (coeffP0*p0.x + coeffC0*c0.x + coeffC1*c1.x + coeffP1*p1.x);
-
-						double v = coeffP0*p0.y + coeffC0*c0.y + coeffC1*c1.y + coeffP1*p1.y;
-						val.setByInd(compInd, v);
-					}
-					ts.add(t);
-					vs.add(val);
+					CurveVal val = solveBezier(s, p0, p1, c0, c1, colData.numComponents);
+					ts.add(val.time);
+					vs.add(val.vals);
 				}
 				continue;
 			}
