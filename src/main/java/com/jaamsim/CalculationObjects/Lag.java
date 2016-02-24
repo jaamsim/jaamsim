99ca0e1351691a82dca02219fafadafc56cee39e
Harry King <d.harry.king@gmail.com>
2016-02-23 21:18:49 -0800

    JS: revisions to Lag
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/CalculationObjects/Lag.java
+++ src/main/java/com/jaamsim/CalculationObjects/Lag.java
@@ -1,6 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2013 Ausenco Engineering Canada Inc.
+ * Copyright (C) 2016 KMA Technologies
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,59 +21,42 @@ import com.jaamsim.input.Keyword;
 import com.jaamsim.input.Output;
 import com.jaamsim.input.ValueInput;
 import com.jaamsim.units.TimeUnit;
+import com.jaamsim.units.UserSpecifiedUnit;
 
 /**
- * The Lag block is a standard control system component whose output is equal to integral( input - output ) / LagTime.
+ * The Lag block is a standard control system component whose output is equal to integral(input - output) / LagTime.
  * This operation has the effect of delaying and smoothing the input signal over the time scale given by LagTime.
  * @author Harry King
  *
  */
 public class Lag extends DoubleCalculation {
 
-	@Keyword(description = "The time constant for this operation: output = integral( input - output) / LagTime.",
-	         example = "Lag-1 LagTime { 15 s }")
+	@Keyword(description = "The time constant for this operation: "
+	                     + "output = integral(input - output) / LagTime.",
+	         exampleList = {"15 s"})
 	private final ValueInput lagTime;
 
-	private double lastUpdateTime;  // The time at which the last update was performed
-	private double integral; // The present value for the integral
-	private double presentValue;  // The present output value
-
 	{
-		lagTime = new ValueInput( "LagTime", "Key Inputs", 1.0d);
+		lagTime = new ValueInput("LagTime", "Key Inputs", 1.0d);
 		lagTime.setValidRange(1.0e-10, Double.POSITIVE_INFINITY);
 		lagTime.setUnitType(TimeUnit.class);
-		this.addInput( lagTime);
+		this.addInput(lagTime);
 	}
 
-	@Override
-	public void earlyInit() {
-		super.earlyInit();
-		integral = 0.0;
-		presentValue = 0.0;
-		lastUpdateTime = 0.0;
-	}
+	public Lag() {}
 
 	@Override
 	public double calculateValue(double simTime, double inputVal, double lastTime, double lastInputVal, double lastVal) {
-		double dt = simTime - lastUpdateTime;
-		double error = this.getInputValue(simTime) - presentValue;
-		return ( integral + error*dt ) / lagTime.getValue();
-	}
-
-	@Override
-	public void update(double simTime) {
-		super.update(simTime);
-		double dt = simTime - lastUpdateTime;
-		double error = this.getInputValue(simTime) - presentValue;
-		integral += error * dt;
-		presentValue = this.getLastValue();
-		lastUpdateTime = simTime;
+		double dt = simTime - lastTime;
+		double error = inputVal - lastVal;
+		return lastVal + dt*error/lagTime.getValue();
 	}
 
 	@Output(name = "Error",
-	 description = "The value for InputValue - OutputValue.")
-	public double getError( double simTime ) {
-		return this.getInputValue(simTime) - presentValue;
+	 description = "The value for InputValue - OutputValue.",
+	    unitType = UserSpecifiedUnit.class)
+	public double getError(double simTime) {
+		return this.getInputValue(simTime) - this.getLastValue();
 	}
 
 }
