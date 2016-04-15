ba74daff2038dd6d43beb109e39b3c98b77ea3a9
Harry King <d.harry.king@gmail.com>
2016-04-15 16:45:36 -0700

    JS: convert LogLogisticDistribution keywords to SampleInput
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ProbabilityDistributions/LogLogisticDistribution.java
+++ src/main/java/com/jaamsim/ProbabilityDistributions/LogLogisticDistribution.java
@@ -18,8 +18,8 @@
 package com.jaamsim.ProbabilityDistributions;
 
 import com.jaamsim.Samples.SampleConstant;
+import com.jaamsim.Samples.SampleInput;
 import com.jaamsim.input.Keyword;
-import com.jaamsim.input.ValueInput;
 import com.jaamsim.rng.MRG1999a;
 import com.jaamsim.units.DimensionlessUnit;
 import com.jaamsim.units.Unit;
@@ -32,26 +32,28 @@ import com.jaamsim.units.UserSpecifiedUnit;
 public class LogLogisticDistribution extends Distribution {
 
 	@Keyword(description = "The scale parameter for the Log-Logistic distribution.",
-	         exampleList = {"3.0"})
-	private final ValueInput scaleInput;
+	         exampleList = {"3.0", "InputValue1", "'2 * [InputValue1].Value'"})
+	private final SampleInput scaleInput;
 
 	@Keyword(description = "The shape parameter for the Log-Logistic distribution.  A decimal value > 0.0.",
-	         exampleList = {"1.0"})
-	private final ValueInput shapeInput;
+	         exampleList = {"1.0", "InputValue1", "'2 * [InputValue1].Value'"})
+	private final SampleInput shapeInput;
 
 	private final MRG1999a rng = new MRG1999a();
 
 	{
 		minValueInput.setDefaultValue(new SampleConstant(0.0d));
 
-		scaleInput = new ValueInput("Scale", "Key Inputs", 1.0d);
-		scaleInput.setValidRange( 0.0, Double.POSITIVE_INFINITY);
-		scaleInput.setUnitType( UserSpecifiedUnit.class );
+		scaleInput = new SampleInput("Scale", "Key Inputs", new SampleConstant(1.0d));
+		scaleInput.setValidRange(0.0, Double.POSITIVE_INFINITY);
+		scaleInput.setUnitType(UserSpecifiedUnit.class);
+		scaleInput.setEntity(this);
 		this.addInput(scaleInput);
 
-		shapeInput = new ValueInput("Shape", "Key Inputs", 1.0d);
-		shapeInput.setValidRange( 2.000001d, Double.POSITIVE_INFINITY);
-		shapeInput.setUnitType( DimensionlessUnit.class );
+		shapeInput = new SampleInput("Shape", "Key Inputs", new SampleConstant(1.0d));
+		shapeInput.setValidRange(2.000001d, Double.POSITIVE_INFINITY);
+		shapeInput.setUnitType(DimensionlessUnit.class);
+		shapeInput.setEntity(this);
 		this.addInput(shapeInput);
 	}
 
@@ -74,18 +76,24 @@ public class LogLogisticDistribution extends Distribution {
 
 		// Inverse transform method
 		double u = rng.nextUniform();
-		return scaleInput.getValue() * Math.pow( u / (1 - u), 1.0 / shapeInput.getValue() );
+		double scale = scaleInput.getValue().getNextSample(simTime);
+		double shape = shapeInput.getValue().getNextSample(simTime);
+		return scale * Math.pow( u / (1 - u), 1.0 / shape );
 	}
 
 	@Override
 	protected double getMean(double simTime) {
-		double theta = Math.PI / shapeInput.getValue();
-		return scaleInput.getValue() * theta / Math.sin( theta );
+		double scale = scaleInput.getValue().getNextSample(simTime);
+		double shape = shapeInput.getValue().getNextSample(simTime);
+		double theta = Math.PI / shape;
+		return scale * theta / Math.sin( theta );
 	}
 
 	@Override
 	protected double getStandardDev(double simTime) {
-		double theta = Math.PI / shapeInput.getValue();
-		return scaleInput.getValue() * Math.sqrt( theta * ( 2.0/Math.sin(2.0*theta) - theta/Math.pow( Math.sin(theta), 2.0) ) );
+		double scale = scaleInput.getValue().getNextSample(simTime);
+		double shape = shapeInput.getValue().getNextSample(simTime);
+		double theta = Math.PI / shape;
+		return scale * Math.sqrt( theta * ( 2.0/Math.sin(2.0*theta) - theta/Math.pow( Math.sin(theta), 2.0) ) );
 	}
 }
