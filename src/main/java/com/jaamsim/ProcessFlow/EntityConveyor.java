27df7fad4dde75936d15848ff7bae8916670bb49
Harry King <d.harry.king@gmail.com>
2016-08-16 18:03:53 -0700

    JS: add trace outputs to EntityConveyor
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ProcessFlow/EntityConveyor.java
+++ src/main/java/com/jaamsim/ProcessFlow/EntityConveyor.java
@@ -137,6 +137,12 @@ public class EntityConveyor extends LinkedService {
 
 	@Override
 	protected boolean updateForStoppage(double startWork, double stopWork, double resumeWork) {
+		if (traceFlag) {
+			trace(0, "updateForStoppage");
+			traceLine(1, "startWork=%.6f, stopWork=%.6f, resumeWork=%.6f",
+					startWork, stopWork, resumeWork);
+			traceLine(1, "startTimeList=%s", startTimeList);
+		}
 
 		// Adjust the start time for each entity to account for the delay
 		double stopDur = resumeWork - stopWork;
@@ -144,6 +150,8 @@ public class EntityConveyor extends LinkedService {
 			double t = Math.min(stopWork, startTimeList.get(i));
 			startTimeList.set(i, t + stopDur);
 		}
+
+		if (traceFlag) traceLine(1, "startTimeList=%s", startTimeList);
 		return false;
 	}
 
