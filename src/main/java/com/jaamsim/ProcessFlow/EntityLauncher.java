8e7af9e13f307521b8215a733799779a2173d32f
Harry King <d.harry.king@gmail.com>
2017-06-18 16:28:45 -0700

    JS: make EntityLauncher a subclass of GameEntity
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ProcessFlow/EntityLauncher.java
+++ src/main/java/com/jaamsim/ProcessFlow/EntityLauncher.java
@@ -19,24 +19,19 @@ package com.jaamsim.ProcessFlow;
 import java.util.ArrayList;
 
 import com.jaamsim.EntityProviders.EntityProvInput;
+import com.jaamsim.GameObjects.GameEntity;
 import com.jaamsim.Graphics.DisplayEntity;
 import com.jaamsim.Graphics.LinkDisplayable;
 import com.jaamsim.Graphics.OverlayEntity;
 import com.jaamsim.Graphics.TextBasics;
 import com.jaamsim.basicsim.Entity;
-import com.jaamsim.basicsim.EntityTarget;
-import com.jaamsim.events.EventHandle;
-import com.jaamsim.events.EventManager;
-import com.jaamsim.events.ProcessTarget;
 import com.jaamsim.input.InputAgent;
 import com.jaamsim.input.InterfaceEntityInput;
 import com.jaamsim.input.Keyword;
 import com.jaamsim.input.KeywordIndex;
-import com.jaamsim.input.StringInput;
 import com.jaamsim.math.Vec3d;
-import com.jogamp.newt.event.KeyEvent;
 
-public class EntityLauncher extends DisplayEntity implements LinkDisplayable {
+public class EntityLauncher extends GameEntity implements LinkDisplayable {
 
 	@Keyword(description = "The prototype for entities to be generated. "
 	                     + "The generated entities will be copies of this entity.",
@@ -47,11 +42,6 @@ public class EntityLauncher extends DisplayEntity implements LinkDisplayable {
 	         exampleList = {"Queue1"})
 	protected final InterfaceEntityInput<Linkable> nextComponent;
 
-	@Keyword(description = "",
-	         exampleList = {""})
-	private final StringInput actionKey;
-
-	private EventManager evt;
 	private int numberGenerated = 0;  // Number of entities generated so far
 
 	{
@@ -65,52 +55,11 @@ public class EntityLauncher extends DisplayEntity implements LinkDisplayable {
 		nextComponent = new InterfaceEntityInput<>(Linkable.class, "NextComponent", "Key Inputs", null);
 		nextComponent.setRequired(true);
 		this.addInput(nextComponent);
-
-		actionKey = new StringInput("ActionKey", "Key Inputs", null);
-		this.addInput(actionKey);
 	}
 
 	public EntityLauncher() {}
-	
-	@Override
-	public void earlyInit() {
-		super.earlyInit();
-		evt = null;
-	}
-	
-	@Override
-	public void startUp() {
-		super.startUp();
-		evt = EventManager.current();
-	}
 
 	@Override
-	public void handleKeyReleased(int keyCode, char keyChar, boolean shift, boolean control, boolean alt) {
-		super.handleKeyReleased(keyCode, keyChar, shift, control, alt);
-
-		// Space bar generates a new entity
-		if (keyCode == KeyEvent.VK_SPACE) {
-			generateEntity();
-			return;
-		}
-	}
-
-	@Override
-	public void handleMouseClicked(short count, Vec3d globalCoord) {
-
-		// Double click generates a new entity
-		if (count == 2) {
-			generateEntity();
-			return;
-		}
-	}
-
-	private void generateEntity() {
-		if (evt == null || doActionHandle.isScheduled())
-			return;
-		evt.scheduleProcessExternal(0L, 0, false, doActionTarget, doActionHandle);
-	}
-
 	public void doAction() {
 
 		// Create a new entity
@@ -125,22 +74,6 @@ public class EntityLauncher extends DisplayEntity implements LinkDisplayable {
 		// Send the entity to the next element in the chain
 		nextComponent.getValue().addEntity(ent);
 	}
-
-	/**
-	 * DoActionTarget
-	 */
-	private static class DoActionTarget extends EntityTarget<EntityLauncher> {
-		DoActionTarget(EntityLauncher ent) {
-			super(ent, "doAction");
-		}
-
-		@Override
-		public void process() {
-			ent.doAction();
-		}
-	}
-	private final ProcessTarget doActionTarget = new DoActionTarget(this);
-	private final EventHandle doActionHandle = new EventHandle();
 
 	@Override
 	public void linkTo(DisplayEntity nextEnt) {
