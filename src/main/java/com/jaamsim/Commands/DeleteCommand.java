2d1926b12202d8d789d4756054c1e35123af8f4a
Harry King <d.harry.king@gmail.com>
2017-09-18 15:22:41 -0700

    JS: add DeleteCommand
    Signed-off-by: Harry King <d.harry.king@gmail.com>

---
+++ src/main/java/com/jaamsim/Commands/DeleteCommand.java
@@ -0,0 +1,51 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2017 JaamSim Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jaamsim.Commands;
+
+import com.jaamsim.basicsim.Entity;
+
+public class DeleteCommand implements Command {
+
+	private final Entity entity;
+	private final String entityName;
+
+	public DeleteCommand(Entity ent) {
+		entity = ent;
+		entityName = entity.getName();
+	}
+
+	@Override
+	public void execute() {
+		entity.kill();
+	}
+
+	@Override
+	public void undo() {
+		entity.restore(entityName);
+	}
+
+	@Override
+	public Command tryMerge(Command cmd) {
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Delete: '%s'", entityName);
+	}
+
+}
