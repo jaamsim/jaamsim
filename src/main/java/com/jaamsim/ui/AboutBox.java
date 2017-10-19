c77dff08f830cfee0f48f2b143b57e7061cae4b1
Harvey Harrison <harvey.harrison@gmail.com>
2017-10-19 09:31:16 -0700

    JS: update version to 2017-11
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/main/java/com/jaamsim/ui/AboutBox.java
+++ src/main/java/com/jaamsim/ui/AboutBox.java
@@ -34,7 +34,7 @@ import com.jaamsim.basicsim.Simulation;
 public class AboutBox extends FrameBox implements ActionListener {
 	private static AboutBox instance;
 
-	public static final String version = "2017-10";
+	public static final String version = "2017-11";
 
 	public AboutBox() {
 		super("About");
