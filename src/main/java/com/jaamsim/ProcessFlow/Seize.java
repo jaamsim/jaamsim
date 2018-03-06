f696d2559a9751fe669390a60ec7be35caa2a1d6
Harry King <d.harry.king@gmail.com>
2018-03-06 15:50:24 -0800

    JS: cosmetic changes to Seize
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ProcessFlow/Seize.java
+++ src/main/java/com/jaamsim/ProcessFlow/Seize.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2013 Ausenco Engineering Canada Inc.
- * Copyright (C) 2017 JaamSim Software Inc.
+ * Copyright (C) 2017-2018 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -71,6 +71,8 @@ public class Seize extends LinkedService implements ResourceUser {
 		numberOfUnitsList.setUnitType(DimensionlessUnit.class);
 		this.addInput(numberOfUnitsList);
 	}
+
+	public Seize() {}
 
 	@Override
 	public void validate() {
