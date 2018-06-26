7587e77d738f29dc9b09ee3616685ed9178d80df
Harry King <d.harry.king@gmail.com>
2018-06-26 12:40:32 -0700

    JS: add RandomStreamUser interface
    Signed-off-by: Harry King <d.harry.king@gmail.com>

---
+++ src/main/java/com/jaamsim/ProbabilityDistributions/RandomStreamUser.java
@@ -0,0 +1,33 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2018 JaamSim Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jaamsim.ProbabilityDistributions;
+
+public interface RandomStreamUser {
+
+	/**
+	 * Returns the random seed used by this object.
+	 * @return random seed
+	 */
+	public int getStreamNumber();
+
+	/**
+	 * Returns the keyword used to enter the random seed.
+	 * @return random seed keyword
+	 */
+	public String getStreamNumberKeyword();
+
+}
