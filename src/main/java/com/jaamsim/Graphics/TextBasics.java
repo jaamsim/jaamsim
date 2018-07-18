32929765cef685849014c08fb7c807035bbb3141
Harry King <d.harry.king@gmail.com>
2018-07-17 17:05:38 -0700

    JS: delete original methods related to default inputs in TextBasics
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/TextBasics.java
+++ src/main/java/com/jaamsim/Graphics/TextBasics.java
@@ -480,32 +480,4 @@ public abstract class TextBasics extends DisplayEntity {
 		return dropShadowOffset.getValue();
 	}
 
-	public StringChoiceInput getFontNameInput() {
-		return fontName;
-	}
-
-	public ValueInput getTextHeightInput() {
-		return textHeight;
-	}
-
-	public StringListInput getFontStyleInput() {
-		return fontStyle;
-	}
-
-	public ColourInput getFontColorInput() {
-		return fontColor;
-	}
-
-	public BooleanInput getDropShadowInput() {
-		return dropShadow;
-	}
-
-	public ColourInput getDropShadowColorInput() {
-		return dropShadowColor;
-	}
-
-	public Vec3dInput getDropShadowOffsetInput() {
-		return dropShadowOffset;
-	}
-
 }
