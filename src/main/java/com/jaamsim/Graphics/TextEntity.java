ebd23f38ef2c3e8d9fd148db296d4077ae62e206
Harry King <d.harry.king@gmail.com>
2018-11-20 15:27:23 -0800

    JS: add static methods getFontsInUse, getFontColoursInUse to TextEntity
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/TextEntity.java
+++ src/main/java/com/jaamsim/Graphics/TextEntity.java
@@ -16,6 +16,11 @@
  */
 package com.jaamsim.Graphics;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.jaamsim.basicsim.Entity;
+import com.jaamsim.input.ColourInput;
 import com.jaamsim.math.Color4d;
 import com.jaamsim.math.Vec3d;
 
@@ -30,5 +35,29 @@ public interface TextEntity {
 	public boolean getDropShadow();
 	public Color4d getDropShadowColor();
 	public Vec3d getDropShadowOffset();
+
+	public static ArrayList<String> getFontsInUse() {
+		ArrayList<String> ret = new ArrayList<>();
+		for (DisplayEntity ent : Entity.getClonesOfIterator(DisplayEntity.class, TextEntity.class)) {
+			TextEntity textEnt = (TextEntity) ent;
+			if (ret.contains(textEnt.getFontName()))
+				continue;
+			ret.add(textEnt.getFontName());
+		}
+		Collections.sort(ret);
+		return ret;
+	}
+
+	public static ArrayList<Color4d> getFontColoursInUse() {
+		ArrayList<Color4d> ret = new ArrayList<>();
+		for (DisplayEntity ent : Entity.getClonesOfIterator(DisplayEntity.class, TextEntity.class)) {
+			TextEntity textEnt = (TextEntity) ent;
+			if (ret.contains(textEnt.getFontColor()))
+				continue;
+			ret.add(textEnt.getFontColor());
+		}
+		Collections.sort(ret, ColourInput.colourComparator);
+		return ret;
+	}
 
 }
