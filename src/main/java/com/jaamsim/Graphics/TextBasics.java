ec9f235a20e8958ac59d649db54a888f68befffe
Harry King <d.harry.king@gmail.com>
2018-11-20 11:37:54 -0800

    JS: delete unused methods getFontsInUse, getFontColoursInUse in TextBasics
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/TextBasics.java
+++ src/main/java/com/jaamsim/Graphics/TextBasics.java
@@ -22,10 +22,8 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.util.ArrayList;
-import java.util.Collections;
 
 import com.jaamsim.DisplayModels.TextModel;
-import com.jaamsim.basicsim.Entity;
 import com.jaamsim.controllers.RenderManager;
 import com.jaamsim.input.BooleanInput;
 import com.jaamsim.input.ColourInput;
@@ -517,28 +515,6 @@ public abstract class TextBasics extends DisplayEntity implements TextEntity {
 			return getTextModel().getDropShadowOffset();
 		}
 		return dropShadowOffset.getValue();
-	}
-
-	public static ArrayList<String> getFontsInUse() {
-		ArrayList<String> ret = new ArrayList<>();
-		for (TextBasics text : Entity.getClonesOfIterator(TextBasics.class)) {
-			if (ret.contains(text.getFontName()))
-				continue;
-			ret.add(text.getFontName());
-		}
-		Collections.sort(ret);
-		return ret;
-	}
-
-	public static ArrayList<Color4d> getFontColoursInUse() {
-		ArrayList<Color4d> ret = new ArrayList<>();
-		for (TextBasics text : Entity.getClonesOfIterator(TextBasics.class)) {
-			if (ret.contains(text.getFontColor()))
-				continue;
-			ret.add(text.getFontColor());
-		}
-		Collections.sort(ret, ColourInput.colourComparator);
-		return ret;
 	}
 
 }
