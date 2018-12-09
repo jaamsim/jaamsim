33e405d820d84477ea13f1f8f0c87d042a47d1ca
Harry King <d.harry.king@gmail.com>
2018-12-08 19:05:43 -0800

    JS: add FillEntity and LineEntity interfaces
    Signed-off-by: Harry King <d.harry.king@gmail.com>

---
+++ src/main/java/com/jaamsim/Graphics/FillEntity.java
@@ -0,0 +1,42 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2018 JaamSim Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jaamsim.Graphics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.jaamsim.basicsim.Entity;
+import com.jaamsim.input.ColourInput;
+import com.jaamsim.math.Color4d;
+
+public interface FillEntity {
+	public boolean isFilled();
+	public Color4d getFillColour();
+
+	public static ArrayList<Color4d> getFillColoursInUse() {
+		ArrayList<Color4d> ret = new ArrayList<>();
+		for (DisplayEntity ent : Entity.getClonesOfIterator(DisplayEntity.class, FillEntity.class)) {
+			FillEntity fillEnt = (FillEntity) ent;
+			if (ret.contains(fillEnt.getFillColour()))
+				continue;
+			ret.add(fillEnt.getFillColour());
+		}
+		Collections.sort(ret, ColourInput.colourComparator);
+		return ret;
+	}
+
+}
