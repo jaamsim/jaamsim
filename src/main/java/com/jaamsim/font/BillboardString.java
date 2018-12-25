271bac3746da246579fb1ab91af4cc9f86fd1fa0
Harry King <d.harry.king@gmail.com>
2018-12-25 07:53:56 -0800

    JS: update copyright info for overlay picking system
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/font/BillboardString.java
+++ src/main/java/com/jaamsim/font/BillboardString.java
@@ -1,6 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2014 Ausenco Engineering Canada Inc.
+ * Copyright (C) 2018 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
