d680f653ca182ed26ffc9c21754d7be604242315
Harvey Harrison <harvey.harrison@gmail.com>
2019-05-31 14:09:58 -0700

    JS: update version to 2019-06
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/main/java/com/jaamsim/ui/AboutBox.java
+++ src/main/java/com/jaamsim/ui/AboutBox.java
@@ -33,7 +33,7 @@ import javax.swing.JTextArea;
 public class AboutBox extends FrameBox implements ActionListener {
 	private static AboutBox instance;
 
-	public static final String version = "2019-05";
+	public static final String version = "2019-06";
 
 	public AboutBox() {
 		super("About");
