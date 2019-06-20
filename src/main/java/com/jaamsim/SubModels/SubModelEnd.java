f868917f5e83015160c013b1e6de77ac0ac4b246
Harry King <d.harry.king@gmail.com>
2019-06-19 21:59:46 -0700

    JS: add code for 'SubModelEnd' to SubModels
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/SubModels/SubModelEnd.java
+++ src/main/java/com/jaamsim/SubModels/SubModelEnd.java
@@ -16,16 +16,81 @@
  */
 package com.jaamsim.SubModels;
 
-import com.jaamsim.Graphics.DisplayEntity;
-import com.jaamsim.ProcessFlow.Linkable;
+import java.util.ArrayList;
 
-public class SubModelEnd extends DisplayEntity implements Linkable {
+import com.jaamsim.EntityProviders.EntityProvInput;
+import com.jaamsim.Graphics.DisplayEntity;
+import com.jaamsim.Graphics.LinkDisplayable;
+import com.jaamsim.Graphics.OverlayEntity;
+import com.jaamsim.Graphics.TextBasics;
+import com.jaamsim.ProcessFlow.Linkable;
+import com.jaamsim.basicsim.Entity;
+import com.jaamsim.input.Keyword;
+import com.jaamsim.math.Vec3d;
+
+public class SubModelEnd extends DisplayEntity implements Linkable, LinkDisplayable {
+
+	@Keyword(description = "The next object, external to the sub-model, to which the processed "
+	                     + "entity is passed. "
+	                     + "If left blank, the entity is returned to the parent sub-model which "
+	                     + "directs it to the object specified by its NextComponent input.",
+	         exampleList = {"Statistics1"})
+	protected final EntityProvInput<DisplayEntity> nextComponent;
+
+	private CompoundEntity submodel;
+
+	{
+		nextComponent = new EntityProvInput<>(DisplayEntity.class, "NextComponent", KEY_INPUTS, null);
+		nextComponent.addInvalidClass(OverlayEntity.class);
+		nextComponent.addInvalidClass(TextBasics.class);
+		this.addInput(nextComponent);
+	}
 
 	public SubModelEnd() {}
 
+	public void setSubModel(CompoundEntity sub) {
+		submodel = sub;
+	}
+
 	@Override
 	public void addEntity(DisplayEntity ent) {
-		// TODO Auto-generated method stub
+
+		// If NextComponent is not specified, return the entity to the sub-model object
+		if (nextComponent.isDefault()) {
+			submodel.addReturnedEntity(ent);
+			return;
+		}
+
+		// If NextComponent is specified, send the entity to that object
+		DisplayEntity nextComp = nextComponent.getValue().getNextEntity(getSimTime());
+		if (!(nextComp instanceof Linkable)) {
+			error("Object '%s' returned by NextComponent does not accept an entity.", nextComp);
+		}
+		((Linkable)nextComp).addEntity(ent);
+	}
+
+	// LinkDisplayable
+	@Override
+	public ArrayList<Entity> getDestinationEntities() {
+		return new ArrayList<>();	}
+
+	@Override
+	public ArrayList<Entity> getSourceEntities() {
+		return new ArrayList<>();
+	}
+
+	@Override
+	public Vec3d getSourcePoint() {
+		return getGlobalPosition();
+	}
+	@Override
+	public Vec3d getSinkPoint() {
+		return getGlobalPosition();
+	}
+
+	@Override
+	public double getRadius() {
+		return getSize().mag2()/2.0;
 	}
 
 }
