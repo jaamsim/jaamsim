a5bc3c660473f734ac176be32fd6014556ee0969
Harry King <d.harry.king@gmail.com>
2019-06-21 15:47:15 -0700

    JS: add threshold to ServerAndQueue
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/SubModels/ServerAndQueue.java
+++ src/main/java/com/jaamsim/SubModels/ServerAndQueue.java
@@ -21,13 +21,17 @@ import java.util.ArrayList;
 import com.jaamsim.Graphics.DisplayEntity;
 import com.jaamsim.ProcessFlow.Queue;
 import com.jaamsim.ProcessFlow.Server;
+import com.jaamsim.Samples.SampleConstant;
 import com.jaamsim.Samples.SampleInput;
 import com.jaamsim.SubModels.SubModelEnd;
 import com.jaamsim.SubModels.SubModelStart;
+import com.jaamsim.Thresholds.ExpressionThreshold;
 import com.jaamsim.basicsim.JaamSimModel;
 import com.jaamsim.input.InputAgent;
 import com.jaamsim.input.Keyword;
+import com.jaamsim.input.Output;
 import com.jaamsim.math.Vec3d;
+import com.jaamsim.units.DimensionlessUnit;
 import com.jaamsim.units.DistanceUnit;
 
 public class ServerAndQueue extends CompoundEntity {
@@ -35,6 +39,18 @@ public class ServerAndQueue extends CompoundEntity {
 	@Keyword(description = "The service time required to process an entity.",
 	         exampleList = { "3.0 h", "NormalDistribution1", "'1[s] + 0.5*[TimeSeries1].PresentValue'" })
 	private SampleInput serviceTime;
+
+	@Keyword(description = "The queue length at which the Threshold output closes.",
+	         exampleList = { "3", "[InputValue1].Value" })
+	private SampleInput maxQueueLength;
+
+	{
+		SampleConstant def = new SampleConstant(Double.POSITIVE_INFINITY);
+		maxQueueLength = new SampleInput("MaxQueueLength", KEY_INPUTS, def);
+		maxQueueLength.setUnitType(DimensionlessUnit.class);
+		maxQueueLength.setValidRange(0.0d, Double.POSITIVE_INFINITY);
+		this.addInput(maxQueueLength);
+	}
 
 	public ServerAndQueue() {}
 
@@ -46,6 +62,7 @@ public class ServerAndQueue extends CompoundEntity {
 		JaamSimModel simModel = getJaamSimModel();
 		SubModelStart start = InputAgent.generateEntityWithName(simModel, SubModelStart.class, getComponentName("Start"), true, true);
 		Queue queue = InputAgent.generateEntityWithName(simModel, Queue.class, getComponentName("Queue"), true, true);
+		ExpressionThreshold threshold = InputAgent.generateEntityWithName(simModel, ExpressionThreshold.class, getComponentName("Threshold"), true, true);
 		Server server = InputAgent.generateEntityWithName(simModel, Server.class, getComponentName("Server"), true, true);
 		SubModelEnd end = InputAgent.generateEntityWithName(simModel, SubModelEnd.class, getComponentName("End"), true, true);
 
@@ -60,11 +77,16 @@ public class ServerAndQueue extends CompoundEntity {
 		InputAgent.applyArgs(server, "WaitQueue", queue.getName());
 		InputAgent.applyArgs(server, "NextComponent", end.getName());
 
+		// Threshold inputs
+		String expString = String.format("[%s].QueueLength < [%s].MaxQueueLength", queue, this);
+		InputAgent.applyArgs(threshold, "OpenCondition", expString);
+
 		// Set the component positions within the sub-model region
-		InputAgent.applyVec3d(start,  "Position", new Vec3d(-1.0d, -0.4d, 0.0d), DistanceUnit.class);
-		InputAgent.applyVec3d(queue,  "Position", new Vec3d(-0.5d,  0.4d, 0.0d), DistanceUnit.class);
-		InputAgent.applyVec3d(server, "Position", new Vec3d( 0.0d, -0.4d, 0.0d), DistanceUnit.class);
-		InputAgent.applyVec3d(end,    "Position", new Vec3d( 1.0d, -0.4d, 0.0d), DistanceUnit.class);
+		InputAgent.applyVec3d(start,     "Position", new Vec3d(-1.0d, -0.4d, 0.0d), DistanceUnit.class);
+		InputAgent.applyVec3d(queue,     "Position", new Vec3d(-0.5d,  0.4d, 0.0d), DistanceUnit.class);
+		InputAgent.applyVec3d(threshold, "Position", new Vec3d( 0.5d,  0.4d, 0.0d), DistanceUnit.class);
+		InputAgent.applyVec3d(server,    "Position", new Vec3d( 0.0d, -0.4d, 0.0d), DistanceUnit.class);
+		InputAgent.applyVec3d(end,       "Position", new Vec3d( 1.0d, -0.4d, 0.0d), DistanceUnit.class);
 
 		// Set the scale, size, and position of the sub-model region
 		setDefaultRegionScale(0.5d);
@@ -75,9 +97,17 @@ public class ServerAndQueue extends CompoundEntity {
 		ArrayList<DisplayEntity> compList = new ArrayList<>();
 		compList.add(start);
 		compList.add(queue);
+		compList.add(threshold);
 		compList.add(server);
 		compList.add(end);
 		setComponentList(compList);
+	}
+
+	@Output(name = "MaxQueueLength",
+	 description = "The queue length at which the Threshold output closes.",
+	    sequence = 0)
+	public int getMaxQueueLength(double simTime) {
+		return (int) maxQueueLength.getValue().getNextSample(simTime);
 	}
 
 }
