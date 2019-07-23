jaamsim/jaamsim

commit f8c7848b6e57163ce204fc991ef630a1bca3a5cc

JS: modify getLabel to find an entity's EntityLabel based on its name
This method is less robust than using the TargetEntity input for the
EntityLabel, however, it allows the input file to be loaded correctly.
The Simulation keyword ShowLabels is processed before the TargetEntity for
the EntityLabels, which causes a second EntityLabel to be created when
getLabels relies on the TargetEntity value. Another way to solve the
problem would be to make TargetEntity an 'early' keyword, but this would
not fix the existing input files.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/Graphics/EntityLabel.java
@@ -54,11 +54,12 @@ public class EntityLabel extends TextBasics {
 	public EntityLabel() {}
 
 	public static EntityLabel getLabel(DisplayEntity ent) {
-		for (EntityLabel label : ent.getJaamSimModel().getClonesOfIterator(EntityLabel.class)) {
-			if (label.getTarget() == ent)
-				return label;
-		}
-		return null;
+		Entity label = ent.getJaamSimModel().getNamedEntity(ent.getName() + "_Label");
+		if (label == null) // FIXME - remove when all labels have the correct name
+			label = ent.getJaamSimModel().getNamedEntity(ent.getName() + "_Label1");
+		if (label != null && !(label instanceof EntityLabel))
+			throw new ErrorException("Entity %s is not an EntityLabel", label);
+		return (EntityLabel) label;
 	}
 
 	@Override

commit db001287bc3a7a3b1ea2cac4d975ac6ef94c37ef

JS: fix method 'getLabel' to search for the correct target entity
Corrects a bug introduced by f8c7848b6e57163ce204fc991ef630a1bca3a5cc
(JS: modify getLabel to find an entity's EntityLabel based on its name).
Searching by name does not find the entity's label when the entity's name
has been changed.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/Graphics/EntityLabel.java
@@ -54,12 +54,24 @@ public class EntityLabel extends TextBasics {
 	public EntityLabel() {}
 
 	public static EntityLabel getLabel(DisplayEntity ent) {
+
+		// Is there a label with the correct name?
 		Entity label = ent.getJaamSimModel().getNamedEntity(ent.getName() + "_Label");
 		if (label == null) // FIXME - remove when all labels have the correct name
 			label = ent.getJaamSimModel().getNamedEntity(ent.getName() + "_Label1");
-		if (label != null && !(label instanceof EntityLabel))
-			throw new ErrorException("Entity %s is not an EntityLabel", label);
-		return (EntityLabel) label;
+		if (label != null) {
+			if (!(label instanceof EntityLabel))
+				throw new ErrorException("Entity %s is not an EntityLabel", label);
+			return (EntityLabel) label;
+		}
+
+		// Otherwise, search for a label with the correct target entity
+		// (Required when the entity's name has been changed)
+		for (EntityLabel lab : ent.getJaamSimModel().getClonesOfIterator(EntityLabel.class)) {
+			if (lab.getTarget() == ent)
+				return lab;
+		}
+		return null;
 	}
 
 	@Override