a033b36c41d3a964579617d707844a3409b88a5a
Harry King <d.harry.king@gmail.com>
2019-09-14 17:39:03 -0700

    JS: use JaamSimModel.getCalendarDate in OverlayClock
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/OverlayClock.java
+++ src/main/java/com/jaamsim/Graphics/OverlayClock.java
@@ -17,7 +17,7 @@
 package com.jaamsim.Graphics;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 import com.jaamsim.input.Input;
@@ -42,18 +42,7 @@ public class OverlayClock extends OverlayText {
 	         exampleList = {"'yyyy-MMM-dd HH:mm:ss.SSS'"})
 	protected final StringInput dateFormatInput;
 
-	private Calendar calendar;
 	private SimpleDateFormat dateFormat;
-
-	private long year;
-	private long dayOfYear;
-	private long hour;
-	private long minute;
-	private long second;
-	private long milli;
-
-	private int month;
-	private int dayOfMonth;
 
 	{
 		formatText.setHidden(true);
@@ -69,8 +58,6 @@ public class OverlayClock extends OverlayText {
 	}
 
 	public OverlayClock() {
-		calendar = Calendar.getInstance();
-		calendar.setTimeZone( TimeZone.getTimeZone( "GMT" ) );
 		dateFormat = new SimpleDateFormat(dateFormatInput.getValue());
 		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
 	}
@@ -85,67 +72,12 @@ public class OverlayClock extends OverlayText {
 		}
 	}
 
-	private static final int[] firstDayOfMonth;
-	static {
-		firstDayOfMonth = new int[13];
-		firstDayOfMonth[0] = 1;
-		firstDayOfMonth[1] = 32;
-		firstDayOfMonth[2] = 60;
-		firstDayOfMonth[3] = 91;
-		firstDayOfMonth[4] = 121;
-		firstDayOfMonth[5] = 152;
-		firstDayOfMonth[6] = 182;
-		firstDayOfMonth[7] = 213;
-		firstDayOfMonth[8] = 244;
-		firstDayOfMonth[9] = 274;
-		firstDayOfMonth[10] = 305;
-		firstDayOfMonth[11] = 335;
-		firstDayOfMonth[12] = 366;
-	}
-
-	/**
-	 * Return the month 1-12 for the given day of the year 1-365
-	 */
-	private static int getMonthForDay( int d ) {
-		for (int i = 1; i < firstDayOfMonth.length; i++) {
-			if (d < firstDayOfMonth[i]) {
-				return i;
-			}
-		}
-		return 12;
-	}
-
-	/**
-	 * Return the day of the year (1-365) that corresponds to the first day of the given month (1-12).
-	 */
-	private static int getFirstDayOfMonth(int month) {
-		return firstDayOfMonth[month-1];
-	}
-
 	@Override
 	public String getRenderText(double simTime) {
-
-		double time = simTime / 3600.0d;  // time in hours
-
-		year = (long)Math.floor(time/8760.0d);
-		dayOfYear = (long)Math.floor(time/24.0d) % 365 + 1;  // dayOfYear = 1 - 365;
-		hour = (long)Math.floor(time) % 24;
-		minute = (long)Math.floor(time*60.0d) % 60;
-		second = (long)Math.floor(simTime) % 60;
-		milli = (long)Math.floor(simTime*1000.0d) % 1000;
-
-		month = getMonthForDay((int) dayOfYear);       // month = 1 - 12
-		dayOfMonth = (int)dayOfYear - getFirstDayOfMonth(month) + 1;
-
-		calendar.set(Calendar.YEAR, (int) year + startingYear.getValue());
-		calendar.set(Calendar.MONTH, month - 1);  // Java months are 0 - 11
-		calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-		calendar.set(Calendar.HOUR_OF_DAY, (int) hour);
-		calendar.set(Calendar.MINUTE, (int) minute);
-		calendar.set(Calendar.SECOND, (int) second);
-		calendar.set(Calendar.MILLISECOND, (int) milli);
-
-		return dateFormat.format(calendar.getTime());
+		double startTime = (startingYear.getValue() - 1970) * 8760.0d * 3600.0d;
+		long millis = getJaamSimModel().simTimeToCalendarMillis(startTime + simTime);
+		Date date = getJaamSimModel().getCalendarDate(millis);
+		return dateFormat.format(date);
 	}
 
 }
