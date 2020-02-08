0b00c9e846b8cdfcffa2e2c3d4128091ebd948c5
Harry King <d.harry.king@gmail.com>
2020-02-07 18:00:26 -0800

    JS: update copyright for files modified by the renderer optimisation
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/render/Renderer.java
+++ src/main/java/com/jaamsim/render/Renderer.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2012 Ausenco Engineering Canada Inc.
- * Copyright (C) 2018 JaamSim Software Inc.
+ * Copyright (C) 2018-2020 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
