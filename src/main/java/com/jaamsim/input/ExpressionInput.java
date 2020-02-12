8b37d463bfcbd68063bc84cddabefe0cee4f868f
Harry King <d.harry.king@gmail.com>
2020-02-12 12:03:56 -0800

    JS: add 'getPresentValueString' to ExpressionInput
    Allows the Input Builder to show the present value of the input.
    
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/input/ExpressionInput.java
+++ src/main/java/com/jaamsim/input/ExpressionInput.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2014 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2018 JaamSim Software Inc.
+ * Copyright (C) 2016-2020 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,6 +91,20 @@ public class ExpressionInput extends Input<ExpParser.Expression> {
 	@Override
 	public boolean useExpressionBuilder() {
 		return true;
+	}
+
+	@Override
+	public String getPresentValueString(double simTime) {
+		if (value == null)
+			return "";
+
+		try {
+			ExpResult res = ExpEvaluator.evaluateExpression(value, simTime);
+			return res.toString();
+		}
+		catch (ExpError e) {
+			return getValueString();
+		}
 	}
 
 }
