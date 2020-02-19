jaamsim/jaamsim

commit c374ff3ebc6d11f67ac2ee1336f92a2fb96c6ff2

JS: return simTime from 'parseRFC8601DateTime' instead of clock ticks
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/Input.java
@@ -42,7 +42,6 @@ import com.jaamsim.basicsim.ObjectType;
 import com.jaamsim.datatypes.BooleanVector;
 import com.jaamsim.datatypes.DoubleVector;
 import com.jaamsim.datatypes.IntegerVector;
-import com.jaamsim.events.EventManager;
 import com.jaamsim.math.Color4d;
 import com.jaamsim.ui.NaturalOrderComparator;
 import com.jaamsim.units.DimensionlessUnit;
@@ -924,17 +923,14 @@ public abstract class Input<T> {
 	}
 
 	/**
-	 * Parse an RFC8601 date time and return it as an offset in microseconds from
-	 * 0AD. This assumes a very simple concept of a 365 day year with no leap years
-	 * and no leap seconds.
-	 *
+	 * Parse an RFC8601 date time and returns the corresponding simulation time in seconds from the
+	 * start of the simulation run.
 	 * An RFC8601 date time looks like YYYY-MM-DD HH:MM:SS.mmm or YYYY-MM-DDTHH:MM:SS.mmm
-	 *
-	 * @param input
-	 * @param datumYear
-	 * @return
+	 * @param simModel - JaamSimModel
+	 * @param input - date string
+	 * @return simulation time in seconds
 	 */
-	public static long parseRFC8601DateTime(JaamSimModel simModel, String input) {
+	public static double parseRFC8601DateTime(JaamSimModel simModel, String input) {
 		if (is8601time.matcher(input).matches()) {
 			int YY = Integer.parseInt(input.substring(0, 4));
 			int MM = Integer.parseInt(input.substring(5, 7));
@@ -1039,7 +1035,7 @@ public abstract class Input<T> {
 		daysInMonth[11] = 31;
 	}
 
-	private static final long getUS(JaamSimModel simModel, String input, int YY, int MM, int DD, int hh, int mm, int ss, int us) {
+	private static final double getUS(JaamSimModel simModel, String input, int YY, int MM, int DD, int hh, int mm, int ss, int us) {
 		// Validate ranges
 		if (MM <= 0 || MM > 12)
 			throw new InputErrorException(INP_ERR_BADDATE, input);
@@ -1054,8 +1050,7 @@ public abstract class Input<T> {
 			throw new InputErrorException(INP_ERR_BADDATE, input);
 
 		long millis = simModel.getCalendarMillis(YY, MM - 1, DD, hh, mm, ss, 0);
-		double simTime = simModel.calendarMillisToSimTime(millis) + us/1e6d;
-		return EventManager.secsToNearestTick(simTime);
+		return simModel.calendarMillisToSimTime(millis) + us/1e6d;
 	}
 
 	public static double parseDouble(String data)
@@ -1124,7 +1119,7 @@ public abstract class Input<T> {
 
 					// RFC8601 date/time format
 					if (Input.isRFC8601DateTime(kw.getArg(i))) {
-						double element = Input.parseRFC8601DateTime(simModel, kw.getArg(i))/1e6;
+						double element = Input.parseRFC8601DateTime(simModel, kw.getArg(i));
 						if (element < minValue || element > maxValue)
 							throw new InputErrorException(INP_ERR_DOUBLERANGE, minValue, maxValue, temp);
 						temp.add(element);

src/main/java/com/jaamsim/BasicObjects/FileToArray.java
@@ -121,7 +121,7 @@ public abstract class FileToArray extends LinkedComponent {
 		// Is the entry a time stamp?
 		if (Input.isRFC8601DateTime(str)) {
 			try {
-				double time = Input.parseRFC8601DateTime(simModel, str)/1e6;
+				double time = Input.parseRFC8601DateTime(simModel, str);
 				return ExpResult.makeNumResult(time, TimeUnit.class);
 			}
 			catch (Exception e) {}

src/main/java/com/jaamsim/input/TimeSeriesDataInput.java
@@ -23,6 +23,7 @@ import com.jaamsim.Samples.TimeSeriesData;
 import com.jaamsim.basicsim.Entity;
 import com.jaamsim.basicsim.JaamSimModel;
 import com.jaamsim.datatypes.DoubleVector;
+import com.jaamsim.events.EventManager;
 import com.jaamsim.units.DimensionlessUnit;
 import com.jaamsim.units.TimeUnit;
 import com.jaamsim.units.Unit;
@@ -85,7 +86,8 @@ public class TimeSeriesDataInput extends Input<TimeSeriesData> {
 			long recordus;
 			if (Input.isRFC8601DateTime(each.get(0))) {
 				Input.assertCountRange(each, 2, 3);
-				recordus = Input.parseRFC8601DateTime(simModel, each.get(0));
+				double simTime = Input.parseRFC8601DateTime(simModel, each.get(0));
+				recordus = EventManager.secsToNearestTick(simTime);
 				each.remove(0);
 			}
 			// Time input in number/unit format

commit b8e4e6c8866244fb5dd0d94b38d44fda8fd3ca68

JS: fix bug in parsing a number in the time formats 'hh:mm:ss' and 'hh:mm:ss.ssssss'
Fixes bug introduced in c374ff3ebc6d11f67ac2ee1336f92a2fb96c6ff2
(JS: return simTime from 'parseRFC8601DateTime' instead of clock ticks)

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/Input.java
@@ -967,7 +967,7 @@ public abstract class Input<T> {
 			ret += hh * usPerHr;
 			ret += mm * usPerMin;
 			ret += ss * usPerSec;
-			return ret;
+			return ret * 1e-6;
 		}
 
 		if (isextendfull.matcher(input).matches()) {
@@ -995,7 +995,7 @@ public abstract class Input<T> {
 			ret += mm * usPerMin;
 			ret += ss * usPerSec;
 			ret += us;
-			return ret;
+			return ret * 1e-6;
 		}
 
 		throw new InputErrorException(INP_ERR_BADDATE, input);