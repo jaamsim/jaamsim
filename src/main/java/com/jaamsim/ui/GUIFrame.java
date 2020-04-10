jaamsim/jaamsim

commit 255a31f68a5de106f6e9e418251d7bc496033164

JS: ignore a command if no inputs are changed
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -3593,6 +3593,8 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 	@Override
 	public void storeAndExecute(Command cmd) {
 		synchronized (undoList) {
+			if (!cmd.isChange())
+				return;
 
 			// Execute the command and catch an error if it occurs
 			cmd.execute();

commit 2318f190a4e987c29dadc93400584c8ba8af3c32

JS: alway execute the command in 'storeAndExecute'
Fixes a bug caused by commit 255a31f68a5de106f6e9e418251d7bc496033164
(JS: ignore a command if no inputs are changed).

For some reason, an entity became frozen if the user attempted to re-size
it using the drag handles.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -3692,11 +3692,12 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 	@Override
 	public void storeAndExecute(Command cmd) {
 		synchronized (undoList) {
-			if (!cmd.isChange())
-				return;
 
 			// Execute the command and catch an error if it occurs
 			cmd.execute();
+
+			if (!cmd.isChange())
+				return;
 
 			// Attempt to merge the command with the previous one
 			Command mergedCmd = null;