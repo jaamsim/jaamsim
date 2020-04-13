7e3ecb56495869bbe8c907dba5c4500429afe0bd
Harry King <d.harry.king@gmail.com>
2020-04-13 09:45:03 -0700

    JS: add javadocs to EventTraceListener interface
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/events/EventTraceListener.java
+++ src/main/java/com/jaamsim/events/EventTraceListener.java
@@ -1,6 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2014 Ausenco Engineering Canada Inc.
+ * Copyright (C) 2020 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,19 +20,86 @@ package com.jaamsim.events;
 
 public interface EventTraceListener {
 
+/**
+ * Called at the start of execution for an event.
+ * @param e - event processor
+ * @param curTick - present simulation time in clock ticks
+ * @param tick - time for the next event in clock ticks
+ * @param priority - priority of the event being executed
+ * @param t - holds the method to be executed by the event
+ */
 public void traceEvent(EventManager e, long curTick, long tick, int priority, ProcessTarget t);
 
+/**
+ * Called when a future event is scheduled by a 'wait' method.
+ * @param e - event processor
+ * @param curTick - present simulation time in clock ticks
+ * @param tick - time for the next event in clock ticks
+ * @param priority - priority of the event to be executed
+ * @param t - holds the method to be executed by the event
+ */
 public void traceWait(EventManager e, long curTick, long tick, int priority, ProcessTarget t);
 
+/**
+ * Called when a future event is scheduled by a 'schedule' or 'scheduleProcessExternal' method.
+ * @param e - event processor
+ * @param curTick - present simulation time in clock ticks
+ * @param tick - time for the next event in clock ticks
+ * @param priority - priority of the event to be executed
+ * @param t - holds the method to be executed by the event
+ */
 public void traceSchedProcess(EventManager e, long curTick, long tick, int priority, ProcessTarget t);
 
+/**
+ * Called when a new process is started by a 'startProcess' method.
+ * @param e - event processor
+ * @param t - holds the method to be executed by the process
+ * @param tick - present simulation time in clock ticks
+ */
 public void traceProcessStart(EventManager e, ProcessTarget t, long tick);
+
+/**
+ * Called when a process finishes execution.
+ * @param e - event processor
+ * @param tick - present simulation time in clock ticks
+ */
 public void traceProcessEnd(EventManager e, long tick);
 
+/**
+ * Called at the start of execution for a future event that has been re-scheduled for immediate
+ * execution.
+ * @param e - event processor
+ * @param curTick - present simulation time in clock ticks
+ * @param tick - time for the next event in clock ticks
+ * @param priority - priority of the event being executed
+ * @param t - holds the method to be executed by the event
+ */
 public void traceInterrupt(EventManager e, long curTick, long tick, int priority, ProcessTarget t);
+
+/**
+ * Called when a future event is cancelled.
+ * @param e - event processor
+ * @param curTick - present simulation time in clock ticks
+ * @param tick - time for the next event in clock ticks
+ * @param priority - priority of the event being cancelled
+ * @param t - holds the method to be executed by the cancelled event
+ */
 public void traceKill(EventManager e, long curTick, long tick, int priority, ProcessTarget t);
 
+/**
+ * Called when a conditional event has been scheduled by a 'scheduleUntil' or 'waitUntil' method.
+ * @param e - event processor
+ * @param tick - present simulation time in clock ticks
+ */
 public void traceWaitUntil(EventManager e, long tick);
+
+/**
+ * Called when a conditional event's condition has been satisfied and the event is scheduled for
+ * execution.
+ * @param e - event processor
+ * @param tick - present simulation time in clock ticks
+ * @param t - holds the method to be executed by the conditional event
+ */
 public void traceWaitUntilEnded(EventManager e, long tick, ProcessTarget t);
 
 }
