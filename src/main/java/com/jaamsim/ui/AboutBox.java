dd5d8a370d5483a06735450df84efd762863f503
Harry King <d.harry.king@gmail.com>
2020-05-01 16:55:49 -0700

    JS: update version to 2020-05
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ui/AboutBox.java
+++ src/main/java/com/jaamsim/ui/AboutBox.java
@@ -33,7 +33,7 @@ import javax.swing.JTextArea;
 public class AboutBox extends FrameBox implements ActionListener {
 	private static AboutBox instance;
 
-	public static final String version = "2020-04";
+	public static final String version = "2020-05";
 
 	public AboutBox() {
 		super("About");
