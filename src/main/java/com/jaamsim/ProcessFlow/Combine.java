ad314b05b6c867dce1cbe5bf9529cd608b07181e
Harry King <d.harry.king@gmail.com>
2020-07-08 18:28:18 -0700

    JS: modify Combine to extend AbstractCombine
    Adds the keywords MatchRequired and NumberRequired to Combine.
    
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ProcessFlow/Combine.java
+++ src/main/java/com/jaamsim/ProcessFlow/Combine.java
@@ -19,60 +19,33 @@ package com.jaamsim.ProcessFlow;
 
 import java.util.ArrayList;
 
-import com.jaamsim.Commands.KeywordCommand;
 import com.jaamsim.Graphics.DisplayEntity;
-import com.jaamsim.Samples.SampleConstant;
-import com.jaamsim.Samples.SampleInput;
+import com.jaamsim.datatypes.IntegerVector;
 import com.jaamsim.input.BooleanInput;
-import com.jaamsim.input.EntityListInput;
-import com.jaamsim.input.InputAgent;
 import com.jaamsim.input.Keyword;
-import com.jaamsim.input.KeywordIndex;
-import com.jaamsim.units.TimeUnit;
 
-public class Combine extends LinkedService {
-
-	@Keyword(description = "The service time required to perform the assembly process.",
-	         exampleList = { "3.0 h", "NormalDistribution1", "'1[s] + 0.5*[TimeSeries1].PresentValue'" })
-	private final SampleInput serviceTime;
-
-	@Keyword(description = "A list of Queue objects in which to place the arriving sub-component entities.",
-	         exampleList = {"Queue1 Queue2 Queue3"})
-	private final EntityListInput<Queue> waitQueueList;
+public class Combine extends AbstractCombine {
 
 	@Keyword(description = "If TRUE, all the matching entities are passed to the next component.\n"
 	                     + "If FALSE, only the entity in the first queue is passed on.",
 	         exampleList = {"TRUE"})
 	private final BooleanInput retainAll;
 
-	private DisplayEntity[] processedEntityList;  // entities being processed
+	private ArrayList<DisplayEntity> processedEntityList;  // entities being processed
 
 	{
-		waitQueue.setHidden(true);
-		match.setHidden(true);
-		watchList.setHidden(true);
-
-		serviceTime = new SampleInput("ServiceTime", KEY_INPUTS, new SampleConstant(TimeUnit.class, 0.0));
-		serviceTime.setUnitType(TimeUnit.class);
-		serviceTime.setValidRange(0, Double.POSITIVE_INFINITY);
-		this.addInput(serviceTime);
-
-		waitQueueList = new EntityListInput<>(Queue.class, "WaitQueueList", KEY_INPUTS, new ArrayList<Queue>());
-		waitQueueList.setRequired(true);
-		this.addInput(waitQueueList);
-
 		retainAll = new BooleanInput("RetainAll", KEY_INPUTS, false);
 		this.addInput(retainAll);
 	}
 
 	public Combine() {
-		processedEntityList = new DisplayEntity[1];
+		processedEntityList = new ArrayList<>();
 	}
 
 	@Override
 	public void earlyInit() {
 		super.earlyInit();
-		processedEntityList = new DisplayEntity[waitQueueList.getListSize()];
+		processedEntityList.clear();
 	}
 
 	@Override
@@ -81,47 +54,44 @@ public class Combine extends LinkedService {
 	}
 
 	@Override
-	public void addQueue(Queue que) {
-		ArrayList<String> toks = new ArrayList<>();
-		waitQueueList.getValueTokens(toks);
-		toks.add(que.getName());
-		KeywordIndex kw = new KeywordIndex(waitQueueList.getKeyword(), toks, null);
-		InputAgent.storeAndExecute(new KeywordCommand(this, kw));
-	}
-
-	@Override
-	public ArrayList<Queue> getQueues() {
-		return waitQueueList.getValue();
-	}
-
-	@Override
 	protected boolean startProcessing(double simTime) {
 
 		// Do the queues have enough entities?
-		ArrayList<Queue> queueList = waitQueueList.getValue();
-		String m = Queue.selectMatchValue(queueList, null);
-		if (m == null) {
-			return false;
+		ArrayList<Queue> queueList = getQueues();
+		IntegerVector numList = getNumberRequired(simTime);
+		if (isMatchRequired()) {
+			String m = Queue.selectMatchValue(queueList, numList);
+			if (m == null) {
+				return false;
+			}
+			this.setMatchValue(m);
 		}
-		this.setMatchValue(m);
+		else {
+			if (!Queue.sufficientEntities(queueList, numList, null)) {
+				return false;
+			}
+		}
 
-		// Remove one entity from each queue
+		// Remove the appropriate entities from each queue
 		// (performed in reverse order so that obj is set to the entity in the first queue)
 		for (int i=queueList.size()-1; i>=0; i--) {
-			DisplayEntity ent = queueList.get(i).removeFirstForMatch(m);
-			if (ent == null)
-				error("An entity with the specified match value %s was not found in %s.",
-						m, queueList.get(i));
+			int ind = Math.min(i, numList.size() - 1);
+			for (int n = 0; n < numList.get(ind); n++) {
+				DisplayEntity ent = queueList.get(i).removeFirstForMatch(getMatchValue());
+				if (ent == null)
+					error("An entity with the specified match value %s was not found in %s.",
+							getMatchValue(), queueList.get(i));
 
-			// Destroy all the entities but the first
-			if (i > 0 && !retainAll.getValue()) {
-				ent.kill();
-				continue;
+				// Destroy all the entities but the first
+				if ((i > 0 || n > 0) && !retainAll.getValue()) {
+					ent.kill();
+					continue;
+				}
+
+				receiveEntity(ent);
+				setEntityState(ent);
+				processedEntityList.add(ent);
 			}
-
-			receiveEntity(ent);
-			setEntityState(ent);
-			processedEntityList[i] = ent;
 		}
 
 		return true;
@@ -132,34 +102,28 @@ public class Combine extends LinkedService {
 
 		// If specified, send all the entities to the next component
 		if (retainAll.getValue()) {
-			for (int i=0; i<processedEntityList.length; i++) {
-				this.sendToNextComponent(processedEntityList[i]);
-				processedEntityList[i] = null;
+			for (DisplayEntity ent : processedEntityList) {
+				sendToNextComponent(ent);
 			}
 		}
 
 		// Otherwise, send just the first one
 		else {
-			this.sendToNextComponent(processedEntityList[0]);
-			processedEntityList[0] = null;
+			sendToNextComponent(processedEntityList.get(0));
 		}
-	}
-
-	@Override
-	protected double getStepDuration(double simTime) {
-		return serviceTime.getValue().getNextSample(simTime);
+		processedEntityList.clear();
 	}
 
 	@Override
 	public boolean isFinished() {
-		return processedEntityList[0] == null;
+		return processedEntityList.isEmpty();
 	}
 
 	@Override
 	public void updateGraphics(double simTime) {
-		if (processedEntityList[0] == null)
+		if (processedEntityList.isEmpty())
 			return;
-		moveToProcessPosition(processedEntityList[0]);
+		moveToProcessPosition(processedEntityList.get(0));
 	}
 
 }
