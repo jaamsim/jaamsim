87bbf5068df8d792fbf8336025dcf99a4b17240a
Harry King <d.harry.king@gmail.com>
2020-07-25 14:42:23 -0700

    JS: set the tooltip position for the ExpressionBox output menu
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ui/ExpressionBox.java
+++ src/main/java/com/jaamsim/ui/ExpressionBox.java
@@ -23,6 +23,7 @@ import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
@@ -797,10 +798,16 @@ public class ExpressionBox extends JDialog {
 		Collections.sort(handles, Input.uiSortOrder);
 
 		for (final OutputHandle hand : handles) {
-			JMenuItem item = new JMenuItem(hand.getName());
+			JMenuItem item = new JMenuItem(hand.getName()) {
+				@Override
+				public Point getToolTipLocation(MouseEvent e) {
+					return new Point(outputMenu.getWidth(), -getY());
+				}
+			};
 			item.setToolTipText(GUIFrame.formatOutputToolTip(
 					hand.getName(),
 					hand.getDescription()) );
+
 			item.addActionListener( new ActionListener() {
 
 				@Override
