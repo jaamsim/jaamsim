9583bb08411ec67623e2e6c0fde260817407e0ff
Harry King <d.harry.king@gmail.com>
2020-07-26 17:55:31 -0700

    JS: allow FindBox to retain its last position
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ui/FindBox.java
+++ src/main/java/com/jaamsim/ui/FindBox.java
@@ -19,8 +19,6 @@ package com.jaamsim.ui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GraphicsEnvironment;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -96,6 +94,9 @@ public class FindBox extends JDialog {
 		buttonPanel.add(closeButton);
 		getContentPane().add(buttonPanel, BorderLayout.CENTER);
 		pack();
+
+		// Set initial position in middle of screen
+		setLocationRelativeTo(null);
 
 		// Dropdown button pressed
 		dropdown.addActionListener(new ActionListener() {
@@ -228,12 +229,6 @@ public class FindBox extends JDialog {
 	}
 
 	public void showDialog(String str) {
-
-		// Position the finder at the centre of the screen
-		Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-		setLocation((winSize.width - getWidth())/2, (winSize.height - getHeight())/2);
-
-		// Show the dialog box and wait for editing to finish
 		searchText.setText(str);
 		this.setVisible(true);
 	}
