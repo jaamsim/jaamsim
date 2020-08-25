05212f180d91af0b58e95030da12b73327c20a9e
Harvey Harrison <harvey.harrison@gmail.com>
2020-08-24 17:17:00 -0700

    JS update version to 2020-11
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/main/java/com/jaamsim/ui/AboutBox.java
+++ src/main/java/com/jaamsim/ui/AboutBox.java
@@ -33,7 +33,7 @@ import javax.swing.JTextArea;
  */
 public class AboutBox extends JDialog {
 
-	public static final String version = "2020-10";
+	public static final String version = "2020-11";
 
 	public AboutBox() {
 		super((JDialog)null, "About", true);
