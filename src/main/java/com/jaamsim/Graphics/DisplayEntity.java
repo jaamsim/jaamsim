51e12d197758c96543f05c01cd26f4756552e605
Harry King <d.harry.king@gmail.com>
2020-09-10 11:57:06 -0700

    JS: use new methods in 'isGraphicsNominal'
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/DisplayEntity.java
+++ src/main/java/com/jaamsim/Graphics/DisplayEntity.java
@@ -400,19 +400,9 @@ public class DisplayEntity extends Entity {
 	}
 
 	public boolean isGraphicsNominal() {
-		boolean ret = position.equals3(positionInput.getValue());
-		ret = ret && size.equals3(sizeInput.getValue());
-		ret = ret && align.equals3(alignmentInput.getValue());
-		ret = ret && orient.equals3(orientationInput.getValue());
-
-		ret = ret && (displayModelList.isEmpty() && displayModelListInput.getValue() == null
-				|| displayModelList.equals(displayModelListInput.getValue()));
-
-		ret = ret && (currentRegion == null && regionInput.getValue() == null
-				|| currentRegion != null && currentRegion.equals(regionInput.getValue()));
-
-		ret = ret && show == showInput.getValue();
-		return ret;
+		return isPositionNominal() && isSizeNominal() && isAlignmentNominal()
+				&& isOrientationNominal() && isDisplayModelNominal() && isRegionNominal()
+				&& isShowNominal();
 	}
 
 	private void showStandardGraphicsKeywords(boolean bool) {
