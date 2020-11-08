jaamsim/jaamsim

commit 5d685fe72781b737e4afbb035023bf17bcc5b716

JS: add FocusListener to EditTable
Sets the selected 'Value' cell in edit mode.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/EditBox.java
@@ -18,6 +18,8 @@
 package com.jaamsim.ui;
 
 import java.awt.Point;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -366,6 +368,19 @@ public static class EditTable extends JTable {
 		this.getTableHeader().setReorderingAllowed(false);
 
 		this.setPresentCellEditor(null);
+
+		addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				int row = getSelectedRow();
+				editCellAt(row, VALUE_COLUMN);
+				if (getEditorComponent() != null) {
+					getEditorComponent().requestFocusInWindow();
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {}
+		});
 	}
 
 	@Override

commit c7b0de935a9d85c15a0f3798fef273acd0b4a721

JS: ensure that editing is finished in EditBox before starting a new CellEditor
Fixes a bug in the Input Editor that caused an input change to be ignored
when the arrow button's location is clicked without first selecting the
cell. The bug was introduced in 5d685fe72781b737e4afbb035023bf17bcc5b716
(JS: add FocusListener to EditTable).

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/EditBox.java
@@ -391,7 +391,8 @@ public static class EditTable extends JTable {
 				int row = getSelectedRow();
 				if (row == -1)
 					return;
-				editCellAt(row, VALUE_COLUMN);
+				if (presentCellEditor == null)
+					editCellAt(row, VALUE_COLUMN);
 				if (getEditorComponent() == null)
 					return;
 				getEditorComponent().requestFocusInWindow();