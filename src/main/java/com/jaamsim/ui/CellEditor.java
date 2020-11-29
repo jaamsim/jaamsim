jaamsim/jaamsim

commit 7288340bc034c2624a4094124ca62a768e383280

JS: update the value in the selected cell for the Input Editor
Fixes a bug that prevented the value in the selected cell from changing
when the Undo button was clicked. This bug affected the earlier version of
the Input Editor, but was more noticeable after the change to the new
spreadsheet style of inputs where a cell can be selected without being in
edit mode.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/CellEditor.java
@@ -243,6 +243,12 @@ public abstract class CellEditor extends AbstractCellEditor implements TableCell
 		return false;
 	}
 
+	public void updateValue() {
+		if (text.hasFocus())
+			return;
+		text.setText(input.getValueString());
+	}
+
 	@Override
 	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
 

src/main/java/com/jaamsim/ui/EditBox.java
@@ -175,13 +175,18 @@ public class EditBox extends FrameBox {
 		if(currentEntity == null)
 			return;
 
-		JTable propTable = (JTable)(((JScrollPane)jTabbedFrame.getSelectedComponent()).getViewport().getComponent(0));
+		EditTable propTable = (EditTable)(((JScrollPane)jTabbedFrame.getSelectedComponent()).getViewport().getComponent(0));
 		int row = propTable.getSelectedRow();
 		((EditTableModel)propTable.getModel()).fireTableDataChanged();
 
 		// Restore the selected cell
 		if (row != -1)
 			propTable.changeSelection(row, VALUE_COLUMN, false, false);
+
+		// Update the value in the selected cell
+		if (propTable.getPresentCellEditor() != null) {
+			propTable.getPresentCellEditor().updateValue();
+		}
 	}
 
 	private synchronized static void killInstance() {

commit 0adcf24b50deb8c606aaff160f75394e964ba1ef

JS: retain the retryString in CellEditor
Fixes a bug introduced by 7288340bc034c2624a4094124ca62a768e383280
(JS: update the value in the selected cell for the Input Editor) that
prevents the 'Edit' option from working correctly when an input error is
made. Note that it is not necessary to reset the retryString property to
null because the CellEditor is discarded after editing is stopped.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/CellEditor.java
@@ -275,7 +275,7 @@ public abstract class CellEditor extends AbstractCellEditor implements TableCell
 	}
 
 	public void updateValue() {
-		if (text.hasFocus())
+		if (text.hasFocus() || retryString != null)
 			return;
 		text.setText(input.getValueString());
 	}
@@ -290,7 +290,6 @@ public abstract class CellEditor extends AbstractCellEditor implements TableCell
 		String val = input.getValueString();
 		if (canRetry() && retryString != null) {
 			val = retryString;
-			retryString = null;
 		}
 		text.setText(val);
 