1e47d0884d7969f3204cdff056f82987b61ee5cd
Harry King <d.harry.king@gmail.com>
2020-12-03 07:26:34 -0800

    JS: fix bug in 'ensureIndexIsVisible' in ScrollablePopupMenu
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ui/ScrollablePopupMenu.java
+++ src/main/java/com/jaamsim/ui/ScrollablePopupMenu.java
@@ -1,6 +1,6 @@
 /*
  * JaamSim Discrete Event Simulation
- * Copyright (C) 2018 JaamSim Software Inc.
+ * Copyright (C) 2018-2020 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -107,6 +107,7 @@ public class ScrollablePopupMenu extends JPopupMenu {
 
 	public void ensureIndexIsVisible(int index) {
 		Rectangle rect = getComponent(index + 1).getBounds();  // scrollBar is the first component
+		rect.y += scrollBar.getValue();
 		scrollBar.scrollRectToVisible(rect);
 	}
 
