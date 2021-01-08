jaamsim/jaamsim

commit 52c05b4e584eaedaefc88c4e61e23a857c54b916

JS: call clearButtons and clearUndoRedo in 'setJaamSimModel'
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -431,6 +431,8 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 		ObjectSelector.allowUpdate();
 		gui.resetViews();
 		gui.setTitle(sm);
+		gui.clearButtons();
+		gui.clearUndoRedo();
 
 		// Set the listeners for the new model
 		sm.setTimeListener(gui);
@@ -4455,7 +4457,6 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 
 		// Set the Control Panel to the new JaamSimModel and reset the user interface
 		setJaamSimModel(simModel);
-		clear();
 
 		// Load the default model
 		sim.setRecordEdits(true);
@@ -4495,7 +4496,6 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 
 			// Set the Control Panel to the new JaamSimModel and reset the user interface
 			setJaamSimModel(simModel);
-			clear();
 
 			// Load the selected input file
 			SwingUtilities.invokeLater(new Runnable() {

src/main/java/com/jaamsim/ui/ExampleBox.java
@@ -147,7 +147,6 @@ public class ExampleBox extends JDialog {
 
 				// Display the new model
 				GUIFrame.setJaamSimModel(simModel);
-				GUIFrame.getInstance().clear();
 				FrameBox.setSelectedEntity(simModel.getSimulation(), false);
 			}
 		});

commit b3ee7a4408b09f87d697bd2c4dfbb5e0b0c0e9af

JS: check 'FindBox.hasInstance()' in 'updateFindButton' for GUIFrame
Fixes a bug introduced by 52c05b4e584eaedaefc88c4e61e23a857c54b916
(JS: call clearButtons and clearUndoRedo in 'setJaamSimModel')
that caused JaamSim to appear in back when it is launched.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/FindBox.java
@@ -1,6 +1,6 @@
 /*
  * JaamSim Discrete Event Simulation
- * Copyright (C) 2019-2020 JaamSim Software Inc.
+ * Copyright (C) 2019-2021 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -102,6 +102,10 @@ public class FindBox extends JDialog {
 		if (myInstance == null)
 			myInstance = new FindBox();
 		return myInstance;
+	}
+
+	public synchronized static boolean hasInstance() {
+		return (myInstance != null);
 	}
 
 	private synchronized static void killInstance() {

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -3533,7 +3533,7 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 	}
 
 	private void updateFindButton() {
-		boolean bool = FindBox.getInstance().isVisible();
+		boolean bool = FindBox.hasInstance() && FindBox.getInstance().isVisible();
 		find.setSelected(bool);
 	}
 