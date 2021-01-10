jaamsim/jaamsim

commit b1fbb67554ffd313f56880f44cf18629d23f2404

JS: remove unnecessary ChangeListener for EditBox
Commit eb6601887203e1671776d5a9145fcef86c99d7cc
(JS: accept an input edit if the cell loses focus)
automatically accepts the edited value for an input if the Input Editor's
tab is changed.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/EditBox.java
@@ -29,9 +29,6 @@ import java.util.Comparator;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
-import javax.swing.SwingUtilities;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
@@ -63,7 +60,6 @@ public class EditBox extends FrameBox {
 	private final JTabbedPane jTabbedFrame;
 
 	private final ArrayList<EditTable> editTableList;
-	private int prevTab;
 
 	private final TableCellRenderer columnRender = new EditBoxColumnRenderer();
 
@@ -79,26 +75,6 @@ public class EditBox extends FrameBox {
 
 		// Provide tabs for the editor
 		jTabbedFrame = new JTabbedPane();
-		jTabbedFrame.addChangeListener(new ChangeListener()  {
-
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				if (prevTab != -1 && editTableList.size() > prevTab) {
-					final CellEditor editor = editTableList.get(prevTab).getPresentCellEditor();
-					if (editor != null) {
-						// Wait for a change in selected entity to take effect
-						SwingUtilities.invokeLater(new Runnable() {
-							@Override
-							public void run() {
-								editor.stopCellEditing();
-							}
-						});
-					}
-				}
-				prevTab = jTabbedFrame.getSelectedIndex();
-				GUIFrame.updateUI();
-			}
-		});
 		getContentPane().add(jTabbedFrame);
 
 		// Save changes to the editor's size and position
@@ -154,12 +130,6 @@ public class EditBox extends FrameBox {
 				initialTab = curTab;
 
 			curTab++;
-		}
-
-		prevTab = -1;
-		if (jTabbedFrame.getTabCount() > 0) {
-			jTabbedFrame.setSelectedIndex(initialTab);
-			prevTab = initialTab;
 		}
 
 		// Set the keyword
@@ -250,7 +220,6 @@ public class EditBox extends FrameBox {
 
 	public void setTab(int tab) {
 		jTabbedFrame.setSelectedIndex(tab);
-		prevTab = tab;
 	}
 
 	public static String formatEditorText(String str) {

commit c8e7d6bb94a5112dd5ee7e7dd9fa4111526db78e

JS: set correct tab in EditBox
Fixes a bug introduced by b1fbb67554ffd313f56880f44cf18629d23f2404
(JS: remove unnecessary ChangeListener for EditBox)
that deleted these two lines of code as part of a larger change.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/EditBox.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2005-2013 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2020 JaamSim Software Inc.
+ * Copyright (C) 2016-2021 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -133,6 +133,8 @@ public class EditBox extends FrameBox {
 		}
 
 		// Set the keyword
+		if (jTabbedFrame.getTabCount() > 0)
+			jTabbedFrame.setSelectedIndex(initialTab);
 		editTableList.get(initialTab).selectKeyword(lastKeyword);
 	}
 