38742d312888c384461c6eef0a3461ae5fdb8f97
Harry King <d.harry.king@gmail.com>
2021-06-25 18:06:38 -0700

    JS: add a FocusListener to SearchField and FindBox
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ui/FindBox.java
+++ src/main/java/com/jaamsim/ui/FindBox.java
@@ -20,6 +20,8 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -96,6 +98,16 @@ public class FindBox extends JDialog {
 				setVisible(false);
 			}
 		} );
+
+		// Focus on the search text
+		addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				searchText.requestFocusInWindow();
+			}
+			@Override
+			public void focusLost(FocusEvent e) {}
+		});
 	}
 
 	public synchronized static FindBox getInstance() {
--- src/main/java/com/jaamsim/ui/SearchField.java
+++ src/main/java/com/jaamsim/ui/SearchField.java
@@ -20,6 +20,8 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
@@ -142,6 +144,16 @@ public abstract class SearchField extends JPanel {
 			@Override
 			public void changedUpdate(DocumentEvent e) {}
 	    });
+
+		// Focus on the search text
+		addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				topicSearch.requestFocusInWindow();
+			}
+			@Override
+			public void focusLost(FocusEvent e) {}
+		});
 	}
 
 	@Override
