cf69e9c7f42bb50035a0050f2c02975c8abd82b2
Harry King <d.harry.king@gmail.com>
2021-07-19 15:23:12 -0700

    JS: add 'NumberOfDuplicates' keyword to Duplicate
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ProcessFlow/Duplicate.java
+++ src/main/java/com/jaamsim/ProcessFlow/Duplicate.java
@@ -21,6 +21,9 @@ import java.util.ArrayList;
 
 import com.jaamsim.Commands.KeywordCommand;
 import com.jaamsim.Graphics.DisplayEntity;
+import com.jaamsim.Samples.SampleConstant;
+import com.jaamsim.Samples.SampleListInput;
+import com.jaamsim.Samples.SampleProvider;
 import com.jaamsim.basicsim.Entity;
 import com.jaamsim.input.InputAgent;
 import com.jaamsim.input.InterfaceEntityListInput;
@@ -28,13 +31,21 @@ import com.jaamsim.input.Keyword;
 import com.jaamsim.input.KeywordIndex;
 import com.jaamsim.input.StringInput;
 import com.jaamsim.states.StateEntity;
+import com.jaamsim.units.DimensionlessUnit;
 
 public class Duplicate extends LinkedComponent {
 
-	@Keyword(description = "The list of components that will receive the duplicated entities. " +
-			"One duplicated entity will be sent to each entry in the list.",
+	@Keyword(description = "The list of components that will receive one or more duplicated entities.",
 	         exampleList = {"Assign1 Queue1"})
 	protected final InterfaceEntityListInput<Linkable> targetComponentList;
+
+	@Keyword(description = "The number of duplicated entities to be sent to each target. "
+	                     + "The last value in the list is used if the number of targets is greater "
+	                     + "than the number of values. "
+	                     + "Only an integer number of entities can be handled. "
+	                     + "A decimal value will be truncated to an integer.",
+	         exampleList = {"2 1", "{ 2 } { 1 }", "{ DiscreteDistribution1 } { 'this.obj.attrib1 + 1' }"})
+	private final SampleListInput numberOfDuplicates;
 
 	@Keyword(description = "The base for the names assigned to the duplicated entities. "
 	                     + "The duplicated entities will be named Name1, Name2, etc.",
@@ -47,12 +58,37 @@ public class Duplicate extends LinkedComponent {
 		targetComponentList.setRequired(true);
 		this.addInput( targetComponentList);
 
+		ArrayList<SampleProvider> def = new ArrayList<>();
+		def.add(new SampleConstant(1));
+		numberOfDuplicates = new SampleListInput("NumberOfDuplicates", KEY_INPUTS, def);
+		numberOfDuplicates.setDimensionless(true);
+		numberOfDuplicates.setUnitType(DimensionlessUnit.class);
+		numberOfDuplicates.setIntegerValue(true);
+		this.addInput(numberOfDuplicates);
+
 		baseName = new StringInput("BaseName", KEY_INPUTS, null);
 		baseName.setDefaultText("EntityName_Dup");
 		this.addInput(baseName);
 	}
 
 	public Duplicate() {}
+
+	public int[] getNumberOfDuplicates(double simTime) {
+
+		// Number of targets entered by the user
+		int[] ret = new int[targetComponentList.getListSize()];
+		for (int i = 0; i < numberOfDuplicates.getListSize(); i++) {
+			int n = (int) numberOfDuplicates.getValue().get(i).getNextSample(simTime);
+			ret[i] = n;
+		}
+
+		// Additional copies of the last value needed to complete the list
+		int lastVal = ret[numberOfDuplicates.getListSize() - 1];
+		for (int i = numberOfDuplicates.getListSize(); i < targetComponentList.getListSize(); i++) {
+			ret[i] = lastVal;
+		}
+		return ret;
+	}
 
 	@Override
 	public void addEntity(DisplayEntity ent) {
@@ -68,29 +104,33 @@ public class Duplicate extends LinkedComponent {
 
 		// Make the duplicates and send them to the targets
 		int n = 1;
-		for (Linkable target : targetComponentList.getValue()) {
+		int[] num = getNumberOfDuplicates(simTime);
+		for (int index = 0; index < targetComponentList.getListSize(); index++) {
+			Linkable target = targetComponentList.getValue().get(index);
+			for (int i = 0; i < num[index]; i++) {
 
-			// Create the duplicated entity
-			DisplayEntity dup = InputAgent.generateEntityWithName(getJaamSimModel(), ent.getClass(), name + n);
-			Entity.fastCopyInputs(ent, dup);
+				// Create the duplicated entity
+				DisplayEntity dup = InputAgent.generateEntityWithName(getJaamSimModel(), ent.getClass(), name + n);
+				Entity.fastCopyInputs(ent, dup);
 
-			// Set the state for the duplicated entity
-			if (dup instanceof StateEntity) {
-				String state = ((StateEntity) ent).getPresentState(simTime);
-				((StateEntity) dup).setPresentState(state);
+				// Set the state for the duplicated entity
+				if (dup instanceof StateEntity) {
+					String state = ((StateEntity) ent).getPresentState(simTime);
+					((StateEntity) dup).setPresentState(state);
+				}
+
+				// Set the graphics for the duplicated entity
+				dup.setRegion(ent.getCurrentRegion());
+				dup.setPosition(ent.getPosition());
+				dup.setDisplayModelList(ent.getDisplayModelList());
+				dup.setSize(ent.getSize());
+				dup.setOrientation(ent.getOrientation());
+				dup.setAlignment(ent.getAlignment());
+
+				// Send the duplicate to the target component
+				target.addEntity(dup);
+				n++;
 			}
-
-			// Set the graphics for the duplicated entity
-			dup.setRegion(ent.getCurrentRegion());
-			dup.setPosition(ent.getPosition());
-			dup.setDisplayModelList(ent.getDisplayModelList());
-			dup.setSize(ent.getSize());
-			dup.setOrientation(ent.getOrientation());
-			dup.setAlignment(ent.getAlignment());
-
-			// Send the duplicate to the target component
-			target.addEntity(dup);
-			n++;
 		}
 
 		// Send the received entity to the next component
