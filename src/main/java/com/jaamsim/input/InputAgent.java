jaamsim/jaamsim

commit b21fe4c5e23ffdc34ada45b50d20a7d8ba22c7e2

JS: show un-encoded file paths for FileInputs in the Input Editor
Fixes a bug caused by double encoding of file paths. When this occurred,
any spaces in the file path that were encoded as %20 were converted to
%2520, where %25 is the code for the percent symbol.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/InputAgent.java
@@ -1695,8 +1695,8 @@ public class InputAgent {
 	static public String getRelativeFilePath(JaamSimModel simModel, URI uri) {
 
 		// Relativize the file path against the resources folder
-		String resString = resRoot.toString();
-		String inputString = uri.toString();
+		String resString = resRoot.getPath();
+		String inputString = uri.getPath();
 		if (inputString.startsWith(resString)) {
 			return String.format("<res>/%s", inputString.substring(resString.length()));
 		}

commit 8315f347ba9f3567f89b5916ded9cbe73f1c301e

JS: show un-encoded file paths CORRECTLY for FileInputs in the Input Editor
Fixes a bug introduced by commit b21fe4c5e23ffdc34ada45b50d20a7d8ba22c7e2
(JS: show un-encoded file paths for FileInputs in the Input Editor.)

The method getPath cannot be used for a resource folder URI. It works in
Eclipse, but not in an executable.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/InputAgent.java
@@ -24,10 +24,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -1730,10 +1733,15 @@ public class InputAgent {
 	static public String getRelativeFilePath(JaamSimModel simModel, URI uri) {
 
 		// Relativize the file path against the resources folder
-		String resString = resRoot.getPath();
-		String inputString = uri.getPath();
+		String resString = resRoot.toString();
+		String inputString = uri.toString();
 		if (inputString.startsWith(resString)) {
-			return String.format("<res>/%s", inputString.substring(resString.length()));
+			inputString = inputString.substring(resString.length());
+			try {
+				inputString = URLDecoder.decode(inputString, StandardCharsets.UTF_8.name());
+			}
+			catch (UnsupportedEncodingException e) {}
+			return String.format("<res>/%s", inputString);
 		}
 
 		// Relativize the file path against the configuration file