jaamsim/jaamsim

commit a6b7b5950ca5b7c01d0f566ed351e026375ba22f

JS: update the RunProgressBox through GUIFrame.updateUI
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -3052,6 +3052,12 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 		int progress = (int) Math.round(simulation.getProgress(simTime) * 100.0d);
 		this.setProgress(progress);
 
+		// Show the overall progress in JaamSim's title bar
+		if (sim.getSimState() >= JaamSimModel.SIM_STATE_CONFIGURED) {
+			int overallProgress = (int) Math.round(runManager.getProgress() * 100.0d);
+			setTitle(sim, overallProgress);
+		}
+
 		// Do nothing further if the simulation is not executing events
 		if (sim.getSimState() != JaamSimModel.SIM_STATE_RUNNING)
 			return;
@@ -3094,13 +3100,6 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 		progressBar.setValue( val );
 		progressBar.repaint(25);
 		lastValue = val;
-
-		// Show the overall progress in JaamSim's title bar
-		JaamSimModel sim = getJaamSimModel();
-		if (sim.getSimState() >= JaamSimModel.SIM_STATE_CONFIGURED) {
-			int overallProgress = (int) Math.round(runManager.getProgress() * 100.0d);
-			setTitle(sim, overallProgress);
-		}
 	}
 
 	/**

src/main/java/com/jaamsim/basicsim/RunManager.java
@@ -131,7 +131,7 @@ public class RunManager implements RunListener {
 	public void runEnded(SimRun run) {
 		Simulation simulation = simModel.getSimulation();
 		if (RunProgressBox.hasInstance())
-			RunProgressBox.getInstance().update();
+			GUIFrame.updateUI();
 
 		// Print the output report
 		if (simulation.getPrintReport())

commit 83dcf74cd2faa989ce05726172eeb0257522e9ca

JS: fix deadlock in validation error dialog
Fixes a bug introduced by a6b7b5950ca5b7c01d0f566ed351e026375ba22f
(JS: update the RunProgressBox through GUIFrame.updateUI) that caused the
Swing thread to deadlock when an error occured during validation in a model
that had not been saved already. When this occurred, only part of the error
message popup appeared and it could not be closed in the normal fashion.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -4686,7 +4686,7 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 	public void handleInputError(Throwable t, Entity ent) {
 		JaamSimModel sim = getJaamSimModel();
 		InputAgent.logMessage(sim, "Validation Error - %s: %s", ent.getName(), t.getMessage());
-		GUIFrame.showErrorDialog("Input Error",
+		GUIFrame.invokeErrorDialog("Input Error",
 				"JaamSim has detected the following input error during validation:",
 				String.format("%s: %-70s", ent.getName(), t.getMessage()),
 				"The error must be corrected before the simulation can be started.");