jaamsim/jaamsim

commit f050b55d15193f6f51755b02421c0fd63f1eb950

JS: calculate 'NumberOfEntities' and 'NumberToStart' once for each EntityContainer
Fixes a bug that prevented a probability distribution from being used with
either input. Previously, the values were re-sampled for every SimEntity
that was received.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ProcessFlow/AbstractPack.java
@@ -54,6 +54,7 @@ public abstract class AbstractPack extends LinkedService {
 	protected EntContainer container;	// the generated EntityContainer
 	private int numberInserted;   // Number of entities inserted to the EntityContainer
 	private int numberToInsert;   // Number of entities to insert in the present EntityContainer
+	private int numberToStartPacking;  // Number of entities required before packing can begin
 	private boolean startedPacking;  // True if the packing process has already started
 	private DisplayEntity packedEntity;  // the entity being packed
 
@@ -91,6 +92,8 @@ public abstract class AbstractPack extends LinkedService {
 		super.earlyInit();
 		container = null;
 		numberInserted = 0;
+		numberToInsert = -1;
+		numberToStartPacking = -1;
 		startedPacking = false;
 		packedEntity = null;
 	}
@@ -110,29 +113,36 @@ public abstract class AbstractPack extends LinkedService {
 	@Override
 	protected boolean startProcessing(double simTime) {
 
-		// If necessary, get a new container
-		if (container == null && !waitForEntities.getValue() && isContainerAvailable()) {
-			container = this.getNextContainer();
-			numberInserted = 0;
-			setContainerState();
+		// A container must be available before packing can start
+		if (container == null) {
+			if (!isContainerAvailable())
+				return false;
+
+			// If necessary, get a new container
+			if (!waitForEntities.getValue()) {
+				container = this.getNextContainer();
+				setContainerState();
+			}
 		}
 
 		// Are there sufficient entities in the queue to start packing?
 		if (!startedPacking) {
 			String m = this.getNextMatchValue(simTime);
-			numberToInsert = this.getNumberToInsert(simTime);
-			if (getQueue(simTime).getCount(m) < getNumberToStart(simTime)) {
+			if (numberToStartPacking < 0)
+				numberToStartPacking = this.getNumberToStart(simTime);
+			if (getQueue(simTime).getCount(m) < numberToStartPacking) {
 				return false;
 			}
 
 			// If necessary, get a new container
 			if (container == null) {
-				if (!isContainerAvailable())
-					return false;
 				container = this.getNextContainer();
-				numberInserted = 0;
 				setContainerState();
 			}
+
+			// Start packing
+			numberInserted = 0;
+			numberToInsert = this.getNumberToInsert(simTime);
 			startedPacking = true;
 			this.setMatchValue(m);
 		}
@@ -165,6 +175,8 @@ public abstract class AbstractPack extends LinkedService {
 			getNextComponent().addEntity((DisplayEntity) container);
 			container = null;
 			numberInserted = 0;
+			numberToInsert = -1;
+			numberToStartPacking = -1;
 			startedPacking = false;
 		}
 	}

commit 33855fb62548d22669224ed150e42c5335d9c129

JS: select 'numberToInsert' before 'numberToStartPacking' in AbstractPack
Fixes a bug introduced by f050b55d15193f6f51755b02421c0fd63f1eb950
(JS: calculate 'NumberOfEntities' and 'NumberToStart' once for each
EntityContainer)

'numberToInsert' must be selected before 'numberToStartPacking' because
the method getNumberToStart uses numberToInsert as its default value.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ProcessFlow/AbstractPack.java
@@ -128,8 +128,10 @@ public abstract class AbstractPack extends LinkedService {
 		// Are there sufficient entities in the queue to start packing?
 		if (!startedPacking) {
 			String m = this.getNextMatchValue(simTime);
-			if (numberToStartPacking < 0)
+			if (numberToStartPacking < 0) {
+				numberToInsert = this.getNumberToInsert(simTime);
 				numberToStartPacking = this.getNumberToStart(simTime);
+			}
 			if (getQueue(simTime).getCount(m) < numberToStartPacking) {
 				return false;
 			}
@@ -142,7 +144,6 @@ public abstract class AbstractPack extends LinkedService {
 
 			// Start packing
 			numberInserted = 0;
-			numberToInsert = this.getNumberToInsert(simTime);
 			startedPacking = true;
 			this.setMatchValue(m);
 		}