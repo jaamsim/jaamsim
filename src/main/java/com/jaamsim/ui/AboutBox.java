df3f416e5723181258e3d640e65352237d77bd7c
Harvey Harrison <harvey.harrison@gmail.com>
2022-07-27 10:26:38 -0700

    JS: update version to 2022-06
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/main/java/com/jaamsim/ui/AboutBox.java
+++ src/main/java/com/jaamsim/ui/AboutBox.java
@@ -33,7 +33,7 @@ import javax.swing.JTextArea;
  */
 public class AboutBox extends JDialog {
 
-	public static final String version = "2022-05";
+	public static final String version = "2022-06";
 
 	public AboutBox() {
 		super((JDialog)null, "About", true);
