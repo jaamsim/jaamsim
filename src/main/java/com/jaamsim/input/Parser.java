jaamsim/jaamsim

commit c9397291edbb3959fdb6332fb3c4b2e5bda5c038

JS: modify 'tokenize' to process a record that is already in the quoted state
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/Parser.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2013 Ausenco Engineering Canada Inc.
- * Copyright (C) 2018 JaamSim Software Inc.
+ * Copyright (C) 2018-2022 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,30 @@ public class Parser {
  * @return true if in quoted context
  */
 public static final boolean tokenize(ArrayList<String> tokens, String rec, boolean stripComments) {
+	return tokenize(tokens, rec, false, stripComments);
+}
+
+public static final boolean tokenize(ArrayList<String> tokens, String rec, boolean quoted, boolean stripComments) {
+
+	// Already in the quoted state
+	int recStart = 0;
+	if (quoted) {
+		for (int i = 0; i < rec.length(); i++) {
+			char c = rec.charAt(i);
+			if (c == '\'') {
+				String lastRec = tokens.remove(tokens.size() - 1);
+				tokens.add(lastRec + rec.substring(0, i));
+				recStart = i + 1;
+				break;
+			}
+		}
+		if (recStart == 0) {
+			String lastRec = tokens.remove(tokens.size() - 1);
+			tokens.add(lastRec + rec);
+			return true;
+		}
+	}
+
 	// Records can be divided into two pieces, the contents portion and possibly
 	// a commented portion, the division point is the first " character, if no
 	// quoting in a record, the entire line is contents for tokenizing
@@ -39,7 +63,7 @@ public static final boolean tokenize(ArrayList<String> tokens, String rec, boole
 	int quoteStart = -1;
 	int cIndex = -1;
 	int endOfRec = rec.length();
-	for (int i = 0; i < rec.length(); i++) {
+	for (int i = recStart; i < rec.length(); i++) {
 		char c = rec.charAt(i);
 		if (c == '\'') {
 			// end the current token

src/main/java/com/jaamsim/input/InputAgent.java
@@ -193,6 +193,7 @@ public class InputAgent {
 		try {
 			ArrayList<String> record = new ArrayList<>();
 			int braceDepth = 0;
+			boolean quoted = false;
 
 			ParseContext pc = new ParseContext(resolved, root);
 
@@ -204,26 +205,8 @@ public class InputAgent {
 					break;
 				line = str;
 
-				ArrayList<String> previousRecord = new ArrayList<>(record);
 				int previousRecordSize = record.size();
-				boolean quoted = Parser.tokenize(record, line, true);
-
-				// Keep reading lines if the end of line was hit while in quoted context
-				while (quoted) {
-
-					// Append the next line to the line
-					String nextLine = buf.readLine();
-					if (nextLine == null)  // end of file
-						break;
-					StringBuilder sb = new StringBuilder(line);
-					sb.append('\n');
-					sb.append(nextLine);
-					line = sb.toString();
-
-					// Clear the record and tokenize the now longer line
-					record = new ArrayList<>(previousRecord);
-					quoted = Parser.tokenize(record, line, true);
-				}
+				quoted = Parser.tokenize(record, line, quoted, true);
 
 				// Print the inputs to the .log file
 				simModel.logMessage(line);
@@ -235,12 +218,10 @@ public class InputAgent {
 					InputAgent.logError(simModel, "Invalid brace depth: %s", braceDepth);
 					record.clear();
 					braceDepth = 0;
+					quoted = false;
 				}
 
-				if( braceDepth > 0 )
-					continue;
-
-				if (record.size() == 0)
+				if( braceDepth > 0 || quoted || record.isEmpty())
 					continue;
 
 				// Process the input lines

commit c126e9fae31db875be4a84f5359d321a88d76fd3

JS: retain newline character within single quotes in 'tokenize' for Parser
Fixes a bug introduced by c9397291edbb3959fdb6332fb3c4b2e5bda5c038
(JS: modify 'tokenize' to process a record that is already in the quoted state)

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/Parser.java
@@ -40,6 +40,8 @@ public static final boolean tokenize(ArrayList<String> tokens, String rec, boole
 	// Already in the quoted state
 	int recStart = 0;
 	if (quoted) {
+
+		// Find the closing single quote
 		for (int i = 0; i < rec.length(); i++) {
 			char c = rec.charAt(i);
 			if (c == '\'') {
@@ -49,9 +51,11 @@ public static final boolean tokenize(ArrayList<String> tokens, String rec, boole
 				break;
 			}
 		}
+
+		// If no closing single quote is found, then append the entire record to the previous token
 		if (recStart == 0) {
 			String lastRec = tokens.remove(tokens.size() - 1);
-			tokens.add(lastRec + rec);
+			tokens.add(lastRec + rec + '\n');
 			return true;
 		}
 	}
@@ -118,7 +122,7 @@ public static final boolean tokenize(ArrayList<String> tokens, String rec, boole
 		tokens.add(rec.substring(tokStart, endOfRec));
 
 	if (quoteStart != -1)
-		tokens.add(rec.substring(quoteStart + 1, endOfRec));
+		tokens.add(rec.substring(quoteStart + 1, endOfRec) + '\n');
 
 	// add comments if they exist including the leading # to denote it as commented
 	if (!stripComments && cIndex > -1)

src/test/java/com/jaamsim/input/TestParser.java
@@ -1,6 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2013 Ausenco Engineering Canada Inc.
+ * Copyright (C) 2022 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +53,7 @@ public void testTokenize() {
 
 	tok.clear();
 	Parser.tokenize(tok, "'OBJECT''KEYWORD''   ", false);
-	tokenMatch(tok, "OBJECT", "KEYWORD", "   ");
+	tokenMatch(tok, "OBJECT", "KEYWORD", "   \n");
 
 	tok.clear();
 	Parser.tokenize(tok, "'OBJECT\"{#}''KEYWORD''   'a#bcd ", false);