985ea1e1d333b12adfc1e9620fc555f2943dc996
Harry King <d.harry.king@gmail.com>
2023-01-01 17:51:24 -0800

    JS: detect an edited value for an input that is the same as its inherited value
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/input/Input.java
+++ src/main/java/com/jaamsim/input/Input.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2010-2012 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2022 JaamSim Software Inc.
+ * Copyright (C) 2016-2023 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -334,6 +334,10 @@ public abstract class Input<T> {
 	@SuppressWarnings("unchecked")
 	public void setProtoInput(Input<?> in) {
 		protoInput = (Input<T>) in;
+	}
+
+	public Input<T> getProtoInput() {
+		return protoInput;
 	}
 
 	public void setHidden(boolean hide) {
