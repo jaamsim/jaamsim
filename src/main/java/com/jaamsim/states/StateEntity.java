99ecdd33b36559cc290dd9d2858e3b5a5398e14c
Harry King <d.harry.king@gmail.com>
2023-01-07 15:05:37 -0800

    JS: remove the interning of state strings in StateEntity
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/states/StateEntity.java
+++ src/main/java/com/jaamsim/states/StateEntity.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2014 Ausenco Engineering Canada Inc.
- * Copyright (C) 2018-2022 JaamSim Software Inc.
+ * Copyright (C) 2018-2023 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -312,8 +312,7 @@ public abstract class StateEntity extends DisplayEntity implements StateUser {
 	}
 
 	private StateRecord createRecord(String state) {
-		String stateName = getJaamSimModel().internString(state);
-		return new StateRecord(stateName, isValidWorkingState(stateName));
+		return new StateRecord(state, isValidWorkingState(state));
 	}
 
 	public void addState(String str) {
