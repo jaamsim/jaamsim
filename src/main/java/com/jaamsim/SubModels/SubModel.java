19c4722d3536b74811f6a7ced29766314523ade9
Harry King <d.harry.king@gmail.com>
2023-01-08 16:44:58 -0800

    JS: move all content from AbstractSubModel to SubModel
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/SubModels/SubModel.java
+++ src/main/java/com/jaamsim/SubModels/SubModel.java
@@ -1,6 +1,6 @@
 /*
  * JaamSim Discrete Event Simulation
- * Copyright (C) 2018-2022 JaamSim Software Inc.
+ * Copyright (C) 2018-2023 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,14 +18,21 @@ package com.jaamsim.SubModels;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
 import com.jaamsim.basicsim.Entity;
 import com.jaamsim.basicsim.GUIListener;
 import com.jaamsim.basicsim.JaamSimModel;
+import com.jaamsim.input.ExpressionHandle;
+import com.jaamsim.input.ExpressionInput;
 import com.jaamsim.input.Input;
 import com.jaamsim.input.InputCallback;
 import com.jaamsim.input.Keyword;
+import com.jaamsim.input.ValueHandle;
+import com.jaamsim.input.ExpParser.Expression;
 import com.jaamsim.ui.DragAndDropable;
+import com.jaamsim.units.Unit;
 
 public class SubModel extends AbstractSubModel implements DragAndDropable {
 
@@ -36,6 +43,10 @@ public class SubModel extends AbstractSubModel implements DragAndDropable {
 	         exampleList = {"{ ServiceTime TimeUnit } { NumberOfUnits }"})
 	protected final PassThroughListInput keywordListInput;
 
+	protected ArrayList<PassThroughData> keywordList;
+	private ArrayList<ExpressionInput> newInputList;
+	private final LinkedHashMap<String, ValueHandle> inputOutputMap = new LinkedHashMap<>();
+
 	public static final String PALETTE_NAME = "Pre-built SubModels";
 
 	{
@@ -45,6 +56,9 @@ public class SubModel extends AbstractSubModel implements DragAndDropable {
 	}
 
 	public SubModel() {
+		keywordList = new ArrayList<>();
+		newInputList = new ArrayList<>();
+
 		GUIListener gui = getJaamSimModel().getGUIListener();
 		if (gui != null)
 			gui.updateModelBuilder();
@@ -63,6 +77,15 @@ public class SubModel extends AbstractSubModel implements DragAndDropable {
 			GUIListener gui = sm.getJaamSimModel().getGUIListener();
 			if (gui != null && gui.isSelected(sm))
 				gui.updateInputEditor();
+		}
+	};
+
+	static final InputCallback subModelKeywordCallback = new InputCallback() {
+		@Override
+		public void callback(Entity ent, Input<?> inp) {
+			ExpressionInput expIn = (ExpressionInput)inp;
+			SubModel subModel = (SubModel) ent;
+			subModel.addInputAsOutput(expIn.getKeyword(), expIn.getValue(), expIn.getUnitType());
 		}
 	};
 
@@ -89,6 +112,83 @@ public class SubModel extends AbstractSubModel implements DragAndDropable {
 			return;
 		super.validate();
 	}
+
+	public void setKeywordList(ArrayList<PassThroughData> list) {
+		keywordList = new ArrayList<>(list);
+	}
+
+	public ArrayList<PassThroughData> getKeywordList() {
+		return keywordList;
+	}
+
+	/**
+	 * Updates the added keywords to match the specified list.
+	 * @param newDataList - data for the new list of added keywords
+	 */
+	void updateKeywords(ArrayList<PassThroughData> newDataList) {
+
+		// Do nothing if the keywords are unchanged
+		if (newDataList.equals(keywordList))
+			return;
+
+		// Remove the old inputs and outputs
+		for (ExpressionInput in : newInputList) {
+			removeInput(in);
+			removeInputAsOutput(in.getKeyword());
+		}
+
+		// Add the new keywords, using the old ones whenever possible to save their input values
+		ArrayList<ExpressionInput> list = new ArrayList<>(newDataList.size());
+		for (PassThroughData data : newDataList) {
+			int index = keywordList.indexOf(data);
+			ExpressionInput in = null;
+			if (index == -1) {
+				in = new ExpressionInput(data.getName(), KEY_INPUTS, null);
+				in.setUnitType(data.getUnitType());
+				in.setCallback(subModelKeywordCallback);
+				in.setRequired(true);
+			}
+			else {
+				in = newInputList.get(index);
+			}
+			addInput(in);
+			addInputAsOutput(in.getKeyword(), in.getValue(), in.getUnitType());
+			list.add(in);
+		}
+		newInputList = list;
+		setKeywordList(newDataList);
+	}
+
+	private void addInputAsOutput(String name, Expression exp, Class<? extends Unit> unitType) {
+		ExpressionHandle eh = new ExpressionHandle(this, exp, name, unitType);
+		inputOutputMap.put(name, eh);
+	}
+
+	private void removeInputAsOutput(String name) {
+		inputOutputMap.remove(name);
+	}
+
+	@Override
+	public ValueHandle getOutputHandle(String outputName) {
+		ValueHandle ret = inputOutputMap.get(outputName);
+		if (ret != null)
+			return ret;
+
+		return super.getOutputHandle(outputName);
+	}
+
+	@Override
+	public ArrayList<ValueHandle> getAllOutputs() {
+		ArrayList<ValueHandle> ret = super.getAllOutputs();
+
+		// Add the Inputs as Outputs
+		for (Entry<String, ValueHandle> e : inputOutputMap.entrySet()) {
+			ret.add(e.getValue());
+		}
+
+		return ret;
+	}
+
 
 	public void updateClones() {
 		for (SubModelClone clone : getClones()) {
