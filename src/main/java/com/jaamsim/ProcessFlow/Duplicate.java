jaamsim/jaamsim

commit a4b159cce3d42bdeaa130f7e6feb50ec559a6d23

JS: create new AttributeHandle in 'addAttribute' for Entity
Require for an entity's clones to have separate AttributeHandles.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/basicsim/Entity.java
@@ -924,7 +924,11 @@ public class Entity {
 	}
 
 	private void addAttribute(String name, AttributeHandle h) {
-		attributeMap.put(name, h);
+		AttributeHandle ah = new AttributeHandle(this, h.getName());
+		ah.setInitialValue(h.getInitialValue());
+		ah.setValue(h.copyValue());
+		ah.setUnitType(h.getUnitType());
+		attributeMap.put(name, ah);
 	}
 
 	public boolean hasAttribute(String name) {

commit 441aa0e1813903605804e441b336dd537b9b71c6

JS: add method 'copyAttributeValues' to Entity and use in Duplicate
Fixes a bug in the Duplicate object introduced by commit:
a4b159cce3d42bdeaa130f7e6feb50ec559a6d23
(JS: create new AttributeHandle in 'addAttribute' for Entity)
which caused the duplicated entities to have the initial value for their
attributes instead of the present value. This occurred because the commit
caused the AttributeHandles recorded by the 'attributeMap'
property to be different from the ones recorded by the
'AttributeDefinitionList' input.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ProcessFlow/Duplicate.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2014 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2022 JaamSim Software Inc.
+ * Copyright (C) 2016-2023 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,6 +108,7 @@ public class Duplicate extends LinkedComponent {
 				// Create the duplicated entity
 				DisplayEntity dup = InputAgent.generateEntityWithName(getJaamSimModel(), ent.getClass(), name + n);
 				Entity.fastCopyInputs(ent, dup);
+				Entity.copyAttributeValues(ent, dup);
 
 				// Set the state for the duplicated entity
 				if (dup instanceof StateEntity) {

src/main/java/com/jaamsim/basicsim/Entity.java
@@ -550,6 +550,20 @@ public class Entity {
 		}
 	}
 
+	/**
+	 * Copies the present attribute values from one entity to another.
+	 * @param ent - entity whose attribute values are to be copied
+	 * @param target - entity whose attribute values are to be assigned
+	 */
+	public static void copyAttributeValues(Entity ent, Entity target) {
+		for (AttributeHandle sourceHandle : ent.attributeMap.values()) {
+			AttributeHandle targetHandle = target.attributeMap.get(sourceHandle.getName());
+			if (targetHandle == null)
+				continue;
+			targetHandle.setValue(sourceHandle.copyValue());
+		}
+	}
+
 	public ArrayList<Entity> getEntityReferences() {
 		ArrayList<Entity> ret = new ArrayList<>();
 		for (Input<?> inp : inpList) {