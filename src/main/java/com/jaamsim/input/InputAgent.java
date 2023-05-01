jaamsim/jaamsim

commit 256dad81fb836cd42965c0f4d6014efe6e376368

JS: copy input changes for an entity to its clones
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/InputAgent.java
@@ -574,11 +574,12 @@ public class InputAgent {
 			ent.setEdited();
 		}
 
-		// Execute the input callback for the entity and its clones
+		// Execute the input callback for the entity
 		in.doCallback(ent);
+
+		// Copy the input value to any clones
 		for (Entity clone : ent.getAllClones()) {
-			Input<?> cloneIn = clone.getInput(in.getKeyword());
-			cloneIn.doCallback(clone);
+			clone.copyInput(ent, in.getKeyword(), kw.context, true);
 		}
 
 		// Refresh the graphics

src/main/java/com/jaamsim/basicsim/Entity.java
@@ -527,6 +527,7 @@ public class Entity {
 		if (getPrototype() == ent && !changed) {
 			// Clear the stub value set for a custom output
 			targetInput.reset();
+			targetInput.doCallback(this);
 			return;
 		}
 

commit d6315901954144383131a9395cb93ba7f1fb471e

JS: disable 'RecordEdits' while an entity's input is copied to its clones
Fixes a bug introduced in 256dad81fb836cd42965c0f4d6014efe6e376368
(JS: copy input changes for an entity to its clones)
that caused the inputs that were copied to a clone to be saved to the cfg
file.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/InputAgent.java
@@ -581,9 +581,12 @@ public class InputAgent {
 		in.doCallback(ent);
 
 		// Copy the input value to any clones
+		boolean bool = simModel.isRecordEdits();
+		simModel.setRecordEdits(false);
 		for (Entity clone : ent.getAllClones()) {
 			clone.copyInput(ent, in.getKeyword(), kw.context, true);
 		}
+		simModel.setRecordEdits(bool);
 
 		// Refresh the graphics
 		GUIListener gui = ent.getJaamSimModel().getGUIListener();