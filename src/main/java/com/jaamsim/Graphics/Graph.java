b0f3d1f2ae5c58dfb747bd37d1a104180a7cd198
Harry King <d.harry.king@gmail.com>
2023-06-10 18:20:52 -0700

    JS: override 'setInputsForDragAndDrop' for Graph
    Prevents the Alignment from being set to (0, 0. -0.5).
    
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/Graph.java
+++ src/main/java/com/jaamsim/Graphics/Graph.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2009-2012 Ausenco Engineering Canada Inc.
- * Copyright (C) 2017 JaamSim Software Inc.
+ * Copyright (C) 2017-2023 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -247,6 +247,9 @@ public class Graph extends GraphBasics  {
 			info.lineWidth = getLineWidth(i, secondaryLineWidths.getValue());
 		}
 	}
+
+	@Override
+	public void setInputsForDragAndDrop() {}
 
 	@Override
 	public void earlyInit(){
