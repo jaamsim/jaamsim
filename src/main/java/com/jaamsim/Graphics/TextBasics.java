60df4ec830f0c9440b9b2325a4671bf2a318d6eb
Harry King <d.harry.king@gmail.com>
2023-07-13 11:45:03 -0700

    JS: use new 'getDisplayModel' method in TextBasics
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/TextBasics.java
+++ src/main/java/com/jaamsim/Graphics/TextBasics.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2015 Ausenco Engineering Canada Inc.
- * Copyright (C) 2018-2021 JaamSim Software Inc.
+ * Copyright (C) 2018-2023 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -318,7 +318,7 @@ public abstract class TextBasics extends AbstractShape implements TextEntity, Ed
 	}
 
 	public TextModel getTextModel() {
-		return (TextModel) getDisplayModel();
+		return getDisplayModel(TextModel.class);
 	}
 
 	public boolean isDefault() {
