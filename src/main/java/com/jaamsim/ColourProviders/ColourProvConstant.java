0f15404b4b2cdd004b355d96156b1d9e3e475077
Harry King <d.harry.king@gmail.com>
2023-07-25 09:46:43 -0700

    JS: add class 'ColourProvConstant'
    Signed-off-by: Harry King <d.harry.king@gmail.com>

---
+++ src/main/java/com/jaamsim/ColourProviders/ColourProvConstant.java
@@ -0,0 +1,45 @@
+/*
+ * JaamSim Discrete Event Simulation
+ * Copyright (C) 2023 JaamSim Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jaamsim.ColourProviders;
+
+import java.util.ArrayList;
+
+import com.jaamsim.basicsim.Entity;
+import com.jaamsim.math.Color4d;
+
+public class ColourProvConstant implements ColourProvider {
+
+	private final Color4d val;
+
+	public ColourProvConstant(Color4d col) {
+		val = col;
+	}
+
+	@Override
+	public Color4d getNextColour(Entity thisEnt, double simTime) {
+		return val;
+	}
+
+	@Override
+	public void appendEntityReferences(ArrayList<Entity> list) {}
+
+	@Override
+	public String toString() {
+		return val.toString();
+	}
+
+}
