jaamsim/jaamsim

commit 0fe3c166d2f606126c9e11eee712b17678d5013b

JS: add logic for showing/hiding sub-models to 'getShow' for DisplayEntity
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/Graphics/DisplayEntity.java
@@ -29,6 +29,7 @@ import com.jaamsim.DisplayModels.ImageModel;
 import com.jaamsim.DisplayModels.PolylineModel;
 import com.jaamsim.DisplayModels.ShapeModel;
 import com.jaamsim.DisplayModels.TextModel;
+import com.jaamsim.SubModels.CompoundEntity;
 import com.jaamsim.basicsim.Entity;
 import com.jaamsim.basicsim.ErrorException;
 import com.jaamsim.basicsim.GUIListener;
@@ -611,11 +612,17 @@ public class DisplayEntity extends Entity {
 	public boolean getShow(double simTime) {
 		if (isPooled())
 			return false;
+		boolean ret;
 		if (!showInput.isConstant())
-			return getShowInput(simTime);
+			ret = getShowInput(simTime);
 		synchronized (position) {
-			return show;
+			ret = show;
 		}
+		if (getParent() instanceof CompoundEntity) {
+			CompoundEntity sub = (CompoundEntity) getParent();
+			ret = ret && (sub.getShowComponents() || getSimulation().isShowSubModels());
+		}
+		return ret;
 	}
 
 	public boolean getShowInput(double simTime) {

commit 4145a24dcc8c9a0513e27e7d8efcf71b340baea5

JS: fix logic in 'getShow' for DisplayEntity
Fixes a bug introduced by 0fe3c166d2f606126c9e11eee712b17678d5013b
(JS: add logic for showing/hiding sub-models to 'getShow' for DisplayEntity)
which caused an expression input to be ignored.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/Graphics/DisplayEntity.java
@@ -613,11 +613,15 @@ public class DisplayEntity extends Entity {
 		if (isPooled())
 			return false;
 		boolean ret;
-		if (!showInput.isConstant())
+		if (!showInput.isConstant()) {
 			ret = getShowInput(simTime);
-		synchronized (position) {
-			ret = show;
 		}
+		else {
+			synchronized (position) {
+				ret = show;
+			}
+		}
+
 		if (getParent() instanceof CompoundEntity) {
 			CompoundEntity sub = (CompoundEntity) getParent();
 			ret = ret && (sub.getShowComponents() || getSimulation().isShowSubModels());