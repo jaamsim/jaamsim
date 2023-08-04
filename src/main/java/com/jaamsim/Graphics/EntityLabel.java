jaamsim/jaamsim

commit 0623c8db052a605a437eb930a46531fe6f16f252

JS: update the EntityLabel for a submodel component when the name of the submodel is changed
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/Graphics/EntityLabel.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2015 Ausenco Engineering Canada Inc.
- * Copyright (C) 2018-2021 JaamSim Software Inc.
+ * Copyright (C) 2018-2022 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,6 +97,22 @@ public class EntityLabel extends TextBasics {
 	@Override
 	public boolean isGraphicsNominal() {
 		return true;
+	}
+
+	@Override
+	public String getText() {
+		if (isEditMode())
+			return super.getText();
+		Entity ent = targetEntity.getValue();
+		if (ent == null)
+			return "ERROR";
+		return ent.getName();
+	}
+
+	@Override
+	public void setEditMode(boolean bool) {
+		updateForTargetNameChange();
+		super.setEditMode(bool);
 	}
 
 	@Override

commit 293487e98bf31766cda58c8a5725a21256d6fc87

JS: trap null entity name in 'getText' for EntityLabel
Fixes a bug introduced by 0623c8db052a605a437eb930a46531fe6f16f252
(JS: update the EntityLabel for a submodel component when the name of the
submodel is changed)

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/Graphics/EntityLabel.java
@@ -107,7 +107,7 @@ public class EntityLabel extends TextBasics {
 		if (isEditMode())
 			return super.getText();
 		Entity ent = targetEntity.getValue();
-		if (ent == null)
+		if (ent == null || ent.getName() == null)
 			return "ERROR";
 		return ent.getName();
 	}