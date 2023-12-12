7165a05187fc36436742bb41dc650e2e14cb7b05
Harry King <d.harry.king@gmail.com>
2023-12-11 17:50:28 -0800

    JS: delete redundant inputs 'SeriesColours' and 'SecondarySeriesColours' from XYGraph
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/XYGraph.java
+++ src/main/java/com/jaamsim/Graphics/XYGraph.java
@@ -16,14 +16,9 @@
  */
 package com.jaamsim.Graphics;
 
-import java.util.ArrayList;
-
 import com.jaamsim.Samples.SampleListInput;
 import com.jaamsim.input.BooleanInput;
-import com.jaamsim.input.ColorListInput;
-import com.jaamsim.input.ColourInput;
 import com.jaamsim.input.Keyword;
-import com.jaamsim.math.Color4d;
 import com.jaamsim.units.UserSpecifiedUnit;
 
 public class XYGraph extends AbstractGraph {
@@ -35,13 +30,6 @@ public class XYGraph extends AbstractGraph {
 	         exampleList = {"{ Entity1 Output1 } { Entity2 Output2 }"})
 	protected final SampleListInput dataSource;
 
-	@Keyword(description = "A list of colors for the primary series to be displayed.\n"
-	                     + "Each color can be specified by either a color keyword or an RGB value.\n"
-	                     + "For multiple series, each color must be enclosed in braces.\n"
-	                     + "If only one color is provided, it is used for all the series.",
-	         exampleList = "XYGraph1 SeriesColors { { red } { green } }")
-	protected final ColorListInput seriesColorsList;
-
 	@Keyword(description = "Set to TRUE if the primary series are to be shown as bars instead of lines.")
 	protected final BooleanInput showBars;
 
@@ -49,13 +37,6 @@ public class XYGraph extends AbstractGraph {
 	                     + "Each source is graphed as a separate line or bar and is specified by an Entity and its Output.",
 	         exampleList = {"{ Entity1 Output1 } { Entity2 Output2 }"})
 	protected final SampleListInput secondaryDataSource;
-
-	@Keyword(description = "A list of colors for the secondary series to be displayed.\n"
-	                     + "Each color can be specified by either a color keyword or an RGB value.\n"
-	                     + "For multiple series, each color must be enclosed in braces.\n"
-	                     + "If only one color is provided, it is used for all the series.",
-	         exampleList = {"{ red } { green }"})
-	protected final ColorListInput secondarySeriesColorsList;
 
 	@Keyword(description = "Set to TRUE if the secondary series are to be shown as bars instead of lines.")
 	protected final BooleanInput secondaryShowBars;
@@ -67,26 +48,12 @@ public class XYGraph extends AbstractGraph {
 		dataSource.setUnitType(UserSpecifiedUnit.class);
 		this.addInput(dataSource);
 
-		ArrayList<Color4d> defSeriesColor = new ArrayList<>(0);
-		defSeriesColor.add(ColourInput.getColorWithName("red"));
-		seriesColorsList = new ColorListInput("SeriesColours", KEY_INPUTS, defSeriesColor);
-		seriesColorsList.setValidCountRange(1, Integer.MAX_VALUE);
-		this.addInput(seriesColorsList);
-		this.addSynonym(seriesColorsList, "LineColors");
-
 		showBars = new BooleanInput("ShowBars", KEY_INPUTS, false);
 		this.addInput(showBars);
 
 		secondaryDataSource = new SampleListInput("SecondaryDataSource", KEY_INPUTS, null);
 		secondaryDataSource.setUnitType(UserSpecifiedUnit.class);
 		this.addInput(secondaryDataSource);
-
-		ArrayList<Color4d> defSecondaryLineColor = new ArrayList<>(0);
-		defSecondaryLineColor.add(ColourInput.getColorWithName("black"));
-		secondarySeriesColorsList = new ColorListInput("SecondarySeriesColours", KEY_INPUTS, defSecondaryLineColor);
-		secondarySeriesColorsList.setValidCountRange(1, Integer.MAX_VALUE);
-		this.addInput(secondarySeriesColorsList);
-		this.addSynonym(secondarySeriesColorsList, "SecondaryLineColors");
 
 		secondaryShowBars = new BooleanInput("SecondaryShowBars", KEY_INPUTS, false);
 		this.addInput(secondaryShowBars);
