jaamsim/jaamsim

commit 86cbaad19c3715d6e8b44ee3aaba1514f9ddc038

JS: override 'applyConditioning' for SampleListInput, StringProvListInput, and EntityProvListInput
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/Samples/SampleListInput.java
@@ -27,6 +27,7 @@ import com.jaamsim.basicsim.JaamSimModel;
 import com.jaamsim.input.Input;
 import com.jaamsim.input.InputErrorException;
 import com.jaamsim.input.KeywordIndex;
+import com.jaamsim.input.Parser;
 import com.jaamsim.input.ArrayListInput;
 import com.jaamsim.units.DimensionlessUnit;
 import com.jaamsim.units.Unit;
@@ -104,6 +105,14 @@ public class SampleListInput extends ArrayListInput<SampleProvider> {
 	public void setValidRange(double min, double max) {
 		minValue = min;
 		maxValue = max;
+	}
+
+	@Override
+	public String applyConditioning(String str) {
+		if (!str.contains("{")) {
+			return str;
+		}
+		return Parser.addSubstringQuotesIfNeeded(str);
 	}
 
 	@Override

src/main/java/com/jaamsim/EntityProviders/EntityProvListInput.java
@@ -27,6 +27,7 @@ import com.jaamsim.input.ArrayListInput;
 import com.jaamsim.input.Input;
 import com.jaamsim.input.InputErrorException;
 import com.jaamsim.input.KeywordIndex;
+import com.jaamsim.input.Parser;
 
 public class EntityProvListInput<T extends Entity> extends ArrayListInput<EntityListProvider<T>> {
 
@@ -37,6 +38,14 @@ public class EntityProvListInput<T extends Entity> extends ArrayListInput<Entity
 		super(key, cat, def);
 		entClass = aClass;
 		unique = true;
+	}
+
+	@Override
+	public String applyConditioning(String str) {
+		if (!str.contains("{")) {
+			return str;
+		}
+		return Parser.addSubstringQuotesIfNeeded(str);
 	}
 
 	@Override

src/main/java/com/jaamsim/StringProviders/StringProvListInput.java
@@ -27,6 +27,7 @@ import com.jaamsim.basicsim.JaamSimModel;
 import com.jaamsim.input.Input;
 import com.jaamsim.input.InputErrorException;
 import com.jaamsim.input.KeywordIndex;
+import com.jaamsim.input.Parser;
 import com.jaamsim.input.ArrayListInput;
 import com.jaamsim.units.DimensionlessUnit;
 
@@ -34,6 +35,11 @@ public class StringProvListInput extends ArrayListInput<StringProvider> {
 
 	public StringProvListInput(String key, String cat, ArrayList<StringProvider> def) {
 		super(key, cat, def);
+	}
+
+	@Override
+	public String applyConditioning(String str) {
+		return Parser.addSubstringQuotesIfNeeded(str);
 	}
 
 	@Override

commit 7306a4892e461118c18e625446954097d65b5504

JS: fix error in 'applyConditioning' for SampleListInput
Fixes an error introduced in 86cbaad19c3715d6e8b44ee3aaba1514f9ddc038
(JS: override 'applyConditioning' for SampleListInput, StringProvListInput,
and EntityProvListInput)

Prevent quotes from being added to an input of { 1 m }.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/Samples/SampleInput.java
@@ -27,7 +27,6 @@ import com.jaamsim.basicsim.JaamSimModel;
 import com.jaamsim.input.Input;
 import com.jaamsim.input.InputErrorException;
 import com.jaamsim.input.KeywordIndex;
-import com.jaamsim.input.Parser;
 import com.jaamsim.units.DimensionlessUnit;
 import com.jaamsim.units.Unit;
 import com.jaamsim.units.UserSpecifiedUnit;
@@ -89,14 +88,7 @@ public class SampleInput extends Input<SampleProvider> {
 
 	@Override
 	public String applyConditioning(String str) {
-
-		// No changes required if the input is a number and unit
-		ArrayList<String> tokens = new ArrayList<>();
-		Parser.tokenize(tokens, str, true);
-		if (tokens.size() == 2 && isDouble(tokens.get(0)))
-			return str;
-
-		return Parser.addQuotesIfNeeded(str);
+		return SampleProvider.addQuotesIfNeeded(str);
 	}
 
 	@Override

src/main/java/com/jaamsim/Samples/SampleListInput.java
@@ -112,7 +112,13 @@ public class SampleListInput extends ArrayListInput<SampleProvider> {
 		if (!str.contains("{")) {
 			return str;
 		}
-		return Parser.addSubstringQuotesIfNeeded(str);
+		String[] array = Parser.splitSubstrings(str);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = SampleProvider.addQuotesIfNeeded(array[i]);
+			sb.append("{").append(array[i]).append("}");
+		}
+		return sb.toString();
 	}
 
 	@Override

src/main/java/com/jaamsim/Samples/SampleProvider.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2013 Ausenco Engineering Canada Inc.
- * Copyright (C) 2022 JaamSim Software Inc.
+ * Copyright (C) 2022-2023 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,26 @@
  */
 package com.jaamsim.Samples;
 
+import java.util.ArrayList;
+
 import com.jaamsim.basicsim.Entity;
+import com.jaamsim.input.Input;
+import com.jaamsim.input.Parser;
 import com.jaamsim.units.Unit;
 
 public interface SampleProvider {
 	public Class<? extends Unit> getUnitType();
 	public double getNextSample(Entity thisEnt, double simTime);
 	public double getMeanValue(double simTime);
+
+	public static String addQuotesIfNeeded(String str) {
+
+		// No changes required if the input is a number and unit
+		ArrayList<String> tokens = new ArrayList<>();
+		Parser.tokenize(tokens, str, true);
+		if (tokens.size() == 2 && Input.isDouble(tokens.get(0)))
+			return str;
+
+		return Parser.addQuotesIfNeeded(str);
+	}
 }