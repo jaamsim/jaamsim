bf02b54914daf97612815c12f68520ea67f1bb4d
Harry King <d.harry.king@gmail.com>
2024-02-06 11:41:08 -0800

    JS: catch exceptions in 'inputCallback' for ExpressionThreshold
    Fixes a bug that can prevent a SubModel containing an ExpressionThreshold
    from being cloned successfully.
    
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Thresholds/ExpressionThreshold.java
+++ src/main/java/com/jaamsim/Thresholds/ExpressionThreshold.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2014 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2023 JaamSim Software Inc.
+ * Copyright (C) 2016-2024 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -170,7 +170,10 @@ public class ExpressionThreshold extends Threshold implements ObserverEntity {
 	static final InputCallback inputCallback = new InputCallback() {
 		@Override
 		public void callback(Entity ent, Input<?> inp) {
-			((ExpressionThreshold)ent).updateInputValue();
+			try {
+				((ExpressionThreshold)ent).updateInputValue();
+			}
+			catch (Exception e) {}
 		}
 	};
 
