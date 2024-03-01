jaamsim/jaamsim

commit 9e89fc6c1fc522327b0bea270a024f51ff5b120c

JS: Reset ObjectSelector horz scroll bar to 0 on window open
Signed-off-by: Eric Finlay <eric.finlay@ausenco.com>
Signed-off-by: Stephen Wong <stephen.wong@ausenco.com>

src/main/java/com/jaamsim/ui/ObjectSelector.java
@@ -148,6 +148,8 @@ public class ObjectSelector extends FrameBox {
 		if (myInstance == null)
 			myInstance = new ObjectSelector();
 
+		myInstance.treeView.getHorizontalScrollBar().getModel().setValue(0);
+
 		return myInstance;
 	}
 

commit b1e3db5fb2c833e2e631c2932713d628d877f9d9

JS: remove the horizontal scroll bar reset in getInstance for ObjectSelector
Fixes a bug introduced way back in 9e89fc6c1fc522327b0bea270a024f51ff5b120c
(JS: Reset ObjectSelector horz scroll bar to 0 on window open)
that caused the horizontal scroll bar to jitter while a model is running.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/ObjectSelector.java
@@ -128,6 +128,15 @@ public class ObjectSelector extends FrameBox {
 	}
 
 	@Override
+	public void setVisible(boolean bool) {
+		if (bool && !isVisible()) {
+			myInstance.treeView.getHorizontalScrollBar().getModel().setValue(0);
+			myInstance.treeView.getVerticalScrollBar().getModel().setValue(0);
+		}
+		super.setVisible(bool);
+	}
+
+	@Override
 	public void setEntity(Entity ent) {
 
 		if (ent == currentEntity)
@@ -201,9 +210,6 @@ public class ObjectSelector extends FrameBox {
 	public static synchronized ObjectSelector getInstance() {
 		if (myInstance == null)
 			myInstance = new ObjectSelector();
-
-		myInstance.treeView.getHorizontalScrollBar().getModel().setValue(0);
-
 		return myInstance;
 	}
 