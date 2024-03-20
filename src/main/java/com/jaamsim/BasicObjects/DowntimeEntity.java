8a836e2e3aeaa19ab671a07b84321d56457e8d65
Harry King <d.harry.king@gmail.com>
2024-03-19 21:24:26 -0700

    JS: add trace statements to DowntimeEntity and EntitySystem
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/BasicObjects/DowntimeEntity.java
+++ src/main/java/com/jaamsim/BasicObjects/DowntimeEntity.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2014 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2023 JaamSim Software Inc.
+ * Copyright (C) 2016-2024 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -433,6 +433,7 @@ public class DowntimeEntity extends StateEntity implements StateEntityListener {
 	}
 
 	private void setDown(boolean b) {
+		if (isTraceFlag()) this.trace(1, "setDown(%s)", b);
 		down = b;
 		if (down)
 			setPresentState(STATE_DOWNTIME);
