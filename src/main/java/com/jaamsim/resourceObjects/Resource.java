6b93a82ee213c0394dcb703de7477f4f444d7d8b
Harry King <d.harry.king@gmail.com>
2024-08-16 19:28:57 -0700

    JS: apply 'setOutput' to inputs of type SampleInput for the Resource library
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/resourceObjects/Resource.java
+++ src/main/java/com/jaamsim/resourceObjects/Resource.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2013 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2022 JaamSim Software Inc.
+ * Copyright (C) 2016-2024 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,6 +54,7 @@ public class Resource extends AbstractResourceProvider {
 		capacity.setUnitType(DimensionlessUnit.class);
 		capacity.setIntegerValue(true);
 		capacity.setValidRange(0, Double.POSITIVE_INFINITY);
+		capacity.setOutput(true);
 		this.addInput(capacity);
 	}
 
