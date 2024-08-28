jaamsim/jaamsim

commit 9900e1e6887724621d61d27a001789846fa86830

JS: add method 'setInitialValue' to EntityNameInput
Greatly reduces the overhead required to set the initial name for an
entity. Restores the entity generation rate for models to the original
level.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/basicsim/Entity.java
@@ -197,6 +197,13 @@ public class Entity {
 	};
 
 	public void setNameInput(String localName) throws InputErrorException {
+		if (nameInput.isDef()) {
+			nameInput.setInitialValue(localName);
+			entityName = localName;
+			if (isRegistered())
+				getJaamSimModel().addNamedEntity(this);
+			return;
+		}
 		InputAgent.applyArgs(this, nameInput.getKeyword(), localName);
 	}
 

src/main/java/com/jaamsim/input/EntityNameInput.java
@@ -66,4 +66,10 @@ public class EntityNameInput extends Input<String> {
 		return false;
 	}
 
+	public void setInitialValue(String name) {
+		value = name;
+		valueTokens = new String[]{name};
+		isDef = false;
+	}
+
 }

commit dd34c7e10937c352433bdd8070f5ae75aabafe5d

JS: ensure the Object Selector is updated in 'setNameInput' for Entity
Fixes a bug introduced in 9900e1e6887724621d61d27a001789846fa86830
(JS: add method 'setInitialValue' to EntityNameInput)

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/basicsim/Entity.java
@@ -198,9 +198,7 @@ public class Entity {
 	public void setNameInput(String localName) throws InputErrorException {
 		if (nameInput.isDef()) {
 			nameInput.setInitialValue(localName);
-			entityName = localName;
-			if (isRegistered())
-				getJaamSimModel().addNamedEntity(this);
+			setLocalName(localName);
 			nameInput.setLocked(isGenerated());
 			return;
 		}