jaamsim/jaamsim

commit 883a9117aa53c77c9a6c03a2b4c9436df9f5ec4e

JS: ignore updateUI until the initial configuration file has been loaded
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -4702,7 +4702,7 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 		@Override
 		public void run() {
 			JaamSimModel sim = getJaamSimModel();
-			if (sim == null)
+			if (sim == null || !sim.isConfigured())
 				return;
 			EventManager evt = sim.getEventManager();
 			double callBackTime = evt.ticksToSeconds(frame.simTicks);

commit 52699c1a220ba8d2b4e09ff9f1abf8d2d3d7fd45

JS: reset the 'Show Labels' button when a new model is opened
Fixes a bug introduced in 883a9117aa53c77c9a6c03a2b4c9436df9f5ec4e
(JS: ignore updateUI until the initial configuration file has been loaded)
It is not clear how the labels were generated correctly prior to this
change.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -3455,9 +3455,39 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 		EditBox.getInstance().setEntity(null);
 	}
 
+	/**
+	 * Resets the toggle buttons to their off positions.
+	 * Some of these buttons do not require resetting, but are done to be consistent with the ones
+	 * that do need to be reset.
+	 */
 	private void clearButtons() {
+
+		// Presentation mode and lock windows
+		presentMode.setSelected(false);
+		lockWindows.setSelected(false);
+
+		// Show axes and grid
+		xyzAxis.setSelected(false);
+		grid.setSelected(false);
+
+		// Show labels and sub-models
+		showLabels.setSelected(false);
+		showSubModels.setSelected(false);
+
+		// Snap-to-grid
+		snapToGrid.setSelected(false);
+
+		// References and links
+		// (doClick is required to perform the action for each button)
+		if (showReferences.isSelected())
+			showReferences.doClick();
+		if (showLinks.isSelected())
+			showLinks.doClick();
 		if (createLinks.isSelected())
 			createLinks.doClick();
+
+		// Reverse
+		// (doClick is required to perform the action for each button)
 		if (reverseButton.isSelected())
 			reverseButton.doClick();
 	}