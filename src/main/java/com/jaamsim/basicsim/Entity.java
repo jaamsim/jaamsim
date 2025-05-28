jaamsim/jaamsim

commit 43d0f632cd45e547c51717108406df5f5b333a80

JS: improve 'getValueTokens(Input, newParent)' to allow it to handle more situations
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/basicsim/Entity.java
@@ -606,17 +606,28 @@ public class Entity {
 			return prototype.getValueTokens(in.getProtoInput(), newParent);
 
 		ArrayList<String> ret = in.getValueTokens();
-		if (ret.isEmpty() || parent == null || parent == newParent)
+		if (ret.isEmpty() || parent == null || newParent == null || parent == newParent)
 			return ret;
+
+		// Find the first parent that has a different name
+		Entity oldP = parent;
+		Entity newP = newParent;
+		while (oldP.getLocalName().equals(newP.getLocalName())) {
+			oldP = oldP.getParent();
+			newP = newP.getParent();
+			if (oldP == null || newP == null) {
+				return ret;
+			}
+		}
 
 		// Replace any explicit references to the parent entity with the specified new parent
 		String oldName = parent.getName();
 		String oldName1 = "[" + oldName + "]";
-		String oldName2 = oldName + ".";
+		String oldName2 = oldP.getLocalName() + ".";
 
 		String newName = newParent.getName();
 		String newName1 = "[" + newName + "]";
-		String newName2 = newName + ".";
+		String newName2 = newP.getLocalName() + ".";
 
 		for (int i = 0; i < ret.size(); i++) {
 			String str = ret.get(i);

commit 96c204e70d573461d242b9627731e171d5c2f107

JS: fix bug in 'getValueTokens(Input, newParent)' for Entity
Fixes a bug introduced by 43d0f632cd45e547c51717108406df5f5b333a80
(JS: improve 'getValueTokens(Input, newParent)' to allow it to handle more
situations)
that caused the Region input to inherit the wrong value for the components
of a nested SubModel.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/basicsim/Entity.java
@@ -634,22 +634,20 @@ public class Entity {
 		// Find the first parent that has a different name
 		Entity oldP = parent;
 		Entity newP = newParent;
-		while (oldP.getLocalName().equals(newP.getLocalName())) {
-			oldP = oldP.getParent();
-			newP = newP.getParent();
-			if (oldP == null || newP == null) {
-				return ret;
-			}
+		while (oldP.getLocalName().equals(newP.getLocalName())
+				&& oldP.parent != null && newP.parent != null) {
+			oldP = oldP.parent;
+			newP = newP.parent;
 		}
 
 		// Replace any explicit references to the parent entity with the specified new parent
 		String oldName = parent.getName();
 		String oldName1 = "[" + oldName + "]";
-		String oldName2 = oldP.getLocalName() + ".";
+		String oldName2 = oldP.getName() + ".";
 
 		String newName = newParent.getName();
 		String newName1 = "[" + newName + "]";
-		String newName2 = newP.getLocalName() + ".";
+		String newName2 = newP.getName() + ".";
 
 		for (int i = 0; i < ret.size(); i++) {
 			String str = ret.get(i);