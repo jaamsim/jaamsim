jaamsim/jaamsim

commit 7b6b36efdfbf49c8d30b4efeb021d220739fcb50

JS: use default alignment for an EntityConveyor that is non-accumulating
Prevents unexpected change to the display of entities. For example, when
an EntityConveyor is vertical in the xy-plane, entities were positioned
on their left-hand edge by the old code.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/ProcessFlow/EntityConveyor.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2013 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2024 JaamSim Software Inc.
+ * Copyright (C) 2016-2025 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -518,9 +518,14 @@ public class EntityConveyor extends LinkedService implements LineEntity {
 
 			convPos = Math.max(convPos, 0.0d);
 			Vec3d localPos = PolylineInfo.getPositionOnPolyline(getCurvePoints(), convPos);
-			Vec3d alignment = entry.entity.getAlignment();
-			alignment.x = -0.5d;
-			entry.entity.setGlobalPositionForAlignment(getGlobalPosition(localPos), alignment);
+			if (isAccumulating()) {
+				Vec3d alignment = entry.entity.getAlignment();
+				alignment.x = -0.5d;
+				entry.entity.setGlobalPositionForAlignment(getGlobalPosition(localPos), alignment);
+			}
+			else {
+				entry.entity.setGlobalPosition(localPos);
+			}
 
 			if (isRotateEntities(simTime)) {
 				Vec3d orient = PolylineInfo.getOrientationOnPolyline(getCurvePoints(), convPos);

commit 7058477f601644fa94719d7a8d96e7397b0e19a2

JS: ensure entities remain aligned to conveyor when relative positions are used
Fixes: 7b6b36efdfbf49c8d30b4efeb021d220739fcb50

Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

src/main/java/com/jaamsim/ProcessFlow/EntityConveyor.java
@@ -524,7 +524,7 @@ public class EntityConveyor extends LinkedService implements LineEntity {
 				entry.entity.setGlobalPositionForAlignment(getGlobalPosition(localPos), alignment);
 			}
 			else {
-				entry.entity.setGlobalPosition(localPos);
+				entry.entity.setGlobalPosition(getGlobalPosition(localPos));
 			}
 
 			if (isRotateEntities(simTime)) {