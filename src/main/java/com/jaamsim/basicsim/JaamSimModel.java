jaamsim/jaamsim

commit 28bf71cbdc7be119d4f8ca1e4c04607188e2652d

JS: provide a JaamSimModel constructor that copies another JaamSimModel
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/basicsim/JaamSimModel.java
@@ -132,6 +132,22 @@ public class JaamSimModel {
 		scenarioIndexList.add(1);
 	}
 
+	public JaamSimModel(JaamSimModel sm) {
+		this(sm.name);
+		autoLoad();
+		simulation = getSimulation();
+		try {
+			configure(sm.configFile);
+		}
+		catch (Exception e) {
+			LogBox.logException(e);
+		}
+	}
+
+	public void setName(String str) {
+		name = str;
+	}
+
 	public final void setTimeListener(EventTimeListener l) {
 		eventManager.setTimeListener(l);
 	}

commit 94d5925de7a26e3b61d9f033f28c193b542ab885

JS: remove unneeded setting of simulation in JaamSimModel
Setting a variable to itself, not needed.
Fixes: 28bf71cbdc7be119d4f8ca1e4c04607188e2652d

Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

src/main/java/com/jaamsim/basicsim/JaamSimModel.java
@@ -127,7 +127,6 @@ public class JaamSimModel implements EventTimeListener {
 		this(sm.name);
 		//System.out.format("%nJaamSimModel constructor%n");
 		autoLoad();
-		simulation = getSimulation();
 		setRecordEdits(true);
 
 		configFile = sm.configFile;