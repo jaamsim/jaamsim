1ee6b011638107f85c9b850e845aaf5c5544bffb
Harry King <d.harry.king@gmail.com>
2025-10-31 11:17:08 -0700

    JS: add output 'EntityDependentList' to DisplayEntity
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/Graphics/DisplayEntity.java
+++ src/main/java/com/jaamsim/Graphics/DisplayEntity.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2002-2011 Ausenco Engineering Canada Inc.
- * Copyright (C) 2017-2024 JaamSim Software Inc.
+ * Copyright (C) 2017-2025 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1628,6 +1628,21 @@ public class DisplayEntity extends Entity {
 					|| ent instanceof OverlayEntity || ent instanceof Region)
 				continue;
 			ret.add((DisplayEntity) ent);
+		}
+		return ret;
+	}
+
+	@Output(name = "EntityDependentList",
+	 description = "The entities whose inputs include one or more references to this entity.",
+	    sequence = 12)
+	public ArrayList<DisplayEntity> getEntityDependentList(double simTime) {
+		ArrayList<DisplayEntity> ret = new ArrayList<>();
+		for (DisplayEntity ent : getJaamSimModel().getClonesOfIterator(DisplayEntity.class)) {
+			if (ent == this)
+				continue;
+			if (ent.getEntityReferences().contains(this)) {
+				ret.add(ent);
+			}
 		}
 		return ret;
 	}
