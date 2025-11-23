jaamsim/jaamsim

commit 213cb49e131e5af996e4246349c47ba6b1a60de0

JS: pull the isGood tests into getActiveView
Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -522,12 +522,10 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 	}
 
 	public void showWindows() {
-		JaamSimModel sim = getJaamSimModel();
-		if (!RenderManager.isGood() || sim == null || sim.getSimulation() == null)
-			return;
-
 		// Identity the view window that is active
-		View activeView = RenderManager.inst().getActiveView();
+		View activeView = RenderManager.getActiveView();
+		if (activeView == null)
+			return;
 
 		// Re-open the view windows
 		for (int i = 0; i < views.size(); i++) {
@@ -541,7 +539,7 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 			RenderManager.inst().createWindow(activeView);
 
 		// Re-open the tools
-		showActiveTools(sim.getSimulation());
+		showActiveTools(getJaamSimModel().getSimulation());
 		updateUI();
 	}
 
@@ -1662,12 +1660,9 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 			@Override
 			public void actionPerformed( ActionEvent event ) {
 				boolean bLock2D = lockViewXYPlane.isSelected();
-
-				if (RenderManager.isGood()) {
-					View currentView = RenderManager.inst().getActiveView();
-					if (currentView != null) {
-						currentView.setLock2D(bLock2D);
-					}
+				View currentView = RenderManager.getActiveView();
+				if (currentView != null) {
+					currentView.setLock2D(bLock2D);
 				}
 				controlStartResume.requestFocusInWindow();
 			}
@@ -3692,9 +3687,7 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 	}
 
 	private void update2dButton() {
-		if (!RenderManager.isGood())
-			return;
-		View view = RenderManager.inst().getActiveView();
+		View view = RenderManager.getActiveView();
 		if (view == null)
 			return;
 		lockViewXYPlane.setEnabled(view.isMovable());
@@ -4291,9 +4284,7 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 		IntegerVector ret = new IntegerVector(v.getWindowPos());
 
 		// Presentation mode
-		View activeView = null;
-		if (RenderManager.isGood())
-			activeView = RenderManager.inst().getActiveView();
+		View activeView = RenderManager.getActiveView();
 		if (presentMode.isSelected() && v == activeView) {
 			ret.set(0, COL1_START);
 			ret.set(1, TOP_START);
@@ -4310,9 +4301,7 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 		IntegerVector ret = new IntegerVector(v.getWindowSize());
 
 		// Presentation mode
-		View activeView = null;
-		if (RenderManager.isGood())
-			activeView = RenderManager.inst().getActiveView();
+		View activeView = RenderManager.getActiveView();
 		if (presentMode.isSelected() && v == activeView) {
 			ret.set(0, VIEW_WIDTH + COL1_WIDTH);
 			ret.set(1, VIEW_HEIGHT + LOWER_HEIGHT);

src/main/java/com/jaamsim/controllers/RenderManager.java
@@ -1827,7 +1827,7 @@ public class RenderManager implements DragSourceListener {
 
 	private Region getRegion(int windowID, int x, int y) {
 		Ray currentRay = getRayForMouse(windowID, x, y);
-		int viewID = getActiveView().getID();
+		int viewID = _getActiveView().getID();
 		List<PickData> picks = pickForRay(currentRay, viewID, false);
 		Collections.sort(picks, new SelectionSorter());
 		for (PickData pd : picks) {
@@ -2015,7 +2015,14 @@ public class RenderManager implements DragSourceListener {
 		control.setRotationAngles(0.0000001, 0.0);
 	}
 
-	public View getActiveView() {
+	public static View getActiveView() {
+		if (!isGood())
+			return null;
+
+		return inst()._getActiveView();
+	}
+
+	private View _getActiveView() {
 		return windowToViewMap.get(activeWindowID);
 	}
 

src/main/java/com/jaamsim/ui/ContextMenu.java
@@ -35,11 +35,11 @@ import com.jaamsim.Commands.KeywordCommand;
 import com.jaamsim.Graphics.DisplayEntity;
 import com.jaamsim.Graphics.EditableText;
 import com.jaamsim.Graphics.EntityLabel;
-import com.jaamsim.SubModels.SubModel;
 import com.jaamsim.Graphics.OverlayEntity;
 import com.jaamsim.Graphics.PolylineInfo;
 import com.jaamsim.Graphics.View;
 import com.jaamsim.SubModels.CompoundEntity;
+import com.jaamsim.SubModels.SubModel;
 import com.jaamsim.basicsim.Entity;
 import com.jaamsim.basicsim.JaamSimModel;
 import com.jaamsim.basicsim.Log;
@@ -361,11 +361,11 @@ public class ContextMenu {
 
 		// 5) Centre in View
 		JMenuItem centerInViewMenuItem = new JMenuItem( "Center in View" );
-		final View v = RenderManager.inst().getActiveView();
 		centerInViewMenuItem.addActionListener( new ActionListener() {
 
 			@Override
 			public void actionPerformed( ActionEvent event ) {
+				View v = RenderManager.getActiveView();
 				// Move the camera position so that the entity is in the centre of the screen
 				Vec3d viewPos = new Vec3d(v.getViewPosition());
 				Vec3d viewCenter = new Vec3d(v.getEffViewCenter());
@@ -380,7 +380,7 @@ public class ContextMenu {
 				InputAgent.storeAndExecute(new KeywordCommand(v, posKw, ctrKw));
 			}
 		} );
-		if (v == null) {
+		if (RenderManager.getActiveView() == null) {
 			centerInViewMenuItem.setEnabled(false);
 		}
 		menu.add( centerInViewMenuItem );

commit 42d221a24eb0954086a9acf2e70de86f5531a7e7

JS: fix window un-minimize path
Fixes: 213cb49e131e5af996e4246349c47ba6b1a60de0

Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -503,22 +503,21 @@ public class GUIFrame extends OSFixJFrame implements EventTimeListener, GUIListe
 	}
 
 	public void showWindows() {
-		// Identity the view window that is active
-		View activeView = RenderManager.getActiveView();
-		if (activeView == null)
-			return;
+		if (RenderManager.isGood()) {
+			// Identity the view window that is active
+			View activeView = RenderManager.getActiveView();
 
-		// Re-open the view windows
-		for (int i = 0; i < views.size(); i++) {
-			View v = views.get(i);
-			if (v != null && v.showWindow() && v != activeView)
-				RenderManager.inst().createWindow(v);
+			// Re-open the view windows
+			for (int i = 0; i < views.size(); i++) {
+				View v = views.get(i);
+				if (v != null && v.showWindow() && v != activeView)
+					RenderManager.inst().createWindow(v);
+			}
+
+			// Re-open the active view window last
+			if (activeView != null)
+				RenderManager.inst().createWindow(activeView);
 		}
-
-		// Re-open the active view window last
-		if (activeView != null)
-			RenderManager.inst().createWindow(activeView);
-
 		// Re-open the tools
 		showActiveTools(getJaamSimModel().getSimulation());
 		updateUI();