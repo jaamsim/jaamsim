88d4d79a71f85d8c27b068e566e6ae1d8f00487f
Harry King <d.harry.king@gmail.com>
2026-01-02 21:26:47 -0800

    JS: delete clones in 'kill' instead of 'deleteEntity' for GUIFrame
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/ui/GUIFrame.java
+++ src/main/java/com/jaamsim/ui/GUIFrame.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2002-2011 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2025 JaamSim Software Inc.
+ * Copyright (C) 2016-2026 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -3440,14 +3440,6 @@ public class GUIFrame extends OSFixJFrame implements GUIListener {
 
 		if (ent instanceof DisplayEntity && !((DisplayEntity) ent).isMovable())
 			throw new ErrorException("Cannot delete an entity that is not movable.");
-
-		// Delete any clones
-		for (Entity clone : ent.getAllClones()) {
-			if (clone.isGenerated() || clone instanceof EntityLabel)
-				clone.kill();
-			else
-				deleteEntity(clone);
-		}
 
 		// Region
 		if (ent instanceof Region) {
