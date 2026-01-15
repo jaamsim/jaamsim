1c60518c35bf2e533993915edd16e5e2aac50ee7
Harry King <d.harry.king@gmail.com>
2026-01-14 22:20:58 -0800

    JS: allow EntityListInput to be used as an output
    Signed-off-by: Harry King <d.harry.king@gmail.com>

--- src/main/java/com/jaamsim/input/EntityListInput.java
+++ src/main/java/com/jaamsim/input/EntityListInput.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2010-2011 Ausenco Engineering Canada Inc.
- * Copyright (C) 2018-2023 JaamSim Software Inc.
+ * Copyright (C) 2018-2026 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -268,6 +268,11 @@ public class EntityListInput<T extends Entity> extends ArrayListInput<T> {
 				continue;
 			list.add(ent);
 		}
+	}
+
+	@Override
+	public Class<?> getReturnType() {
+		return ArrayList.class;
 	}
 
 }
