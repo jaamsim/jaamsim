jaamsim/jaamsim

commit 7c162fea2628a2c140155183d59b2a544f6bd7ae

JS: evaluate the input expression when an attribute is initialized
Allows the expression used to set the initial value for an attribute to
reference any other attribute or custom output regardless of the order
in which they were defined.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/AttributeHandle.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2014 Ausenco Engineering Canada Inc.
- * Copyright (C) 2021-2024 JaamSim Software Inc.
+ * Copyright (C) 2021-2025 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,18 +18,20 @@
 package com.jaamsim.input;
 
 import com.jaamsim.basicsim.Entity;
+import com.jaamsim.basicsim.ErrorException;
+import com.jaamsim.input.ExpParser.Expression;
 import com.jaamsim.units.Unit;
 
 public class AttributeHandle extends ValueHandle {
 	private final String attributeName;
-	private final ExpResult initialValue;
+	private final Expression expression;
 	private ExpResult value;
-	private final Class<? extends Unit> unitType;
+	private Class<? extends Unit> unitType;
 
-	public AttributeHandle(Entity e, String name, ExpResult initVal, ExpResult val, Class<? extends Unit> ut) {
+	public AttributeHandle(Entity e, String name, Expression exp, ExpResult val, Class<? extends Unit> ut) {
 		super(e);
 		attributeName = name;
-		initialValue = initVal;
+		expression = exp;
 		value = val;
 		unitType = ut;
 	}
@@ -39,24 +41,31 @@ public class AttributeHandle extends ValueHandle {
 		return unitType;
 	}
 
-	public ExpResult getInitialValue() {
-		return initialValue.getCopy();
+	public Expression getExpression() {
+		return expression;
 	}
 
 	public void setValue(ExpResult val) {
 		value = val;
+		unitType = val.unitType;
 	}
 
 	@Override
 	public <T> T getValue(double simTime, Class<T> klass) {
-		return getValue(klass);
+		if (value == null) {
+			try {
+				ExpResult res = ExpEvaluator.evaluateExpression(expression, ent, simTime);
+				return res.getValue(klass);
+			}
+			catch (ExpError e) {
+				throw new ErrorException(ent, e);
+			}
+		}
+		return value.getValue(klass);
 	}
 
 	public <T> T getValue(Class<T> klass) {
-		if (value == null) {
-			return null;
-		}
-		return value.getValue(klass);
+		return getValue(0.0d, klass);
 	}
 
 	public ExpResult copyValue() {

src/main/java/com/jaamsim/basicsim/Entity.java
@@ -1,7 +1,7 @@
 /*
  * JaamSim Discrete Event Simulation
  * Copyright (C) 2002-2011 Ausenco Engineering Canada Inc.
- * Copyright (C) 2016-2024 JaamSim Software Inc.
+ * Copyright (C) 2016-2025 JaamSim Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ import com.jaamsim.input.AttributeHandle;
 import com.jaamsim.input.BooleanInput;
 import com.jaamsim.input.EntityNameInput;
 import com.jaamsim.input.ExpError;
+import com.jaamsim.input.ExpEvaluator;
 import com.jaamsim.input.ExpResType;
 import com.jaamsim.input.ExpResult;
 import com.jaamsim.input.ExpValResult;
@@ -157,7 +158,7 @@ public class Entity {
 		this.addInput(active);
 
 		attributeDefinitionList = new AttributeDefinitionListInput("AttributeDefinitionList",
-				OPTIONS, new ArrayList<AttributeHandle>());
+				OPTIONS, new ArrayList<NamedExpression>());
 		attributeDefinitionList.setCallback(userOutputCallback);
 		attributeDefinitionList.setHidden(false);
 		this.addInput(attributeDefinitionList);
@@ -304,7 +305,13 @@ public class Entity {
 			if (!(vh instanceof AttributeHandle))
 				continue;
 			AttributeHandle h = (AttributeHandle) vh;
-			h.setValue(h.getInitialValue());
+			try {
+				ExpResult res = ExpEvaluator.evaluateExpression(h.getExpression(), this, 0.0d);
+				h.setValue(res);
+			}
+			catch (ExpError e) {
+				throw new ErrorException(this, attributeDefinitionList.getKeyword(), e);
+			}
 		}
 
 		// Clear the clone pool
@@ -927,9 +934,9 @@ public class Entity {
 
 	public void updateUserOutputMap() {
 		clearUserOutputs();
-		for (AttributeHandle h : attributeDefinitionList.getValue()) {
-			AttributeHandle ah = new AttributeHandle(this, h.getName(), h.getInitialValue(), h.copyValue(), h.getUnitType());
-			addUserOutputHandle(h.getName(), ah);
+		for (NamedExpression ne : attributeDefinitionList.getValue()) {
+			AttributeHandle ah = new AttributeHandle(this, ne.getName(), ne.getExpression(), null, ne.getUnitType());
+			addUserOutputHandle(ne.getName(), ah);
 		}
 		for (NamedExpression ne : namedExpressionInput.getValue()) {
 			ExpressionHandle eh = new ExpressionHandle(this, ne.getExpression(), ne.getName(), ne.getUnitType());

src/main/java/com/jaamsim/input/AttributeDefinitionListInput.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 import com.jaamsim.basicsim.Entity;
+import com.jaamsim.basicsim.ErrorException;
 import com.jaamsim.input.ExpEvaluator.EntityParseContext;
 import com.jaamsim.units.DimensionlessUnit;
 import com.jaamsim.units.Unit;
@@ -31,11 +32,9 @@ import com.jaamsim.units.Unit;
  * Entity AttributeDefinitionList { { AttibuteName1 Value1 } { AttibuteName2 Value2 } ... }
  * @author Harry King
  */
-public class AttributeDefinitionListInput extends ArrayListInput<AttributeHandle> {
+public class AttributeDefinitionListInput extends ArrayListInput<NamedExpression> {
 
-	private ArrayList<ExpEvaluator.EntityParseContext> parseContextList;
-
-	public AttributeDefinitionListInput(String key, String cat, ArrayList<AttributeHandle> def) {
+	public AttributeDefinitionListInput(String key, String cat, ArrayList<NamedExpression> def) {
 		super(key, cat, def);
 	}
 
@@ -49,8 +48,7 @@ public class AttributeDefinitionListInput extends ArrayListInput<AttributeHandle
 
 		// Divide up the inputs by the inner braces
 		ArrayList<KeywordIndex> subArgs = kw.getSubArgs();
-		ArrayList<AttributeHandle> temp = new ArrayList<>(subArgs.size());
-		ArrayList<ExpEvaluator.EntityParseContext> pcList = new ArrayList<>(subArgs.size());
+		ArrayList<NamedExpression> temp = new ArrayList<>(subArgs.size());
 
 		// Ensure that no attribute names are repeated
 		HashSet<String> nameSet = new HashSet<>();
@@ -75,22 +73,16 @@ public class AttributeDefinitionListInput extends ArrayListInput<AttributeHandle
 					throw new InputErrorException("Attribute name is the same as existing output name: %s", name);
 				}
 
-				ExpResult expVal;
 				Class<? extends Unit> unitType = DimensionlessUnit.class;
 
 				// Parse the expression
 				String expString = subArg.getArg(1);
 				EntityParseContext pc = ExpEvaluator.getParseContext(thisEnt, expString);
 				ExpParser.Expression exp = ExpParser.parseExpression(pc, expString);
-				expVal = ExpEvaluator.evaluateExpression(exp, thisEnt, 0);
-				if (expVal.type == ExpResType.NUMBER) {
-					unitType = expVal.unitType;
-				}
 
 				// Save the data for this attribute
-				AttributeHandle h = new AttributeHandle(thisEnt, name, expVal, expVal, unitType);
-				temp.add(h);
-				pcList.add(pc);
+				NamedExpression ne = new NamedExpression(name, pc, exp, unitType);
+				temp.add(ne);
 
 			} catch (ExpError e) {
 				throw new InputErrorException(e);
@@ -100,7 +92,6 @@ public class AttributeDefinitionListInput extends ArrayListInput<AttributeHandle
 		}
 
 		// Save the data for each attribute
-		parseContextList = pcList;
 		value = temp;
 	}
 
@@ -119,10 +110,10 @@ public class AttributeDefinitionListInput extends ArrayListInput<AttributeHandle
 		if (value == null || isDef)
 			return;
 		for (int i = 0; i < value.size(); i++) {
-			AttributeHandle h = value.get(i);
+			NamedExpression ne = value.get(i);
 			toks.add("{");
-			toks.add(h.getName());
-			toks.add(parseContextList.get(i).getUpdatedSource());
+			toks.add(ne.getName());
+			toks.add(ne.getParseContext().getUpdatedSource());
 			toks.add("}");
 		}
 	}
@@ -133,22 +124,44 @@ public class AttributeDefinitionListInput extends ArrayListInput<AttributeHandle
 	}
 
 	@Override
+	public String getStubDefinition() {
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		for (NamedExpression ne : value) {
+			if (first) {
+				first = false;
+			}
+			else {
+				sb.append(Input.BRACE_SEPARATOR);
+			}
+			sb.append(ne.getStubDefinition());
+		}
+		return sb.toString();
+	}
+
+	@Override
 	public String getPresentValueString(Entity thisEnt, double simTime) {
 		if (value == null || isDef)
 			return "";
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < value.size(); i++) {
-			AttributeHandle h = value.get(i);
+			NamedExpression ne = value.get(i);
 			if (i > 0)
 				sb.append(BRACE_SEPARATOR);
 
 			// Opening brace and attribute name
 			sb.append("{").append(BRACE_SEPARATOR);
-			sb.append(h.getName()).append(SEPARATOR);
+			sb.append(ne.getName()).append(SEPARATOR);
 
 			// Present value
-			sb.append(h.getInitialValue());
+			try {
+				ExpResult res = ExpEvaluator.evaluateExpression(ne.getExpression(), thisEnt, simTime);
+				sb.append(res.toString());
+			}
+			catch (ExpError e) {
+				throw new ErrorException(thisEnt, getKeyword(), i + 1, e);
+			}
 
 			// Closing brace
 			sb.append(BRACE_SEPARATOR).append("}");

commit 90c6ee1d6e53535736ed661488168b13df9f8822

JS: return the correct unit type for an attribute prior to startup
Fixes a bug introduced in 7c162fea2628a2c140155183d59b2a544f6bd7ae
(JS: evaluate the input expression when an attribute is initialized)

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/AttributeHandle.java
@@ -38,6 +38,15 @@ public class AttributeHandle extends ValueHandle {
 
 	@Override
 	public Class<? extends Unit> getUnitType() {
+		if (value == null) {
+			try {
+				ExpResult res = ExpEvaluator.evaluateExpression(expression, ent, 0.0d);
+				return res.unitType;
+			}
+			catch (ExpError e) {
+				throw new ErrorException(ent, e);
+			}
+		}
 		return unitType;
 	}
 