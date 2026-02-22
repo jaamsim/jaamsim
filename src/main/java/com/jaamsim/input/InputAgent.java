jaamsim/jaamsim

commit e63e8276b85c2298c12739645ec26114c0db8af5

JS: avoid saving an EntityLabel clone that will be created automatically
Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/InputAgent.java
@@ -960,6 +960,9 @@ public class InputAgent {
 			if (ent instanceof EntityLabel && !((EntityLabel) ent).getShowInput()
 					&& ((EntityLabel) ent).isDefault())
 				continue;
+			if (ent.getParent() != simModel.getSimulation() && ent.getPrototype() != null
+					&& ent.getParent().getPrototype() == ent.getPrototype().getParent())
+				continue;
 			newEntities.add(ent);
 		}
 		Collections.sort(newEntities, uiEntitySortOrder);

commit aa3060253dca62027d93d66c8674cea934e9f9de

JS: fix bug in configuration file saving for 'Nested SubModel Example'
Fixes a bug introduced by e63e8276b85c2298c12739645ec26114c0db8af5
(JS: avoid saving an EntityLabel clone that will be created automatically)
The bug prevented the two nested 'ServerPlusQueue' SubModels from being
defined in the saved configuration file.

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/InputAgent.java
@@ -947,12 +947,14 @@ public class InputAgent {
 				continue;
 			if (ent instanceof ObjectType || ent.getObjectType() == null)
 				continue;
-			if (ent instanceof EntityLabel && !((EntityLabel) ent).getShowInput()
-					&& ((EntityLabel) ent).isDefault())
-				continue;
-			if (ent.getParent() != null && ent.getPrototype() != null
-					&& ent.getParent().getPrototype() == ent.getPrototype().getParent())
-				continue;
+			if (ent instanceof EntityLabel) {
+				EntityLabel label = (EntityLabel) ent;
+				if (!label.getShowInput() && label.isDefault())
+					continue;
+				if (label.getParent() != null && label.getPrototype() != null
+					&& label.getParent().getPrototype() == label.getPrototype().getParent())
+					continue;
+			}
 			newEntities.add(ent);
 		}
 		Collections.sort(newEntities, uiEntitySortOrder);