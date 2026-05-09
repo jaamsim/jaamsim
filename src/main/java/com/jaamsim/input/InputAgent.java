jaamsim/jaamsim

commit 8a61c3e1b1cf90e5dc8424ed96628d8ce0b495df

JS: inline formatDoubleInput into the single callsite
Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

src/main/java/com/jaamsim/input/InputAgent.java
@@ -523,7 +523,11 @@ public class InputAgent {
 	}
 
 	public static void applyValue(Entity ent, String keyword, double val, String unit){
-		KeywordIndex kw = formatDoubleInput(keyword, val, unit);
+		ArrayList<String> tokens = new ArrayList<>(2);
+		tokens.add(String.format((Locale)null, "%s", val));
+		if (unit != null && !unit.isEmpty())
+			tokens.add(unit);
+		KeywordIndex kw = new KeywordIndex(keyword, tokens, null);
 		InputAgent.apply(ent, kw);
 	}
 
@@ -1835,14 +1839,6 @@ public class InputAgent {
 
 		// Set the model state to unedited
 		simModel.setSessionEdited(false);
-	}
-
-	public static KeywordIndex formatDoubleInput(String keyword, double val, String unit) {
-		ArrayList<String> tokens = new ArrayList<>(2);
-		tokens.add(String.format((Locale)null, "%s", val));
-		if (unit != null && !unit.isEmpty())
-			tokens.add(unit);
-		return new KeywordIndex(keyword, tokens, null);
 	}
 
 	public static KeywordIndex formatInput(String keyword, String str) {

commit 130435fb11e8f9a6f7ffc5e30777193a90d4a898

JS: add method 'formatValue' to KeywordIndex
Fixes a bug in JaamSim Pro caused by 8a61c3e1b1cf90e5dc8424ed96628d8ce0b495df
(JS: inline formatDoubleInput into the single callsite)

Signed-off-by: Harry King <d.harry.king@gmail.com>

src/main/java/com/jaamsim/input/InputAgent.java
@@ -504,7 +504,7 @@ public class InputAgent {
 	 * @param keyword - the keyword.
 	 * @param args - the input value String for the keyword.
 	 */
-	public static void applyArgs(Entity ent, String keyword, String... args){
+	public static void applyArgs(Entity ent, String keyword, String... args) {
 		KeywordIndex kw = KeywordIndex.formatArgs(keyword, args);
 		InputAgent.apply(ent, kw);
 	}
@@ -519,17 +519,13 @@ public class InputAgent {
 		InputAgent.apply(ent, kw);
 	}
 
-	public static void applyIntegers(Entity ent, String keyword, int... args){
+	public static void applyIntegers(Entity ent, String keyword, int... args) {
 		KeywordIndex kw = KeywordIndex.formatIntegers(keyword, args);
 		InputAgent.apply(ent, kw);
 	}
 
-	public static void applyValue(Entity ent, String keyword, double val, String unit){
-		ArrayList<String> tokens = new ArrayList<>(2);
-		tokens.add(String.format((Locale)null, "%s", val));
-		if (unit != null && !unit.isEmpty())
-			tokens.add(unit);
-		KeywordIndex kw = new KeywordIndex(keyword, tokens, null);
+	public static void applyValue(Entity ent, String keyword, double val, String unit) {
+		KeywordIndex kw = KeywordIndex.formatValue(keyword, val, unit);
 		InputAgent.apply(ent, kw);
 	}
 

src/main/java/com/jaamsim/input/KeywordIndex.java
@@ -143,4 +143,13 @@ public class KeywordIndex {
 		}
 		return new KeywordIndex(keyword, tokens, null);
 	}
+
+	public static KeywordIndex formatValue(String keyword, double val, String unit) {
+		ArrayList<String> tokens = new ArrayList<>(2);
+		tokens.add(String.format((Locale)null, "%s", val));
+		if (unit != null && !unit.isEmpty())
+			tokens.add(unit);
+		return new KeywordIndex(keyword, tokens, null);
+	}
+
 }